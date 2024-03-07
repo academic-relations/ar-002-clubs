1095cd0bda22b252fc059f295ed37928fc168527
Nicole Lee <nicolelee2001@gmail.com>
2024-03-08 01:57:39 +0900

    feat: impl Card component

---
+++ packages/web/src/common/components/Card.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+
+const CardInner = styled.div`
+  position: relative;
+  padding: 16px 20px;
+  font-family: ${({ theme }) => theme.fonts.FAMILY.PRETENDARD};
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: ${({ theme }) => theme.fonts.WEIGHT.REGULAR};
+  color: ${({ theme }) => theme.colors.BLACK};
+  background-color: ${({ theme }) => theme.colors.WHITE};
+  border-radius: ${({ theme }) => theme.round.md};
+  box-shadow: ${({ theme }) => theme.shadow.md};
+`;
+
+const Card: React.FC<React.PropsWithChildren> = ({ children = <div /> }) => (
+  <CardInner>{children}</CardInner>
+);
+
+export default Card;
