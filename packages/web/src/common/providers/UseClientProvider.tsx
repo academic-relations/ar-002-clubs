5b21914f66f08b24281f06700d8432e954fe262e
Nicole Lee <nicolelee2001@gmail.com>
2024-03-09 00:49:54 +0900

    refactor: apply responsive layout within ClientProvider

--- packages/web/src/common/providers/UseClientProvider.tsx
+++ packages/web/src/common/providers/UseClientProvider.tsx
@@ -8,15 +8,40 @@
 
 import React from "react";
 
-import { ThemeProvider as StyledProvider } from "styled-components";
+import styled, { ThemeProvider as StyledProvider } from "styled-components";
 
 import theme from "@sparcs-clubs/web/styles/themes";
+
+const ResponsiveContent = styled.div`
+  width: 1120px;
+  margin: 80px auto;
+
+  @media (max-width: 1440px) {
+    width: 880px;
+  }
+
+  @media (max-width: 1200px) {
+    width: 640px;
+  }
+
+  @media (max-width: 960px) {
+    width: 560px;
+    margin: 40px auto;
+  }
+
+  @media (max-width: 720px) {
+    width: calc(100% - 40px);
+    margin: 20px;
+  }
+`;
 
 export const UseClientProvider: React.FC<React.PropsWithChildren> = ({
   children = <div />,
 }) => (
   <main>
     {/* @ts-expect-error-next-line */}
-    <StyledProvider theme={theme}>{children}</StyledProvider>
+    <StyledProvider theme={theme}>
+      <ResponsiveContent>{children}</ResponsiveContent>
+    </StyledProvider>
   </main>
 );
