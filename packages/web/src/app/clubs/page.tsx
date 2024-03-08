9e48d98ebcd68be438778253445c1432779350b9
Woojeong Choi <gldeel@kaist.ac.kr>
2024-03-08 23:49:50 +0900

    feat: impl section title

--- packages/web/src/app/clubs/page.tsx
+++ packages/web/src/app/clubs/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import SectionTitle from "@sparcs-clubs/web/common/components/SectionTitle";
 import { UseClientProvider } from "@sparcs-clubs/web/common/providers/UseClientProvider";
 // import clubsData from "./data/clubs";
 
 const Clubs = () => (
   <main>
-    <UseClientProvider>{/* TODO */}</UseClientProvider>
+    <UseClientProvider>
+      <SectionTitle>생활문화</SectionTitle>
+    </UseClientProvider>
   </main>
 );
 
