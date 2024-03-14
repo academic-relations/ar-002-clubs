735ae5359fdd97255ccc9dad76edb9e7988e5616
Jiho Park <jihopark7777@gmail.com>
2024-03-14 20:35:11 +0900

    feat(prisma): configure prisma module

--- packages/api/src/env.ts
+++ packages/api/src/env.ts
@@ -1,9 +1,28 @@
+import { PrismaClientOptions } from "@prisma/client/runtime";
 import { z } from "zod";
 
 const schema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]),
   SERVER_PORT: z.coerce.number(),
   SECRET_KEY: z.string(),
+  DATABASE_URL: z.string(),
 });
 
-export const env = schema.parse(process.env);
+const env = schema.parse(process.env);
+
+const prismaConfig: PrismaClientOptions = {
+  datasources: {
+    db: {
+      url: env.DATABASE_URL,
+    },
+  },
+  errorFormat: "pretty",
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+  ],
+};
+
+export { env, prismaConfig };
