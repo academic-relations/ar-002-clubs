59bee5a59daae4af02d75d5ca7ff81375350bd68
Nicole Lee <nicolelee2001@gmail.com>
2024-03-17 15:05:40 +0900

    refactor: refactor ClubCard

--- packages/web/src/features/clubs/components/ClubCard.tsx
+++ packages/web/src/features/clubs/components/ClubCard.tsx
@@ -4,10 +4,8 @@ import React from "react";
 import styled from "styled-components";
 
 import Card from "@sparcs-clubs/web/common/components/Card";
+import Icon from "@sparcs-clubs/web/common/components/Icon";
 import Tag from "@sparcs-clubs/web/common/components/Tag";
-
-import ClubNameAndMemberCountRow from "@sparcs-clubs/web/features/clubs/components/ClubNameAndMemberCountRow";
-import DescriptionRow from "@sparcs-clubs/web/features/clubs/components/DescriptionRow";
 
 import type { ClubInfo } from "@sparcs-clubs/web/types/clubs.types";
 import {
@@ -15,42 +13,76 @@ import {
   getTagColorFromClubType,
 } from "@sparcs-clubs/web/types/clubs.types";
 
-const ClubCardWrapper = styled.div`
-  width: 100%;
-`;
+interface ClubCardProps {
+  club: ClubInfo;
+}
 
 const ClubCardInner = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  gap: 16px;
 `;
 
-interface ClubCardProps {
-  club: ClubInfo;
-}
+const ClubCardRow = styled.div`
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
 
-const ClubCard: React.FC<ClubCardProps> = ({ club }) => {
-  const presidentAndAdvisor: string =
-    club.advisor === null
-      ? `회장 ${club.president}`
-      : `회장 ${club.president} | 지도교수 ${club.advisor}`;
-  return (
-    <ClubCardWrapper>
-      <Card>
-        <ClubCardInner>
-          <ClubNameAndMemberCountRow
-            name={club.name}
-            membersCount={club.members}
-          />
-          <DescriptionRow>{presidentAndAdvisor}</DescriptionRow>
-          <DescriptionRow>{club.description}</DescriptionRow>
-          <Tag color={getTagColorFromClubType(club.type)}>
-            {getClubType(club)}
-          </Tag>
-        </ClubCardInner>
-      </Card>
-    </ClubCardWrapper>
-  );
-};
+const ClubCardNameRow = styled(ClubCardRow)`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ClubName = styled.div`
+  width: 100%;
+  height: 24px;
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: ${({ theme }) => theme.fonts.WEIGHT.MEDIUM};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const ClubMemberCount = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+  font-size: 14px;
+  line-height: 16px;
+`;
+
+const ClubCard: React.FC<ClubCardProps> = ({ club }) => (
+  <Card>
+    <ClubCardInner>
+      <ClubCardNameRow>
+        <ClubName>{club.name}</ClubName>
+        <ClubMemberCount>
+          <Icon type="person" size={16} />
+          <div>{club.members}</div>
+        </ClubMemberCount>
+      </ClubCardNameRow>
+
+      <ClubCardRow>
+        {club.advisor === null
+          ? `회장 ${club.president}`
+          : `회장 ${club.president} | 지도교수 ${club.advisor}`}
+      </ClubCardRow>
+      <ClubCardRow>{club.description}</ClubCardRow>
+
+      <ClubCardRow>
+        <Tag color={getTagColorFromClubType(club.type)}>
+          {getClubType(club)}
+        </Tag>
+      </ClubCardRow>
+    </ClubCardInner>
+  </Card>
+);
 
 export default ClubCard;
