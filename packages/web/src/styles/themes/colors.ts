fa0a9419d77fc0e859ce6da0d61423f1077359fc
Woojeong Choi <gldeel@kaist.ac.kr>
2024-03-19 00:35:43 +0900

    style: add colors

--- packages/web/src/styles/themes/colors.ts
+++ packages/web/src/styles/themes/colors.ts
@@ -22,6 +22,18 @@ const colors = {
     200: "#E7F0FF",
     600: "#065DAC",
   },
+  PURPLE: {
+    200: "#F6EEFE",
+    600: "#9836EF",
+  },
+  PINK: {
+    200: "#FFF1F9",
+    600: "#DB2777",
+  },
+  YELLOW: {
+    200: "#FFFDE9",
+    600: "#E9A311",
+  },
 };
 
 export default colors;
