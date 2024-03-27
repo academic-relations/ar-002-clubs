54c6fa5014d9d81d50ddff60b6a6d525c00d55aa
Woojeong Choi <gldeel@kaist.ac.kr>
2024-03-27 16:22:46 +0900

    feat: textInput

--- packages/web/src/common/components/Forms/TextInput.tsx
+++ packages/web/src/common/components/Forms/TextInput.tsx
@@ -1,16 +1,27 @@
 import React, { InputHTMLAttributes } from "react";
-import styled from "styled-components";
-import Label from "./_atomic/Label";
-import ErrorMessage from "./_atomic/ErrorMessage";
+import styled, { css } from "styled-components";
+import Label from "@sparcs-clubs/web/common/components/Forms/_atomic/Label";
+import ErrorMessage from "@sparcs-clubs/web/common/components/Forms/_atomic/ErrorMessage";
 
 // Define the props interface
 interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   placeholder: string;
   errorMessage?: string;
+  area?: boolean; // Add this line
 }
 
-const Input = styled.input`
+const errorBorderStyle = css`
+  border-color: ${({ theme }) => theme.colors.RED[600]};
+`;
+
+const areaInputStyle = css`
+  height: 100px;
+  // TODO text 위쪽 정렬
+  // TODO 여러 줄 받기
+`;
+
+const Input = styled.input<{ hasError?: boolean; area?: boolean }>`
   display: block;
   width: 300px;
   padding: 8px 12px 8px 12px;
@@ -23,6 +34,7 @@ const Input = styled.input`
   line-height: 20px;
   font-weight: ${({ theme }) => theme.fonts.WEIGHT.REGULAR};
   color: ${({ theme }) => theme.colors.BLACK};
+  background-color: ${({ theme }) => theme.colors.WHITE};
   &:focus {
     border-color: ${({ theme }) => theme.colors.PRIMARY};
   }
@@ -35,11 +47,20 @@ const Input = styled.input`
   &:disabled {
     background-color: ${({ theme }) => theme.colors.GRAY[100]};
     border-color: ${({ theme }) => theme.colors.GRAY[200]};
-    color: ${({ theme }) => theme.colors.GRAY[300]};
+    color: ${({ theme }) => theme.colors.RED[600]};
   }
   ::placeholder {
     color: ${({ theme }) => theme.colors.GRAY[200]};
   }
+  ${({ hasError }) => hasError && errorBorderStyle}
+  ${({ area }) => area && areaInputStyle} // TextAreaInput
+`;
+
+const InputWrapper = styled.div`
+  width: 300px;
+  flex-direction: column;
+  display: flex;
+  gap: 4px;
 `;
 
 // Component
@@ -47,11 +68,20 @@ const TextInput: React.FC<TextInputProps> = ({
   label = "",
   placeholder,
   errorMessage = "",
+  area = false,
 }) => (
   <div>
-    {label && <Label>{label}</Label>}
-    <Input placeholder={placeholder} />
-    {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    <InputWrapper>
+      {label && <Label>{label}</Label>}
+      <InputWrapper>
+        <Input
+          placeholder={placeholder}
+          hasError={!!errorMessage}
+          area={area}
+        />
+        {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+      </InputWrapper>
+    </InputWrapper>
   </div>
 );
 
