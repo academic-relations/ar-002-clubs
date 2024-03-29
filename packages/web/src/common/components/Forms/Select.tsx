773652c5a62ea968295d8ab519fea3b0c8064ddd
Woojeong Choi <gldeel@kaist.ac.kr>
2024-03-30 00:54:59 +0900

    feat: select and selectbox

--- packages/web/src/common/components/Forms/Select.tsx
+++ packages/web/src/common/components/Forms/Select.tsx
@@ -1,51 +1,28 @@
-"use client";
-
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
+import SelectBox, { SelectProps } from "./SelectBox";
+import Label from "./_atomic/Label";
+import ErrorMessage from "./_atomic/ErrorMessage";
 
-interface SelectItem {
-  label: string;
-  value: string;
-  selectable: boolean; // 선택 불가능한 항목 구분
-}
-
-interface SelectProps {
-  items: SelectItem[];
-}
-
-const StyledSelect = styled.select`
-  width: 100%;
-  padding: 8px 16px;
-  margin: 0;
-  background-color: white;
-  color: black;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+const InputWrapper = styled.div`
+  width: 300px;
+  flex-direction: column;
+  display: flex;
+  gap: 4px;
 `;
 
-const StyledOption = styled.option`
-  /* option에 대한 스타일이며, 브라우저에 따라 제한적일 수 있음 */
-  color: darkblue;
-  background-color: lightgray;
-`;
+const Select: React.FC<SelectProps> = ({
+  label = "",
+  errorMessage = "",
+  ...props
+}) => (
+  <InputWrapper>
+    {label && <Label>{label}</Label>}
+    <InputWrapper>
+      <SelectBox {...props} />
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </InputWrapper>
+  </InputWrapper>
+);
 
-const Select: React.FC<SelectProps> = ({ items }) => {
-  const [selectedValue, setSelectedValue] = useState("");
-
-  // Filter out the non-selectable items.
-  const selectableItems = items.filter(item => item.selectable);
-
-  return (
-    <StyledSelect
-      value={selectedValue}
-      onChange={e => setSelectedValue(e.target.value)}
-    >
-      {selectableItems.map(item => (
-        <StyledOption key={item.value} value={item.value}>
-          {item.label}
-        </StyledOption>
-      ))}
-    </StyledSelect>
-  );
-};
 export default Select;
