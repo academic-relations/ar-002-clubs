c471bebd3334f75a25713503c4093933a129c0cd
Jiuuung <kjg682534@kaist.ac.kr>
2024-04-02 17:46:07 +0900

    fix(server): fix wrong generic use

--- packages/api/src/features/clubs/clubs.controller.ts
+++ packages/api/src/features/clubs/clubs.controller.ts
@@ -1,11 +1,14 @@
 import { ClubsResponseDtoType } from "@sparcs-clubs/interface/common/interfaces/IClubs";
 import { Controller, Get, UseFilters } from "@nestjs/common";
 import { ClubExceptionFilter } from "@sparcs-clubs/api/exceptions/exception.filter";
-import { ClubNotFoundException } from "@sparcs-clubs/api/exceptions/club.exception";
+import {
+  ClubException,
+  ClubNotFoundException,
+} from "@sparcs-clubs/api/exceptions/club.exception";
 import { ClubsService } from "./clubs.service";
 
 @Controller("clubs")
-@UseFilters(new ClubExceptionFilter())
+@UseFilters(new ClubExceptionFilter<ClubException>())
 export class ClubsController {
   constructor(private readonly clubsService: ClubsService) {}
 
