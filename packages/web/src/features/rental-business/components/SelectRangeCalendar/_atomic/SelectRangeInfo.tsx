0a92ff9f7ba5ff4312bdfbe692f2b04ac0e6cb4b
Woojeong Choi <gldeel@kaist.ac.kr>
2024-04-21 20:31:56 +0900

    feat: create selectRangeInfo

---
+++ packages/web/src/features/rental-business/components/SelectRangeCalendar/_atomic/SelectRangeInfo.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const LabelWrapper = styled.div`
+  font-size: 16px;
+`;
+
+const SelectRangeInfo: React.FC = () => (
+  <div>
+    <LabelWrapper>label</LabelWrapper>
+  </div>
+);
+
+export default SelectRangeInfo;
