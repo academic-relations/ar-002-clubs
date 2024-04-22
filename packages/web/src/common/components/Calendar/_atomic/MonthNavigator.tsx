cc53f9344c189abb587ea95d38391bb325adb2e1
Woojeong Choi <gldeel@kaist.ac.kr>
2024-04-23 00:05:18 +0900

    fix: edit icon

--- packages/web/src/common/components/Calendar/_atomic/MonthNavigator.tsx
+++ packages/web/src/common/components/Calendar/_atomic/MonthNavigator.tsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { addMonths, format, subMonths } from "date-fns";
 import { ko } from "date-fns/locale";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import Icon from "@sparcs-clubs/web/common/components/Icon";
 
 interface MonthNavigatorProps {
   initialDate?: Date;
@@ -15,7 +14,6 @@ const NavigatorWrapper = styled.div`
   align-items: center;
   justify-content: space-between;
   width: 108px;
-  /* 화살표 안 움직이게 하려고 피그마(96px)랑 약간 다르게 했는데 확인해주세요 */
   font-size: 16px;
   line-height: 20px;
   font-weight: ${({ theme }) => theme.fonts.WEIGHT.MEDIUM};
@@ -24,26 +22,6 @@ const NavigatorWrapper = styled.div`
   color: ${({ theme }) => theme.colors.BLACK};
   user-select: none;
   gap: 16px;
-`;
-
-const IconWrapper = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 20px;
-  height: 20px;
-  background: none;
-  border: none;
-  cursor: pointer;
-  padding: 0;
-`;
-
-const StyledChevronLeftIcon = styled(ChevronLeftIcon)`
-  font-size: 20px;
-`;
-
-const StyledChevronRightIcon = styled(ChevronRightIcon)`
-  font-size: 20px;
 `;
 
 const MonthDisplay = styled.div`
@@ -71,13 +49,9 @@ const MonthNavigator: React.FC<MonthNavigatorProps> = ({
 
   return (
     <NavigatorWrapper>
-      <IconWrapper onClick={handlePrevious}>
-        <StyledChevronLeftIcon />
-      </IconWrapper>
+      <Icon type="chevron_left" size={20} onClick={handlePrevious} />
       <MonthDisplay>{format(currentDate, "M월", { locale: ko })}</MonthDisplay>
-      <IconWrapper onClick={handleNext}>
-        <StyledChevronRightIcon />
-      </IconWrapper>
+      <Icon type="chevron_right" size={20} onClick={handleNext} />
     </NavigatorWrapper>
   );
 };
--- packages/web/src/common/components/Icon.tsx
+++ packages/web/src/common/components/Icon.tsx
@@ -1,23 +1,30 @@
 "use client";
 
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Icon as MUIIcon } from "@mui/material";
 
 interface IconProps {
   type: string;
   size: number;
+  onClick?: () => void;
 }
 
-const IconInner = styled.div<{ size: number }>`
+const IconInner = styled.div<{ size: number; clickable: boolean }>`
   display: flex;
   font-size: ${({ size }) => size}px;
   color: ${({ theme }) => theme.colors.BLACK};
+  ${({ clickable }) =>
+    clickable &&
+    css`
+      cursor: pointer;
+    `}
 `;
 
-const Icon: React.FC<IconProps> = ({ type, size }) => (
-  <IconInner size={size}>
+const Icon: React.FC<IconProps> = ({ type, size, onClick = () => {} }) => (
+  <IconInner size={size} clickable={!!onClick} onClick={onClick}>
     <MUIIcon fontSize="inherit">{type}</MUIIcon>
   </IconInner>
 );
+
 export default Icon;
