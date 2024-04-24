cf2eaa9777a5d59aba3d3ed00e5ac5eb4642d070
Woojeong Choi <gldeel@kaist.ac.kr>
2024-04-24 23:35:40 +0900

    feat: add useEffect for date changes

--- packages/web/src/features/rental-business/components/SelectRangeCalendar/SelectRangeCalendar.tsx
+++ packages/web/src/features/rental-business/components/SelectRangeCalendar/SelectRangeCalendar.tsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import SelectRange from "./_atomic/SelectRange";
 import RangeCalendar from "./_atomic/RangeCalendar";
 
+interface SelectRangeCalendarProps {
+  onDatesChange: (
+    rentalDate: Date | undefined,
+    returnDate: Date | undefined,
+  ) => void;
+}
+
 const SelectRangeCalendarWrapper = styled.div`
   display: inline-flex;
   width: 1000px;
+  /* 나중에 calendar 관련 width 확인할 때 여기도 봐야함 */
   gap: 20px;
   align-items: flex-end;
 `;
 
-const SelectRangeCalendar: React.FC = () => {
+const SelectRangeCalendar: React.FC<SelectRangeCalendarProps> = ({
+  onDatesChange,
+}) => {
   const [rentalDate, setRentalDate] = useState<Date | undefined>();
   const [returnDate, setReturnDate] = useState<Date | undefined>();
+
+  useEffect(() => {
+    onDatesChange(rentalDate, returnDate);
+  }, [rentalDate, returnDate, onDatesChange]);
+
   return (
     <SelectRangeCalendarWrapper>
       <RangeCalendar
