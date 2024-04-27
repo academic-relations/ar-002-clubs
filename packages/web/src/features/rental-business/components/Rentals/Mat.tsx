9606de6b769b605c62f9f350c2a95d91d4d4ac5e
Woojeong Choi <gldeel@kaist.ac.kr>
2024-04-27 22:03:33 +0900

    feat: impl mat

--- packages/web/src/features/rental-business/components/Rentals/Mat.tsx
+++ packages/web/src/features/rental-business/components/Rentals/Mat.tsx
@@ -1,8 +1,24 @@
-import Typography from "@sparcs-clubs/web/common/components/Typography";
+import React from "react";
+import ItemNumberInput from "@sparcs-clubs/web/common/components/Forms/ItemNumberInput";
 import { RentalFrameProps } from "@sparcs-clubs/web/features/rental-business/frames/RentalNoticeFrame";
 
-const Mat: React.FC<RentalFrameProps> = () => (
-  <Typography type="p">대여 개수</Typography>
+const Mat: React.FC<RentalFrameProps & { limit?: number }> = ({
+  limit = 99,
+  rental,
+  setRental,
+}) => (
+  <ItemNumberInput
+    label="돗자리 개수"
+    placeholder="0개"
+    itemLimit={limit}
+    value={rental?.mat ? String(rental?.mat) : undefined}
+    handleChange={value =>
+      setRental({
+        ...rental,
+        mat: Number(value),
+      })
+    }
+  />
 );
 
 export default Mat;
