3acd7c2b56d81e6439df9785c55308130ebafe59
Woojeong Choi <gldeel@kaist.ac.kr>
2024-04-27 22:03:07 +0900

    feat: impl easel

--- packages/web/src/features/rental-business/components/Rentals/Easel.tsx
+++ packages/web/src/features/rental-business/components/Rentals/Easel.tsx
@@ -1,8 +1,24 @@
-import Typography from "@sparcs-clubs/web/common/components/Typography";
+import React from "react";
+import ItemNumberInput from "@sparcs-clubs/web/common/components/Forms/ItemNumberInput";
 import { RentalFrameProps } from "@sparcs-clubs/web/features/rental-business/frames/RentalNoticeFrame";
 
-const Easel: React.FC<RentalFrameProps> = () => (
-  <Typography type="p">대여 개수</Typography>
+const Easel: React.FC<RentalFrameProps & { limit?: number }> = ({
+  limit = 99,
+  rental,
+  setRental,
+}) => (
+  <ItemNumberInput
+    label="이젤 개수"
+    placeholder="0개"
+    itemLimit={limit}
+    value={rental?.easel ? String(rental?.easel) : undefined}
+    handleChange={value =>
+      setRental({
+        ...rental,
+        easel: Number(value),
+      })
+    }
+  />
 );
 
 export default Easel;
