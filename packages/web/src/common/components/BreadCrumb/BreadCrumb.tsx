294b7ba7494b56436b4408ee64f39fcc9bb0e529
Woojeong Choi <gldeel@kaist.ac.kr>
2024-05-02 23:27:43 +0900

    feat: impl breadCrumb link

--- packages/web/src/common/components/BreadCrumb/BreadCrumb.tsx
+++ packages/web/src/common/components/BreadCrumb/BreadCrumb.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import colors from "@sparcs-clubs/web/styles/themes/colors";
+import Link from "next/link";
 import BreadCrumbItem from "./_atomic/BreadCrumbItem";
 import Icon from "../Icon";
 
+interface BreadCrumbItemDetails {
+  name: string;
+  path: string;
+}
+
 interface BreadCrumbProps {
-  items: string[];
+  items: BreadCrumbItemDetails[];
 }
 
 const BreadCrumbContainer = styled.div`
@@ -15,16 +21,18 @@ const BreadCrumbContainer = styled.div`
 `;
 
 const BreadCrumb: React.FC<BreadCrumbProps> = ({ items }) => {
-  const itemsWithMain = ["메인", ...items];
+  const itemsWithMain = [{ name: "메인", path: "/" }, ...items];
 
   return (
     <BreadCrumbContainer>
       {itemsWithMain.map((item, index) => (
         <>
-          <BreadCrumbItem
-            text={item}
-            disabled={index === itemsWithMain.length - 1}
-          />
+          <Link href={item.path} passHref>
+            <BreadCrumbItem
+              text={item.name}
+              disabled={index === itemsWithMain.length - 1}
+            />
+          </Link>
           {index < itemsWithMain.length - 1 && (
             <Icon type="chevron_right" size={20} color={colors.PRIMARY} />
           )}
