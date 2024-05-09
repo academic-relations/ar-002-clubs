6dca14cb45147a66fcba179cbd7af95c8794b043
Jiuuung <kjg682534@kaist.ac.kr>
2024-05-10 07:32:44 +0900

    feat: apiClb009, apiClb010 interface

---
+++ packages/interface/src/api/club/endpoint/apiClb010.ts
@@ -0,0 +1,59 @@
+import { HttpStatusCode } from "axios";
+import { z } from "zod";
+/**
+ * @version v0.1
+ * @description semesterId에 해당하는 학기에 cludId의 동아리에서 활동한 모든 회원 정보를 가져옵니다.
+ */
+
+const url = (clubId: number, semesterId: number) =>
+  `/student/clubs/club/${clubId}/members/semesters/semester/${semesterId}`;
+const method = "GET";
+
+const requestParam = z.object({
+  cludId: z.number().int().min(1),
+  semesterId: z.number().int().min(1),
+});
+
+const requestQuery = z.object({});
+
+const requestBody = z.object({});
+
+const responseBodyMap = {
+  [HttpStatusCode.Ok]: z.object({
+    semesterId: z.number().int().min(1),
+    members: z
+      .object({
+        studentNumber: z.number().int().min(20000000).max(30000000),
+        name: z.string().max(30),
+        email: z.string().max(50),
+        phoneNumber: z.string().max(20).optional(),
+      })
+      .array(),
+  }),
+};
+
+const responseErrorMap = {};
+
+const apiClb010 = {
+  url,
+  method,
+  requestParam,
+  requestQuery,
+  requestBody,
+  responseBodyMap,
+  responseErrorMap,
+};
+
+type ApiClb010RequestParam = z.infer<typeof apiClb010.requestParam>;
+type ApiClb010RequestQuery = z.infer<typeof apiClb010.requestQuery>;
+type ApiClb010RequestBody = z.infer<typeof apiClb010.requestBody>;
+type ApiClb010ResponseOk = z.infer<(typeof apiClb010.responseBodyMap)[200]>;
+
+export default apiClb010;
+
+export type {
+  ApiClb010RequestParam,
+  ApiClb010RequestQuery,
+  ApiClb010RequestBody,
+  ApiClb010ResponseOk,
+};
