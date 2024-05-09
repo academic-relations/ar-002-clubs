c089025a760d857b9becf7f559286f50f0605847
Woojeong Choi <gldeel@kaist.ac.kr>
2024-05-09 22:23:21 +0900

    feat: add modal to rental business

--- packages/web/src/features/rental-business/frames/RentalInfoFrame/index.tsx
+++ packages/web/src/features/rental-business/frames/RentalInfoFrame/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 import Button from "@sparcs-clubs/web/common/components/Button";
 import StepProcess from "@sparcs-clubs/web/common/components/StepProcess/StepProcess";
+import Modal from "@sparcs-clubs/web/common/components/Modal";
 import { RentalFrameProps } from "../RentalNoticeFrame";
 import RentalInfoFirstFrame from "./RentalInfoFirstFrame";
 import RentalInfoSecondFrame from "./RentalInfoSecondFrame";
@@ -53,21 +54,37 @@ const RentalInfoFrame: React.FC<RentalFrameProps> = ({ rental, setRental }) => {
   const props = { rental, setRental };
   const [step, setStep] = useState(0);
   const [nextEnabled, setNextEnabled] = useState(true);
+  const [showReturnModal, setShowReturnModal] = useState(false);
+  const [showAssignModal, setShowAssignModal] = useState(false);
+
   const CurrentFrame = frames[step];
 
   const onPrev = useCallback(() => {
-    if (step === 0) {
-      setRental({ ...rental, agreement: false });
+    if (step === 1) {
+      setShowReturnModal(true);
+      // 값이 있을 때 이전 단계 돌아갈지 물어보도록
       return;
     }
     setStep(step - 1);
-  }, [step, setStep, rental, setRental]);
+  }, [step, setStep]);
 
   const onNext = useCallback(() => {
     if (nextEnabled && step < frames.length - 1) {
       setStep(step + 1);
     }
+    if (step === frames.length - 1) {
+      setShowAssignModal(true);
+    }
   }, [nextEnabled, step, setStep]);
+
+  const onCloseReturnModal = useCallback(() => {
+    setShowReturnModal(false);
+    setStep(0);
+  }, []);
+
+  const onCloseAssignModal = useCallback(() => {
+    setShowAssignModal(false);
+  }, []);
 
   return (
     <RentalFrame>
@@ -77,13 +94,25 @@ const RentalInfoFrame: React.FC<RentalFrameProps> = ({ rental, setRental }) => {
       </RentalNoticeFrameInner>
       <StyledBottom>
         <Button onClick={onPrev}>이전</Button>
-        {/* TODO: 2에서 1로 돌아가려고 하면 초기화 경고 modal */}
         <Button onClick={onNext} type={nextEnabled ? "default" : "disabled"}>
           {step === frames.length - 1 ? "신청" : "다음"}
         </Button>
-        {/* TODO: 신청 완료 modal */}
         {/* TODO: 백이랑 연결 */}
       </StyledBottom>
+      {showReturnModal && (
+        <Modal onClose={onCloseReturnModal}>
+          <div>
+            정말로 이전 단계로 돌아가시겠습니까? 모든 입력 정보가 초기화됩니다.
+          </div>
+          <Button onClick={onCloseReturnModal}>확인</Button>
+        </Modal>
+      )}
+      {showAssignModal && (
+        <Modal onClose={onCloseAssignModal}>
+          <div>신청하시겠습니까? 신청 후에는 수정이 불가능합니다.</div>
+          <Button onClick={onCloseAssignModal}>확인</Button>
+        </Modal>
+      )}
     </RentalFrame>
   );
 };
