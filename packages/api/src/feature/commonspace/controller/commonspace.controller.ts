5c76d63d48e427ddab1fcc289d69351d83beb46e
Jiuuung <kjg682534@kaist.ac.kr>
2024-05-12 21:26:36 +0900

    feat: impl cms001 on progress

---
+++ packages/api/src/feature/commonspace/controller/commonspace.controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from "@nestjs/common";
+import { ApiCms001ResponseOK } from "@sparcs-clubs/interface/api/common-space/endpoint/apiCms001";
+import { CommonSpaceService } from "../service/commonspace.service";
+
+@Controller("common-spaces")
+export class CommonSpaceController {
+  constructor(private readonly commonspaceService: CommonSpaceService) {}
+
+  async getCommonSpaces(): Promise<ApiCms001ResponseOK> {
+    const result = await this.commonspaceService.getCommonSpaces();
+    return result;
+  }
+}
