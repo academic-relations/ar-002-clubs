83efedaceacdd1cf26d993555a7464e4757d624a
Woojeong Choi <gldeel@kaist.ac.kr>
2024-05-13 00:45:10 +0900

    feat: impl MemberSearchAndFilter

---
+++ packages/web/src/common/components/SearchInput.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SearchInput = () => <div>검색</div>;
+
+export default SearchInput;
--- packages/web/src/features/manage-club/members/components/MemberSearchAndFilter.tsx
+++ packages/web/src/features/manage-club/members/components/MemberSearchAndFilter.tsx
@@ -1,5 +1,38 @@
+import FilterButton from "@sparcs-clubs/web/common/components/FilterButton";
+import SearchInput from "@sparcs-clubs/web/common/components/SearchInput";
+import TextButton from "@sparcs-clubs/web/common/components/TextButton";
 import React from "react";
+import styled from "styled-components";
 
-const MemberSearchAndFilter = () => <div>검색&필터</div>;
+const MemberSearchAndFilterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const SearchAndFilterWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+`;
+
+const ResetButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-end;
+`;
+
+const MemberSearchAndFilter = () => (
+  <MemberSearchAndFilterWrapper>
+    <SearchAndFilterWrapper>
+      <SearchInput />
+      <FilterButton />
+    </SearchAndFilterWrapper>
+    <ResetButtonWrapper>
+      <TextButton text="검색/필터 초기화" />
+    </ResetButtonWrapper>
+  </MemberSearchAndFilterWrapper>
+);
 
 export default MemberSearchAndFilter;
