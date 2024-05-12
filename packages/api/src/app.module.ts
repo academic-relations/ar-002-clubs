1c648dec72d372e0f91eba99996cf8c614168625
Jiuuung <kjg682534@kaist.ac.kr>
2024-05-12 23:03:18 +0900

    fix: app.module.ts

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ClubModule } from "./feature/club/club.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DrizzleModule } from "./drizzle/drizzle.module";
 import { UserModule } from "./feature/user/user.module";
 
 @Module({
-  imports: [DrizzleModule, UserModule],
+  imports: [DrizzleModule, ClubModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
