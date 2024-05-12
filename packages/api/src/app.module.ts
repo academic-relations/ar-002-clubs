c82cbecf76b3c6354f6d5c6f97b76c9281eb600c
Jiuuung <kjg682534@kaist.ac.kr>
2024-05-12 23:04:16 +0900

    fix: app.module.ts

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { CommonSpaceModule } from "./feature/commonspace/commonspace.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DrizzleModule } from "./drizzle/drizzle.module";
 import { UserModule } from "./feature/user/user.module";
 
 @Module({
-  imports: [DrizzleModule, UserModule],
+  imports: [DrizzleModule, CommonSpaceModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
