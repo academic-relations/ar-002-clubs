2301c20b3462577568d04c0cebdad7cc71db511b
Woojeong Choi <gldeel@kaist.ac.kr>
2024-05-18 18:05:40 +0900

    feat: impl manageclub main frame

---
+++ packages/web/src/features/manage-club/ManageClubMainFrame.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const ManageClubMainFrame: React.FC = () => (
+  <div>
+    manageclub main
+    <div>동아리 정보</div>
+    <div>동아리 활동</div>
+    <div>회원 명단</div>
+    <div>서비스 신청</div>
+  </div>
+);
+
+export default ManageClubMainFrame;
