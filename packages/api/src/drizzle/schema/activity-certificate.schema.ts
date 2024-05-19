a99a9d075e8174d3669f8e9e6aec97f51510dcfb
Woojeong Choi <gldeel@kaist.ac.kr>
2024-05-20 00:26:17 +0900

    fix: fix activity certificate schema

--- packages/api/src/drizzle/schema/activity-certificate.schema.ts
+++ packages/api/src/drizzle/schema/activity-certificate.schema.ts
@@ -6,35 +6,8 @@ import {
   timestamp,
   date,
 } from "drizzle-orm/mysql-core";
-
-// ActivityCertificate table
-export const ActivityCertificate = mysqlTable("activity_certificate", {
-  id: int("id").autoincrement().primaryKey(),
-  clubId: int("club_id").notNull(),
-  studentId: int("student_id").notNull(),
-  studentPhoneNumber: varchar("student_phone_number", { length: 30 }),
-  activityCertificateStatusEnum: int(
-    "activity_certificate_status_enum",
-  ).notNull(),
-  issueNumber: int("issue_number"),
-  issuedAt: datetime("issued_at"),
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at"),
-  deletedAt: timestamp("deleted_at"),
-});
-
-// ActivityCertificateItem table
-export const ActivityCertificateItem = mysqlTable("activity_certificate_item", {
-  id: int("id").autoincrement().primaryKey(),
-  activityCertificateId: int("activity_certificate_id").notNull(),
-  order: int("order").notNull(),
-  startMonth: date("start_month").notNull(),
-  endMonth: date("end_month").notNull(),
-  detail: varchar("detail", { length: 100 }),
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at"),
-  deletedAt: timestamp("deleted_at"),
-});
+import { Student } from "./user.schema";
+import { Club } from "./club.schema";
 
 // ActivityCertificateStatusEnum table
 export const ActivityCertificateStatusEnum = mysqlTable(
@@ -47,3 +20,38 @@ export const ActivityCertificateStatusEnum = mysqlTable(
     deletedAt: timestamp("deleted_at"),
   },
 );
+
+// ActivityCertificate table
+export const ActivityCertificate = mysqlTable("activity_certificate", {
+  id: int("id").autoincrement().primaryKey(),
+  clubId: int("club_id")
+    .notNull()
+    .references(() => Club.id),
+  studentId: int("student_id")
+    .notNull()
+    .references(() => Student.id),
+  studentPhoneNumber: varchar("student_phone_number", { length: 30 }),
+  activityCertificateStatusEnum: int("activity_certificate_status_enum")
+    .notNull()
+    .references(() => ActivityCertificateStatusEnum.id),
+  issueNumber: int("issue_number"),
+  issuedAt: datetime("issued_at"),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at"),
+  deletedAt: timestamp("deleted_at"),
+});
+
+// ActivityCertificateItem table
+export const ActivityCertificateItem = mysqlTable("activity_certificate_item", {
+  id: int("id").autoincrement().primaryKey(),
+  activityCertificateId: int("activity_certificate_id")
+    .notNull()
+    .references(() => ActivityCertificate.id),
+  order: int("order").notNull(),
+  startMonth: date("start_month").notNull(),
+  endMonth: date("end_month").notNull(),
+  detail: varchar("detail", { length: 100 }),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at"),
+  deletedAt: timestamp("deleted_at"),
+});
