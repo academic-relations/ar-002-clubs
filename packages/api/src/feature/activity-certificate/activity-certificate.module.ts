6239f6404b351ab0df6a178b7e7851144aaf3da8
Woojeong Choi <gldeel@kaist.ac.kr>
2024-05-20 01:10:28 +0900

    fix: delete activity certificate item repository

--- packages/api/src/feature/activity-certificate/activity-certificate.module.ts
+++ packages/api/src/feature/activity-certificate/activity-certificate.module.ts
@@ -4,15 +4,10 @@ import { DrizzleModule } from "src/drizzle/drizzle.module";
 import { ActivityCertificateController } from "./controller/activity-certificate.controller";
 import { ActivityCertificateService } from "./service/activity-certificate.service";
 import { ActivityCertificateRepository } from "./repository/activity-certificate.repository";
-import { ActivityCertificateItemRepository } from "./repository/activity-certificate-item.repository";
 
 @Module({
   imports: [DrizzleModule],
   controllers: [ActivityCertificateController],
-  providers: [
-    ActivityCertificateService,
-    ActivityCertificateRepository,
-    ActivityCertificateItemRepository,
-  ],
+  providers: [ActivityCertificateService, ActivityCertificateRepository],
 })
 export class ActivityCertificateModule {}
