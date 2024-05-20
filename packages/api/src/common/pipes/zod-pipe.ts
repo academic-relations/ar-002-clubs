4e1669fa4295cceca0e3fb0f219ad71d4428e9fa
Jiuuung <kjg682534@kaist.ac.kr>
2024-05-20 22:18:49 +0900

    fix: comment

--- packages/api/src/common/pipes/zod-pipe.ts
+++ packages/api/src/common/pipes/zod-pipe.ts
@@ -10,6 +10,14 @@ interface RequestZodSchema {
 export class ZodPipe implements PipeTransform {
   constructor(private schema: RequestZodSchema) {}
 
+  /*
+  요약
+  - request의 param, query, body를 zod타입에 맞게 cast하고 validation하는 역할
+  작업 과정
+  1. interface의 attribute에 coerce 추가
+  2. controller에 @UsePipes(new ZodPipe(apiRnt001))와 같은 형태로 데코레이터 추가
+  3. controller에서 @Param(), @Query(), @Body()로 pipe 적용
+  */
   transform(value: unknown, metadata: ArgumentMetadata) {
     const schemaMap: Record<string, ZodSchema> = {
       param: this.schema.requestParam,
