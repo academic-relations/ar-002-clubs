baa3c4d9fb0766b8040d22ee8855f9b700714012
Woojeong Choi <gldeel@kaist.ac.kr>
2024-05-25 04:42:15 +0900

    feat: add designer notice

--- packages/web/src/app/not-found.tsx
+++ packages/web/src/app/not-found.tsx
@@ -8,6 +8,8 @@ const Custom404: NextPage = () => (
   <div>
     <Image src={myGif} alt="Not Implemented Yet" width={400} height={400} />
     아직 구현되지 않았어요!
+    <h1>디</h1>
+    <h1>자이너 구함!!</h1>
   </div>
 );
 
