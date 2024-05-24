1b2d7af7643591d87c87ae8e5a79355b69655972
pbc1017 <knsol2@naver.com>
2024-05-24 13:35:33 +0900

    feat: api clb-003

---
+++ packages/api/src/common/repository/club.club-t.respository.ts
@@ -0,0 +1,29 @@
+import { Injectable, Inject } from "@nestjs/common";
+import { MySql2Database } from "drizzle-orm/mysql2";
+import { DrizzleAsyncProvider } from "src/drizzle/drizzle.provider";
+import { ClubT } from "src/drizzle/schema/club.schema";
+import { and, eq } from "drizzle-orm";
+import { Professor } from "@sparcs-clubs/api/drizzle/schema/user.schema";
+
+@Injectable()
+export class ClubTRepository {
+  constructor(@Inject(DrizzleAsyncProvider) private db: MySql2Database) {}
+
+  async findClubDetail(semesterId: number, clubId: number) {
+    return this.db
+      .select({
+        clubStatusEnumId: ClubT.clubStatusEnumId,
+        characteristicKr: ClubT.characteristicKr,
+        professorId: ClubT.professorId,
+        professorName: Professor.name,
+      })
+      .from(ClubT)
+      .where(and(eq(ClubT.semesterId, semesterId), eq(ClubT.id, clubId)))
+      .leftJoin(Professor, eq(Professor.id, ClubT.professorId))
+      .then(result => ({
+        clubStatusEnumId: result[0]?.clubStatusEnumId,
+        characteristicKr: result[0]?.characteristicKr,
+        advisor: result[0]?.professorId ? result[0].professorName : null,
+      }));
+  }
+}
