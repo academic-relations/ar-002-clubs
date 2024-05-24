f329763280a2e730f80a0145f4ebf29471614637
Woojeong Choi <gldeel@kaist.ac.kr>
2024-05-24 15:16:29 +0900

    feat: impl progress line

---
+++ packages/web/src/common/components/ProgressCheckSection/_atomic/ProgressLine.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { Status } from "./ProgressDot";
+
+const ProgressLine = styled.div<{ status: Status }>`
+  display: flex;
+  height: 2px;
+  background-color: ${props => {
+    switch (props.status) {
+      case Status.Approved:
+        return props.theme.colors.PRIMARY;
+      case Status.Canceled:
+        return props.theme.colors.RED[600];
+      case Status.NotYet:
+        return props.theme.colors.GRAY[300];
+      default:
+        return props.theme.colors.GRAY[300];
+    }
+  }};
+`;
+
+export default ProgressLine;
