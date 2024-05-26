c077555187d1d5af164e623783277985d3c843d5
pbc1017 <knsol2@naver.com>
2024-05-27 07:50:01 +0900

    fix: interface ApiNtc001

--- packages/api/src/feature/notice/controller/notice.controller.ts
+++ packages/api/src/feature/notice/controller/notice.controller.ts
@@ -1,14 +1,11 @@
-import { Controller, Get, Query } from "@nestjs/common";
-
+import { Controller, Get, Query, UsePipes } from "@nestjs/common";
 import apiNtc001 from "@sparcs-clubs/interface/api/notice/endpoint/apiNtc001";
-
 import type {
   ApiNtc001RequestQuery,
   ApiNtc001ResponseOK,
 } from "@sparcs-clubs/interface/api/notice/endpoint/apiNtc001";
-
 import logger from "@sparcs-clubs/api/common/util/logger";
-
+import { ZodPipe } from "@sparcs-clubs/api/common/pipe/zod-pipe";
 import { NoticeService } from "../service/notice.service";
 
 @Controller()
@@ -16,27 +13,17 @@ export class NoticeController {
   constructor(private readonly noticesService: NoticeService) {}
 
   @Get("/notices")
+  @UsePipes(new ZodPipe(apiNtc001))
   async getNotices(
-    @Query("pageOffset") pageOffset: ApiNtc001RequestQuery["pageOffset"],
-    @Query("itemCount") itemCount: ApiNtc001RequestQuery["itemCount"],
+    @Query() query: ApiNtc001RequestQuery,
   ): Promise<ApiNtc001ResponseOK> {
-    // zodExceptionHandler는 전역으로 관리될것이라고 알고있어서 추가하지 않았습니다
-    // 쿼리 파라미터를 파싱하는 가장 좋은 방법에 관해 고민이 됩니다
-    const query: ApiNtc001RequestQuery = apiNtc001.requestQuery.parse({
-      pageOffset: Number(pageOffset),
-      itemCount: Number(itemCount),
-    });
-
-    // 로거 사용예시입니다.
     logger.debug(
       `[/notices] offset: ${query.pageOffset}, count: ${query.itemCount}`,
     );
-
     const notices = await this.noticesService.getNotices(
       query.pageOffset,
       query.itemCount,
     );
-
     return notices;
   }
 }
