e52bc43af4dc9d28e75321f3dd7b2db09e726380
YINGMEI <j.yeongmee@gmail.com>
2024-05-27 21:44:38 +0900

    feat: add bread crumb component to service related pages

--- packages/web/src/features/activity-certificate/frames/ActivityCertificateMainFrame.tsx
+++ packages/web/src/features/activity-certificate/frames/ActivityCertificateMainFrame.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import BreadCrumb from "@sparcs-clubs/web/common/components/BreadCrumb";
 import PageTitle from "@sparcs-clubs/web/common/components/PageTitle";
 import React, { useState } from "react";
 import styled from "styled-components";
@@ -9,8 +10,8 @@ import {
   FirstErrorStatus,
   SecondErrorStatus,
 } from "../types/activityCertificate";
-import ActivityCertificateNoticeFrame from "./ActivityCertificateNoticeFrame";
 import ActivityCertificateInfoFrame from "./ActivityCertificateInfoFrame";
+import ActivityCertificateNoticeFrame from "./ActivityCertificateNoticeFrame";
 
 const ActivityCertificatePageMainFrameInner = styled.div`
   width: 100%;
@@ -20,6 +21,12 @@ const ActivityCertificatePageMainFrameInner = styled.div`
   align-items: flex-start;
   gap: 60px;
   display: inline-flex;
+`;
+
+const PageHeadWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 const ActivityCertificateMainFrame: React.FC = () => {
@@ -78,7 +85,12 @@ const ActivityCertificateMainFrame: React.FC = () => {
 
   return (
     <ActivityCertificatePageMainFrameInner>
-      <PageTitle>활동확인서 발급</PageTitle>
+      <PageHeadWrapper>
+        <BreadCrumb
+          items={[{ name: "활동확인서 발급", path: "/activity-certificate" }]}
+        />
+        <PageTitle>활동확인서 발급</PageTitle>
+      </PageHeadWrapper>
       {activityCertificateProgress.agreement ? (
         <ActivityCertificateInfoFrame {...props} />
       ) : (
