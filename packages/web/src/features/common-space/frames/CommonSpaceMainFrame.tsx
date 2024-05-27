814d28a5c8d2ab619fe0b8b032dd6c8a1517c346
YINGMEI <j.yeongmee@gmail.com>
2024-05-27 21:44:38 +0900

    feat: add bread crumb component to service related pages

--- packages/web/src/features/common-space/frames/CommonSpaceMainFrame.tsx
+++ packages/web/src/features/common-space/frames/CommonSpaceMainFrame.tsx
@@ -1,14 +1,21 @@
+import BreadCrumb from "@sparcs-clubs/web/common/components/BreadCrumb";
+import PageTitle from "@sparcs-clubs/web/common/components/PageTitle";
 import React from "react";
 import styled from "styled-components";
-import PageTitle from "@sparcs-clubs/web/common/components/PageTitle";
 import type { CommonSpaceInterface } from "../types/commonSpace";
-import CommonSpaceNoticeFrame from "./CommonSpaceNoticeFrame";
 import CommonSpaceInfoFrame from "./CommonSpaceInfoFrame";
+import CommonSpaceNoticeFrame from "./CommonSpaceNoticeFrame";
 
 const CommonSpaceMainFrameInner = styled.div`
   display: flex;
   flex-direction: column;
   gap: 60px;
+`;
+
+const PageHeadWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 const CommonSpaceMainFrame: React.FC = () => {
@@ -18,7 +25,12 @@ const CommonSpaceMainFrame: React.FC = () => {
   const props = { commonSpace, setCommonSpace };
   return (
     <CommonSpaceMainFrameInner>
-      <PageTitle>공용공간 비정기사용</PageTitle>
+      <PageHeadWrapper>
+        <BreadCrumb
+          items={[{ name: "공용공간 비정기사용", path: "/common-space" }]}
+        />
+        <PageTitle>공용공간 비정기사용</PageTitle>
+      </PageHeadWrapper>
       {commonSpace.agreement ? (
         <CommonSpaceInfoFrame {...props} />
       ) : (
--- packages/web/src/features/rental-business/frames/RentalMainFrame.tsx
+++ packages/web/src/features/rental-business/frames/RentalMainFrame.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import styled from "styled-components";
 import PageTitle from "@sparcs-clubs/web/common/components/PageTitle";
 import RentalInfoFrame from "@sparcs-clubs/web/features/rental-business/frames/RentalInfoFrame";
+import React from "react";
+import styled from "styled-components";
 
+import BreadCrumb from "@sparcs-clubs/web/common/components/BreadCrumb";
 import type { RentalInterface } from "../types/rental";
 import RentalNoticeFrame from "./RentalNoticeFrame";
 
@@ -12,6 +13,12 @@ const RentalMainFrameInner = styled.div`
   gap: 60px;
 `;
 
+const PageHeadWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
 const RentalMainFrame: React.FC = () => {
   const [rental, setRental] = React.useState<RentalInterface>({
     agreement: false,
@@ -19,7 +26,10 @@ const RentalMainFrame: React.FC = () => {
   const props = { rental, setRental };
   return (
     <RentalMainFrameInner>
-      <PageTitle>대여 사업</PageTitle>
+      <PageHeadWrapper>
+        <BreadCrumb items={[{ name: "대여 사업", path: "/rental-business" }]} />
+        <PageTitle>대여 사업</PageTitle>
+      </PageHeadWrapper>
       {rental.agreement ? (
         <RentalInfoFrame {...props} />
       ) : (
