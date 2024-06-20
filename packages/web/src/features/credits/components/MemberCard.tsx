89805f0b8f104e9f80ba86010c73f9435b50326b
Woojeong Choi <gldeel@kaist.ac.kr>
2024-06-21 08:45:19 +0900

    feat: impl hover comment

--- packages/web/src/features/credits/components/MemberCard.tsx
+++ packages/web/src/features/credits/components/MemberCard.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Card from "@sparcs-clubs/web/common/components/Card";
 import Typography from "@sparcs-clubs/web/common/components/Typography";
 import styled from "styled-components";
@@ -18,32 +19,54 @@ const MemberCard = ({
 }: {
   member: Member;
   isSmall?: boolean;
-}) => (
-  <Card padding="16px 20px" gap={8}>
-    <MemberWrapper>
-      <Image src={SparcsLogo} alt="SPARCS Logo" height={isSmall ? 20 : 24} />
+}) => {
+  const [displayText, setDisplayText] = useState(member.role);
+
+  const handleMouseEnter = () => {
+    if (member.comment) {
+      setDisplayText(member.comment);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setDisplayText(member.role);
+  };
+
+  return (
+    <Card padding="16px 20px" gap={8}>
+      <MemberWrapper>
+        <Image src={SparcsLogo} alt="SPARCS Logo" height={isSmall ? 20 : 24} />
+        <Typography
+          ff="RALEWAY"
+          fs={isSmall ? 14 : 16}
+          lh={isSmall ? 20 : 24}
+          fw="EXTRABOLD" // TODO: EXTRABOLD가 적용되지 않는 것 같음
+          color="SPARCS.main"
+        >
+          {member.nickname}
+        </Typography>
+        {/* TODO: 나눔스퀘어 글꼴 적용 */}
+        <Typography
+          fs={isSmall ? 10 : 12}
+          lh={isSmall ? 16 : 20}
+          color="SPARCS.member"
+        >
+          {member.name}
+        </Typography>
+      </MemberWrapper>
       <Typography
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
         ff="RALEWAY"
-        fs={isSmall ? 14 : 16}
-        lh={isSmall ? 20 : 24}
-        fw="EXTRABOLD" // TODO: EXTRABOLD가 적용되지 않는 것 같음
-        color="SPARCS.main"
+        fw="BOLD"
+        fs={10}
+        lh={16}
+        color="GRAY.600"
       >
-        {member.nickname}
+        {displayText}
       </Typography>
-      {/* TODO: 나눔스퀘어 글꼴 적용 */}
-      <Typography
-        fs={isSmall ? 10 : 12}
-        lh={isSmall ? 16 : 20}
-        color="SPARCS.member"
-      >
-        {member.name}
-      </Typography>
-    </MemberWrapper>
-    <Typography ff="RALEWAY" fw="BOLD" fs={10} lh={16} color="GRAY.600">
-      {member.role}
-    </Typography>
-  </Card>
-);
+    </Card>
+  );
+};
 
 export default MemberCard;
