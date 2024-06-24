fe34b2c823936b3866caac214d670006193ac8e5
Woojeong Choi <gldeel@kaist.ac.kr>
2024-06-24 16:54:59 +0900

    feat: add roleType

--- packages/web/src/features/credits/credits.ts
+++ packages/web/src/features/credits/credits.ts
@@ -2,6 +2,7 @@ export interface Member {
   nickname: string;
   name: string;
   role: string;
+  roleType: "PM" | "APM" | "intern" | "member";
   comment?: string;
 }
 
@@ -18,51 +19,61 @@ const credits: Credits[] = [
         nickname: "chan",
         name: "박병찬",
         role: "PM",
+        roleType: "PM",
       },
       {
         nickname: "dora",
         name: "이지윤",
         role: "APM(FE) / Designer",
+        roleType: "APM",
       },
       {
         nickname: "hama",
         name: "하승종",
         role: "APM(BE)",
+        roleType: "APM",
       },
       {
         nickname: "andy",
         name: "예상우",
         role: "FE",
+        roleType: "member",
       },
       {
         nickname: "ava",
         name: "주영미",
         role: "FE",
+        roleType: "member",
       },
       {
         nickname: "april",
         name: "남지현",
         role: "BE",
+        roleType: "member",
       },
       {
         nickname: "david",
         name: "정동윤",
         role: "FE",
+        roleType: "member",
       },
       {
         nickname: "eel",
         name: "최우정",
         role: "FE / BE",
+        roleType: "member",
       },
       {
         nickname: "tom",
         name: "양지웅",
         role: "FE / BE",
+        roleType: "member",
       },
       {
         nickname: "night",
         name: "박지호",
         role: "??",
+        roleType: "member",
       },
     ],
   },
@@ -73,51 +84,61 @@ const credits: Credits[] = [
         nickname: "night",
         name: "박지호",
         role: "PM",
+        roleType: "PM",
       },
       {
         nickname: "dora",
         name: "이지윤",
         role: "APM(FE) / Designer",
+        roleType: "APM",
       },
       {
         nickname: "chan",
         name: "박병찬",
         role: "APM(BE)",
+        roleType: "APM",
       },
       {
         nickname: "andy",
         name: "예상우",
         role: "FE",
+        roleType: "member",
       },
       {
         nickname: "april",
         name: "남지현",
         role: "BE",
+        roleType: "member",
       },
       {
         nickname: "ava",
         name: "주영미",
         role: "FE",
+        roleType: "member",
       },
       {
         nickname: "david",
         name: "정동윤",
         role: "FE",
+        roleType: "member",
       },
       {
         nickname: "tom",
         name: "양지웅",
         role: "BE",
+        roleType: "member",
       },
       {
         nickname: "eel",
         name: "최우정",
         role: "인턴",
+        roleType: "intern",
       },
       {
         nickname: "hama",
         name: "하승종",
         role: "인턴",
+        roleType: "intern",
       },
     ],
   },
