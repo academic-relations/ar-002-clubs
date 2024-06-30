187163c4be3a68e982bba2501b5677de39453312
Woojeong Choi <gldeel@kaist.ac.kr>
2024-06-30 20:27:21 +0200

    feat: impl my acf table without pagination

--- packages/web/src/app/my/activity-certificate/page.tsx
+++ packages/web/src/app/my/activity-certificate/page.tsx
@@ -1,25 +1,84 @@
 "use client";
 
-import React from "react";
+import React, { useMemo } from "react";
+
+import { ActivityCertificateOrderStatusEnum } from "@sparcs-clubs/interface/common/enum/activityCertificate.enum";
 
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
+import Tag from "@sparcs-clubs/web/common/components/Tag";
 import MyServiceTable from "@sparcs-clubs/web/features/my/component/MyServiceTable";
 
-const MyActivityCertificate = () => (
-  <FlexWrapper direction="column" gap={20}>
-    <PageHead
-      items={[
-        { name: "마이페이지", path: "/my" },
-        {
-          name: "활동확인서 발급 내역",
-          path: "/my/activity-certificate",
-        },
-      ]}
-      title="활동확인서 발급 내역"
-    />
-    <MyServiceTable />
-  </FlexWrapper>
-);
+import { mockupMyAcf } from "@sparcs-clubs/web/features/my/service/_mock/mockMyClub";
+import { formatDateTime } from "@sparcs-clubs/web/utils/Date/formateDate";
+import {
+  getTagDetail,
+  StatusDetail,
+} from "@sparcs-clubs/web/utils/getTagDetail";
+
+const MyActivityCertificate = () => {
+  const TagList: {
+    [key in ActivityCertificateOrderStatusEnum]: StatusDetail;
+  } = {
+    [ActivityCertificateOrderStatusEnum.Applied]: {
+      text: "신청",
+      color: "BLUE",
+    },
+    [ActivityCertificateOrderStatusEnum.Approved]: {
+      text: "승인",
+      color: "YELLOW",
+    },
+    [ActivityCertificateOrderStatusEnum.Issued]: {
+      text: "발급",
+      color: "GREEN",
+    },
+    [ActivityCertificateOrderStatusEnum.Rejected]: {
+      text: "반려",
+      color: "RED",
+    },
+    [ActivityCertificateOrderStatusEnum.Received]: {
+      text: "수령",
+      color: "GREEN",
+    }, // TODO: 수령 따로 필요한지 확인
+  };
+  return (
+    <FlexWrapper direction="column" gap={20}>
+      <PageHead
+        items={[
+          { name: "마이페이지", path: "/my" },
+          {
+            name: "활동확인서 발급 내역",
+            path: "/my/activity-certificate",
+          },
+        ]}
+        title="활동확인서 발급 내역"
+      />
+      <MyServiceTable
+        headers={[
+          { type: "Header", text: "상태" },
+          { type: "Header", text: "신청 일시" },
+          { type: "Header", text: "동아리" },
+          { type: "Header", text: "발급 매수" },
+        ]}
+        widths={[10, 30, 30, 30]}
+        minWidths={[90, 180, 180, 180]}
+        contents={useMemo(
+          () =>
+            mockupMyAcf.items.map(item => {
+              const { color, text } = getTagDetail(item.statusEnum, TagList);
+              return [
+                <Tag color={color}>{text}</Tag>,
+                formatDateTime(item.createdAt),
+                item.studentName, // TODO: mock data 재사용하면서 student name으로 되어있는데 동아리 이름으로 바꾸기
+                `${item.issuedNumber}매`,
+              ];
+            }),
+          [],
+        )}
+        contentsTypes={["Tag", "Default", "Default", "Default"]}
+      />
+    </FlexWrapper>
+  );
+};
 
 export default MyActivityCertificate;
--- packages/web/src/features/my/component/MyServiceTable.tsx
+++ packages/web/src/features/my/component/MyServiceTable.tsx
@@ -1,5 +1,60 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const MyServiceTable: React.FC = () => <div>test</div>;
+import TableCell, {
+  TableCellType,
+} from "@sparcs-clubs/web/common/components/Table/TableCell";
+import {
+  TableRow,
+  TableWrapper,
+} from "@sparcs-clubs/web/common/components/Table/TableWrapper";
+
+interface TableHeaderProps {
+  type: TableCellType;
+  text: string;
+}
+interface MyServiceTableProps {
+  headers: TableHeaderProps[];
+  widths: number[];
+  minWidths: number[];
+  contentsTypes: TableCellType[];
+  contents: ReactNode[][];
+}
+
+const MyServiceTable: React.FC<MyServiceTableProps> = ({
+  headers,
+  widths,
+  minWidths,
+  contentsTypes,
+  contents,
+}) => (
+  <TableWrapper>
+    <TableRow>
+      {headers.map((header, index) => (
+        <TableCell
+          key={String(index) + header.text}
+          type={header.type}
+          width={`${widths[index]}%`}
+          minWidth={minWidths[index]}
+        >
+          {header.text}
+        </TableCell>
+      ))}
+    </TableRow>
+    {contents.map((content, rowIndex) => (
+      <TableRow key={String(rowIndex) + content[0]}>
+        {content.map((cell, cellIndex) => (
+          <TableCell
+            key={String(cellIndex) + cell}
+            type={contentsTypes[cellIndex]}
+            width={`${widths[cellIndex]}%`}
+            minWidth={minWidths[cellIndex]}
+          >
+            {cell}
+          </TableCell>
+        ))}
+      </TableRow>
+    ))}
+  </TableWrapper>
+);
 
 export default MyServiceTable;
