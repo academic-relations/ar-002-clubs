f0ad5ed054547fdeb334bdb2366214fb46cf8a75
YINGMEI <j.yeongmee@gmail.com>
2024-07-03 00:39:21 +0900

    fix: move pagehead component into flexWrapper component

--- packages/web/src/app/manage-club/funding/page.tsx
+++ packages/web/src/app/manage-club/funding/page.tsx
@@ -1,27 +1,7 @@
 "use client";
 
-import PageHead from "@sparcs-clubs/web/common/components/PageHead";
-import {
-  manageClubFundingPageBreadCrumbName,
-  manageClubFundingPageName,
-  manageClubFundingPagePath,
-} from "@sparcs-clubs/web/constants/manageClubFunding";
 import ManageClubFundingMainFrame from "@sparcs-clubs/web/features/manage-club/funding/frame/ManageClubFundingMainFrame";
 
-const Funding = () => (
-  <>
-    <PageHead
-      items={[
-        { name: "대표 동아리 관리", path: "/manage-club" },
-        {
-          name: manageClubFundingPageBreadCrumbName,
-          path: manageClubFundingPagePath,
-        },
-      ]}
-      title={manageClubFundingPageName}
-    />
-    <ManageClubFundingMainFrame />
-  </>
-);
+const Funding = () => <ManageClubFundingMainFrame />;
 
 export default Funding;
