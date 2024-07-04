0d44246c6bfb14f43d9d9c4b12d3f5aacc496984
Woojeong Choi <gldeel@kaist.ac.kr>
2024-07-04 20:30:07 +0200

    feat: impl FundingInfoFrame

--- packages/web/src/features/manage-club/funding/create/frame/_atomic/FundingInfoFrame.tsx
+++ packages/web/src/features/manage-club/funding/create/frame/_atomic/FundingInfoFrame.tsx
@@ -1,9 +1,32 @@
+import React, { useState } from "react";
+
+import Card from "@sparcs-clubs/web/common/components/Card";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
+import TextInput from "@sparcs-clubs/web/common/components/Forms/TextInput";
+import Select from "@sparcs-clubs/web/common/components/Select";
 
-const FundingInfoFrame = () => (
-  <FlexWrapper direction="column" gap={40}>
-    <FoldableSectionTitle title="지원금 정보" />
-  </FlexWrapper>
-);
+const FundingInfoFrame = () => {
+  const [toggle, setToggle] = useState(true);
+
+  return (
+    <FlexWrapper direction="column" gap={40}>
+      <FoldableSectionTitle
+        title="지원금 정보"
+        toggle={toggle}
+        toggleHandler={() => setToggle(!toggle)}
+      />
+      {toggle && (
+        <Card outline style={{ marginLeft: 24 }}>
+          <TextInput label="항목명" placeholder="항목명을 입력하세요" />
+          <FlexWrapper direction="row" gap={32}>
+            <Select items={[]} label="지출 목적" />
+            {/* TODO: dateInput */}
+            {/* TODO: 지출 금액 입력 */}
+          </FlexWrapper>
+        </Card>
+      )}
+    </FlexWrapper>
+  );
+};
 export default FundingInfoFrame;
