ba9c1aebeb88346cbe7f283d58b228ca60969b82
Woojeong Choi <gldeel@kaist.ac.kr>
2024-07-05 23:26:16 +0200

    chore: delete unused annotation

--- packages/web/src/features/manage-club/funding/create/frame/FundingInfoFrame.tsx
+++ packages/web/src/features/manage-club/funding/create/frame/FundingInfoFrame.tsx
@@ -3,10 +3,8 @@ import React, { useState } from "react";
 import Card from "@sparcs-clubs/web/common/components/Card";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
-// import DateInput from "@sparcs-clubs/web/common/components/Forms/DateInput";
 import TextInput from "@sparcs-clubs/web/common/components/Forms/TextInput";
 import Select from "@sparcs-clubs/web/common/components/Select";
-// import Typography from "@sparcs-clubs/web/common/components/Typography";
 
 const FundingInfoFrame = () => {
   const [toggle, setToggle] = useState<boolean>(true);
@@ -46,24 +44,12 @@ const FundingInfoFrame = () => {
               onSelect={setSelectedPurpose}
             />
             {/* TODO: 지출 일자, 지출 금액 해당 컴포넌트로 구현 */}
-            <FlexWrapper direction="column" gap={4} style={{ width: "100%" }}>
-              {/* <Typography
-                    ff="PRETENDARD"
-                    fw="MEDIUM"
-                    fs={16}
-                    lh={20}
-                    color="BLACK"
-                  >
-                    지출 일자
-                  </Typography>
-                  <DateInput date={new Date()} /> */}
-              <TextInput
-                label="지출 일자"
-                placeholder="20XX.XX.XX"
-                value={date}
-                handleChange={setDate}
-              />
-            </FlexWrapper>
+            <TextInput
+              label="지출 일자"
+              placeholder="20XX.XX.XX"
+              value={date}
+              handleChange={setDate}
+            />
             <TextInput
               label="지출 금액"
               placeholder="금액을 입력해주세요"
