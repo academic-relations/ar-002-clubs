3690d9c2d3672f38e48c6bf1b2aee476a0e503d8
Woojeong Choi <gldeel@kaist.ac.kr>
2024-07-11 10:41:55 +0200

    feat: funding enums

--- packages/interface/src/common/enum/funding.enum.ts
+++ packages/interface/src/common/enum/funding.enum.ts
@@ -4,3 +4,37 @@ export enum FundingOrderStatusEnum {
   Rejected, // 반려
   Commitee, // 운위
 }
+
+export enum FundingDeadlineEnum {
+  Activity, // 활동
+  Writing, // 작성
+  Revision, // 수정
+  Review, // 검토
+  Exception, // 예외
+}
+
+export enum FixtureEvidenceEnum {
+  Purchase, // 구매
+  Management, // 관리
+}
+
+export enum FixtureClassEnum {
+  Electronics, // 전자기기
+  Furniture, // 가구
+  MusicalInstruments, // 악기
+  Software, // 소프트웨어
+  Others, // 기타
+}
+
+export enum TransportationEnum {
+  CityBus, // 시내/마을버스
+  IntercityBus, // 고속/시외버스
+  Rail, // 철도
+  Taxi, // 택시
+  CharterBus, // 전세버스
+  Cargo, // 화물 운반
+  CallVan, // 콜밴
+  Airplane, // 비행기
+  Ship, // 선박
+  Others, // 기타
+}
