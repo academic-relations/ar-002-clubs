37e3e3a058bc1be5306d94e89577cb3bf8b5dcca
YINGMEI <j.yeongmee@gmail.com>
2024-07-13 21:08:43 +0900

    feat: add Info card to RegisterClubMainFrame

--- packages/web/src/features/register-club/frame/RegisterClubMainFrame.tsx
+++ packages/web/src/features/register-club/frame/RegisterClubMainFrame.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 
+import Info from "@sparcs-clubs/web/common/components/Info";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
 
 import BasicInformSection from "../components/BasicInformSection";
@@ -25,6 +26,8 @@ const RegisterClubMainFrame: React.FC<RegisterClubMainFrameProps> = ({
       title={`동아리 ${type} 신청`}
       enableLast
     />
+    {/* TODO. 등록 기간, 신청마감 동적처리  */}
+    <Info text="현재는 2024년 봄학기 동아리 등록 기간입니다 (신청 마감 : 2024년 3월 10일 23:59)" />
     <BasicInformSection
       isFirstApply={type === RegisterClubType.provisional}
       isReapply={type === RegisterClubType.renewal}
