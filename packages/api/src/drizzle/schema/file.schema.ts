498baae4e398108c2b2f8228b4b779f6d1ec99c7
Perry Byeongchan Park <chan@sparcs.org>
2024-07-13 21:18:22 +0900

    feat: impl file upload (#363)

---
+++ packages/api/src/drizzle/schema/file.schema.ts
@@ -0,0 +1,31 @@
+import {
+  datetime,
+  int,
+  mysqlTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 as uuidv4 } from "uuid";
+
+import { User } from "./user.schema";
+
+function createId(): string {
+  return uuidv4();
+}
+
+export const File = mysqlTable("file", {
+  id: varchar("id", { length: 128 })
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  extension: varchar("extension", { length: 30 }).notNull(),
+  size: int("size").notNull(),
+  userId: int("user_id")
+    .notNull()
+    .references(() => User.id),
+  signedAt: datetime("signed_at").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+  deletedAt: timestamp("deleted_at"),
+});
