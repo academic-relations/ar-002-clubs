effb80af7de5938ef7cc1b0d7cac9dfa59951b88
Woojeong Choi <gldeel@kaist.ac.kr>
2024-07-15 09:20:55 +0200

    feat: apply getUserProfile

--- packages/web/src/features/my/frame/MyInfoFrame.tsx
+++ packages/web/src/features/my/frame/MyInfoFrame.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import Button from "@sparcs-clubs/web/common/components/Button";
 import Card from "@sparcs-clubs/web/common/components/Card";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
 import PhoneInput from "@sparcs-clubs/web/common/components/Forms/PhoneInput";
+import useGetUserProfile from "@sparcs-clubs/web/features/common-space/service/getUserProfile";
 
 const MyInfoFrame: React.FC = () => {
-  const [mockPhone, setMockPhone] = React.useState<string>("01000000000");
+  const [toggle, setToggle] = useState<boolean>(true);
 
-  const [toggle, setToggle] = React.useState<boolean>(true);
+  const {
+    data: myInfo,
+    isLoading: infoLoading,
+    isError: infoError,
+  } = useGetUserProfile();
 
-  const [phone, setPhone] = React.useState<string>(mockPhone);
-  const [errorPhone, setErrorPhone] = React.useState<boolean>(false);
+  const myPhone = myInfo?.phoneNumber;
 
-  const buttonType = phone === mockPhone || errorPhone ? "disabled" : "default";
+  const [phone, setPhone] = useState<string>(myPhone ?? "");
+  const [errorPhone, setErrorPhone] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (myInfo) {
+      setPhone(myPhone ?? "");
+    }
+  }, [myInfo, myPhone]);
+
+  const buttonType = phone === myPhone || errorPhone ? "disabled" : "default";
 
   return (
     <FlexWrapper direction="column" gap={40}>
@@ -22,25 +36,26 @@ const MyInfoFrame: React.FC = () => {
         title="나의 정보"
         toggle={toggle}
         toggleHandler={() => setToggle(!toggle)}
-      />
-      {toggle && (
-        <Card outline gap={32} style={{ flex: 1 }}>
-          <PhoneInput
-            label="전화번호"
-            placeholder="전화번호를 입력하세요"
-            value={phone}
-            onChange={setPhone}
-            setErrorStatus={setErrorPhone}
-          />
-          <Button
-            type={buttonType}
-            style={{ width: "max-content", alignSelf: "flex-end" }}
-            onClick={() => setMockPhone(phone)} // TODO: 실제 전화번호 수정 기능 연결
-          >
-            저장
-          </Button>
-        </Card>
-      )}
+      >
+        <AsyncBoundary isLoading={infoLoading} isError={infoError}>
+          <Card outline gap={32} style={{ flex: 1 }}>
+            <PhoneInput
+              label="전화번호"
+              placeholder="전화번호를 입력하세요"
+              value={phone}
+              onChange={setPhone}
+              setErrorStatus={setErrorPhone}
+            />
+            <Button
+              type={buttonType}
+              style={{ width: "max-content", alignSelf: "flex-end" }}
+              onClick={() => {}} // TODO: 실제 전화번호 수정 기능 연결
+            >
+              저장
+            </Button>
+          </Card>
+        </AsyncBoundary>
+      </FoldableSectionTitle>
     </FlexWrapper>
   );
 };
