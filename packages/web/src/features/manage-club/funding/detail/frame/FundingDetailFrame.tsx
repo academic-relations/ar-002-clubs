104435715bc1d6016bd8657b22e29a64a2c1ea8d
Woojeong Choi <gldeel@kaist.ac.kr>
2024-07-16 21:52:34 +0200

    feat: onClick return

--- packages/web/src/features/manage-club/funding/detail/frame/FundingDetailFrame.tsx
+++ packages/web/src/features/manage-club/funding/detail/frame/FundingDetailFrame.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { useRouter } from "next/navigation";
 import styled from "styled-components";
 
 import Button from "@sparcs-clubs/web/common/components/Button";
@@ -20,39 +21,49 @@ const ButtonWrapper = styled.div`
   justify-content: space-between;
 `;
 
-const FundingDetailFrame: React.FC<FundingDetailFrameProps> = ({ isNow }) => (
-  <FlexWrapper direction="column" gap={40}>
-    <Card outline>
-      {isNow && (
-        <ProgressStatus
-          labels={["신청 완료", "동아리 연합회 승인 대기"]}
-          progress={[{ status: Status.Approved, date: new Date() }]}
-          // TODO: 반려일 경우 optional에 RejectReasonToast 추가
-        />
-      )}
-      <FundingInfoList />
-      <FlexWrapper direction="column" gap={16}>
-        <Typography
-          ff="PRETENDARD"
-          fw="MEDIUM"
-          fs={16}
-          lh={20}
-          color="BLACK"
-          style={{ paddingLeft: 2, paddingRight: 2 }}
-        >
-          필수 증빙
-        </Typography>
-      </FlexWrapper>
-    </Card>
-    <ButtonWrapper>
-      <Button type="default">목록으로 돌아가기</Button>
-      {isNow && (
-        <FlexWrapper direction="row" gap={10}>
-          <Button type="default">삭제</Button>
-          <Button type="default">신청</Button>
+const FundingDetailFrame: React.FC<FundingDetailFrameProps> = ({ isNow }) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push("/manage-club/funding");
+  };
+
+  return (
+    <FlexWrapper direction="column" gap={40}>
+      <Card outline>
+        {isNow && (
+          <ProgressStatus
+            labels={["신청 완료", "동아리 연합회 승인 대기"]}
+            progress={[{ status: Status.Approved, date: new Date() }]}
+            // TODO: 반려일 경우 optional에 RejectReasonToast 추가
+          />
+        )}
+        <FundingInfoList />
+        <FlexWrapper direction="column" gap={16}>
+          <Typography
+            ff="PRETENDARD"
+            fw="MEDIUM"
+            fs={16}
+            lh={20}
+            color="BLACK"
+            style={{ paddingLeft: 2, paddingRight: 2 }}
+          >
+            필수 증빙
+          </Typography>
         </FlexWrapper>
-      )}
-    </ButtonWrapper>
-  </FlexWrapper>
-);
+      </Card>
+      <ButtonWrapper>
+        <Button type="default" onClick={onClick}>
+          목록으로 돌아가기
+        </Button>
+        {isNow && (
+          <FlexWrapper direction="row" gap={10}>
+            <Button type="default">삭제</Button>
+            <Button type="default">신청</Button>
+          </FlexWrapper>
+        )}
+      </ButtonWrapper>
+    </FlexWrapper>
+  );
+};
 export default FundingDetailFrame;
