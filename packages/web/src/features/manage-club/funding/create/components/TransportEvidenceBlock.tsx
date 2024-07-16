29d86db876569f0eeca4e2b9a1818d5100bb2352
Woojeong Choi <gldeel@kaist.ac.kr>
2024-07-16 12:06:13 +0200

    feat: impl handleTransportationChange

--- packages/web/src/features/manage-club/funding/create/components/TransportEvidenceBlock.tsx
+++ packages/web/src/features/manage-club/funding/create/components/TransportEvidenceBlock.tsx
@@ -59,116 +59,113 @@ const purposeInfo = (enumString: string | undefined) => {
 const TransportEvidenceBlock: React.FC<FundingFrameProps> = ({
   funding,
   setFunding,
-}) => (
-  <FlexWrapper direction="column" gap={8}>
-    <EvidenceBlockTitle title="교통비 증빙">
-      <Card outline gap={32}>
-        <FlexWrapper direction="row" gap={32}>
-          <FixedWidthWrapper>
-            <Select
-              items={TransportationList}
-              label="교통수단"
-              placeholder="교통수단을 선택해주세요"
-              selectedValue={funding.transportation?.transportationEnumId}
-              onSelect={value =>
-                setFunding({
-                  ...funding,
-                  transportation: {
-                    ...funding.transportation,
-                    transportationEnumId: value,
-                  },
-                })
+}) => {
+  const handleTransportationChange = (key: string, value: string) => {
+    setFunding(prevFunding => ({
+      ...prevFunding,
+      transportation: {
+        ...prevFunding.transportation,
+        [key]: value,
+      },
+    }));
+  };
+
+  return (
+    <FlexWrapper direction="column" gap={8}>
+      <EvidenceBlockTitle title="교통비 증빙">
+        <Card outline gap={32}>
+          <FlexWrapper direction="row" gap={32}>
+            <FixedWidthWrapper>
+              <Select
+                items={TransportationList}
+                label="교통수단"
+                placeholder="교통수단을 선택해주세요"
+                selectedValue={funding.transportation?.transportationEnumId}
+                onSelect={value =>
+                  setFunding({
+                    ...funding,
+                    transportation: {
+                      ...funding.transportation,
+                      transportationEnumId: value,
+                    },
+                  })
+                }
+              />
+            </FixedWidthWrapper>
+            <TextInput
+              placeholder="출발지를 입력해주세요"
+              label="출발지"
+              value={funding.transportation?.origin}
+              handleChange={value =>
+                handleTransportationChange("origin", value)
               }
             />
-          </FixedWidthWrapper>
-          <TextInput
-            placeholder="출발지를 입력해주세요"
-            label="출발지"
-            value={funding.transportation?.origin}
-            handleChange={value =>
-              setFunding({
-                ...funding,
-                transportation: {
-                  ...funding.transportation,
-                  origin: value,
-                },
-              })
-            }
-          />
-          <TextInput
-            placeholder="도착지를 입력해주세요"
-            label="도착지"
-            value={funding.transportation?.destination}
-            handleChange={value =>
-              setFunding({
-                ...funding,
-                transportation: {
-                  ...funding.transportation,
-                  destination: value,
-                },
-              })
-            }
-          />
-        </FlexWrapper>
-        {isParticipantsRequired(
-          funding.transportation?.transportationEnumId,
-        ) && (
+            <TextInput
+              placeholder="도착지를 입력해주세요"
+              label="도착지"
+              value={funding.transportation?.destination}
+              handleChange={value =>
+                handleTransportationChange("destination", value)
+              }
+            />
+          </FlexWrapper>
+          {isParticipantsRequired(
+            funding.transportation?.transportationEnumId,
+          ) && (
+            <FlexWrapper direction="column" gap={4}>
+              <Typography
+                ff="PRETENDARD"
+                fs={16}
+                fw="MEDIUM"
+                lh={20}
+                color="BLACK"
+              >
+                탑승자 명단
+              </Typography>
+              <SelectParticipant
+                data={mockParticipantData}
+                // onChange 추가
+              />
+            </FlexWrapper>
+          )}
           <FlexWrapper direction="column" gap={4}>
             <Typography
               ff="PRETENDARD"
-              fs={16}
               fw="MEDIUM"
+              fs={16}
               lh={20}
               color="BLACK"
+              style={{ paddingLeft: 2, paddingRight: 2 }}
             >
-              탑승자 명단
+              이용 목적
             </Typography>
-            <SelectParticipant data={mockParticipantData} />
+            {isPurposeInfoRequired(
+              funding.transportation?.transportationEnumId,
+            ) && (
+              <Typography
+                ff="PRETENDARD"
+                fw="REGULAR"
+                fs={14}
+                lh={20}
+                color="GRAY.600"
+                style={{ whiteSpace: "pre-wrap" }}
+              >
+                {purposeInfo(funding.transportation?.transportationEnumId)}
+              </Typography>
+            )}
+            <TextInput
+              area
+              placeholder="이용 목적을 입력하세요"
+              value={funding.transportation?.purposeOfTransportation}
+              handleChange={value =>
+                handleTransportationChange("purposeOfTransportation", value)
+              }
+            />
           </FlexWrapper>
-        )}
-        <FlexWrapper direction="column" gap={4}>
-          <Typography
-            ff="PRETENDARD"
-            fw="MEDIUM"
-            fs={16}
-            lh={20}
-            color="BLACK"
-            style={{ paddingLeft: 2, paddingRight: 2 }}
-          >
-            이용 목적
-          </Typography>
-          {isPurposeInfoRequired(
-            funding.transportation?.transportationEnumId,
-          ) && (
-            <Typography
-              ff="PRETENDARD"
-              fw="REGULAR"
-              fs={14}
-              lh={20}
-              color="GRAY.600"
-              style={{ whiteSpace: "pre-wrap" }}
-            >
-              {purposeInfo(funding.transportation?.transportationEnumId)}
-            </Typography>
-          )}
-          <TextInput
-            area
-            placeholder="이용 목적을 입력하세요"
-            value={funding.transportation?.purposeOfTransportation}
-            handleChange={value =>
-              setFunding({
-                ...funding,
-                transportation: {
-                  ...funding.transportation,
-                  purposeOfTransportation: value,
-                },
-              })
-            }
-          />
-        </FlexWrapper>
-      </Card>
-    </EvidenceBlockTitle>
-  </FlexWrapper>
-);
+        </Card>
+      </EvidenceBlockTitle>
+    </FlexWrapper>
+  );
+};
 
 export default TransportEvidenceBlock;
