432f4d8f86fe7696f28dc7febb003f60ff1cce92
Woojeong Choi <gldeel@kaist.ac.kr>
2024-07-17 11:29:24 +0200

    feat: impl filePreview

---
+++ packages/web/src/common/components/FilePreview.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import InsertDriveFileOutlined from "@mui/icons-material/InsertDriveFileOutlined";
+import styled from "styled-components";
+
+import Icon from "./Icon";
+import Typography from "./Typography";
+
+interface FilePreviewProps {
+  fileName: string;
+  edit: boolean;
+}
+
+const FilePreviewWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  font-size: 16px;
+  justify-content: space-between;
+`;
+
+const FilePreview: React.FC<FilePreviewProps> = ({ fileName, edit }) => (
+  <FilePreviewWrapper>
+    <InsertDriveFileOutlined fontSize="inherit" />
+    <Typography
+      ff="PRETENDARD"
+      fs={14}
+      lh={16}
+      fw="REGULAR"
+      color="BLACK"
+      style={{ flex: 1 }}
+    >
+      {fileName}
+    </Typography>
+    {/* edit 기능 넣기 */}
+    {edit && <Icon type="close" size={16} />}
+  </FilePreviewWrapper>
+);
+
+export default FilePreview;
