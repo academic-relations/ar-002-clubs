cd7ba7770ee4cedcb44cbb6db252a7ef9e86ac7f
chacha <chacha@sparcs.org>
2024-07-18 19:52:36 +0900

    feat: delete frame and add border for selected

--- packages/web/src/features/register-club/components/ClubButton.tsx
+++ packages/web/src/features/register-club/components/ClubButton.tsx
@@ -8,36 +8,45 @@ import Typography from "@sparcs-clubs/web/common/components/Typography";
 interface ClubButtonProps {
   title: string;
   buttonText: string;
+  selected: bool;
   onClick?: VoidFunction;
 }
 
-const ClubButtonInner = styled.div`
+const ClubButtonTextInner = styled.div`
   display: flex;
-  height: 120px;
   flex-direction: column;
   justify-content: center;
   align-self: stretch;
+  height: 120px;
 `;
 
 const ClubButton: React.FC<ClubButtonProps> = ({
   title,
   buttonText,
+  selected,
   onClick = () => {},
 }) => (
-  <Card padding="20px 24px" gap={16} onClick={onClick}>
+  <Card
+    padding="20px 24px"
+    gap={16}
+    onClick={onClick}
+    style={{ border: selected ? "1px solid #0099AD" : "inherit" }}
+  >
     <Typography fw="MEDIUM" fs={20} lh={24}>
       {title}
     </Typography>
-    <ClubButtonInner>
+    <ClubButtonTextInner>
       <Typography
         fw="REGULAR"
         fs={12}
         lh={24}
-        style={{ whiteSpace: "pre-wrap" }}
+        style={{
+          whiteSpace: "pre-wrap",
+        }}
       >
         {buttonText}
       </Typography>
-    </ClubButtonInner>
+    </ClubButtonTextInner>
   </Card>
 );
 
