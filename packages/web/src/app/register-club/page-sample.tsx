dc45997338803e5500a55349695e99c3cb7ddfc1
chacha <chacha@sparcs.org>
2024-07-18 15:04:33 +0900

    feat: add frame and sample page for register-club

---
+++ packages/web/src/app/register-club/page-sample.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+import RegisterClubSampleFrame from "@sparcs-clubs/web/features/register-club/frame/RegisterClubSampleFrame";
+
+const RegisterClub = () => <RegisterClubSampleFrame />;
+
+export default RegisterClub;
---
+++ packages/web/src/features/register-club/frame/RegisterClubSampleFrame.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
+import PageHead from "@sparcs-clubs/web/common/components/PageHead";
+
+import ClubButton from "@sparcs-clubs/web/features/register-club/components/ClubButton";
+
+const RegisterClubSampleFrame: React.FC = () => (
+  <FlexWrapper
+    direction="column"
+    gap={60}
+    style={{ alignItems: "flex-start", width: "1120px" }}
+  >
+    <PageHead
+      items={[{ name: "동아리 등록", path: "/register-club" }]}
+      title="동아리 등록"
+    />
+    <ClubButton
+      title="재등록"
+      buttonText={[
+        {
+          id: 1,
+          text: "  •  직전 학기에 정동아리 지위를 유지했던 동아리만 등록 가능",
+        },
+      ]}
+    />
+    <ClubButton
+      title="신규 등록"
+      buttonText={[
+        {
+          id: 1,
+          text: "  •  2개 정규학기 이상 가등록 지위를 유지한 동아리 등록 가능",
+        },
+        {
+          id: 2,
+          text: "  •  등록 취소 이후 3개 정규학기 이상 지나지 않은 단체 등록 가능",
+        },
+      ]}
+    />
+    <ClubButton
+      title="가등록"
+      buttonText={[
+        {
+          id: 1,
+          text: "  •  새로 동아리를 만들려는 학부 총학생회 정회원 등록 가능",
+        },
+        {
+          id: 2,
+          text: "  •  직전 학기에 가등록 지워를 유지한 동아리 등록 가능",
+        },
+      ]}
+    />
+  </FlexWrapper>
+);
+
+export default RegisterClubSampleFrame;
