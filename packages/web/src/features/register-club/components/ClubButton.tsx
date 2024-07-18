7bb456e3235ce8de698525d679b62c64eaf1cb75
chacha <chacha@sparcs.org>
2024-07-18 13:57:16 +0900

    feat: undo useless style and map buttonText

--- packages/web/src/features/register-club/components/ClubButton.tsx
+++ packages/web/src/features/register-club/components/ClubButton.tsx
@@ -4,11 +4,15 @@ import styled from "styled-components";
 
 import Card from "@sparcs-clubs/web/common/components/Card";
 import Typography from "@sparcs-clubs/web/common/components/Typography";
-import shadow from "@sparcs-clubs/web/styles/themes/shadow";
+
+interface ClubButtonTextProps {
+  id: int;
+  text: string;
+}
 
 interface ClubButtonProps {
   title: string;
-  buttonText?: string;
+  buttonText: ClubButtonTextProps;
   onClick?: VoidFunction;
 }
 
@@ -22,32 +26,20 @@ const ClubButtonInner = styled.div`
 
 const ClubButton: React.FC<ClubButtonProps> = ({
   title,
-  buttonText = "직전 학기에 정동아리 지위를 유지했던 동아리만 등록 가능",
+  buttonText,
   onClick = () => {},
 }) => (
-  <Card
-    padding="20px 24px"
-    gap={16}
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "flex-start",
-      flex: "1 0 0",
-      borderRadius: "8px",
-      background: "WHITE",
-      boxShadow: shadow,
-      width: "360px",
-    }}
-    onClick={onClick}
-  >
-    <Typography ff="PRETENDARD" fw="MEDIUM" fs={20} lh={24} color="BLACK">
+  <Card padding="20px 24px" gap={16} onClick={onClick}>
+    <Typography fw="MEDIUM" fs={20} lh={24}>
       {title}
     </Typography>
     <ClubButtonInner>
-      <Typography ff="PRETENDARD" fw="REGULAR" fs={12} lh={24} color="BLACK">
-        {buttonText}
-      </Typography>
+      {buttonText &&
+        buttonText.map(e => (
+          <Typography fw="REGULAR" fs={12} lh={24} key={e.id}>
+            {e.text}
+          </Typography>
+        ))}
     </ClubButtonInner>
   </Card>
 );
