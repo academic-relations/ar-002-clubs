75ddfc0822861f8da6ac89fa45e18439bd8c8bad
Woojeong Choi <gldeel@kaist.ac.kr>
2024-07-20 12:54:22 +0200

    feat: button connect

--- packages/web/src/app/manage-club/funding/[id]/edit/page.tsx
+++ packages/web/src/app/manage-club/funding/[id]/edit/page.tsx
@@ -2,10 +2,14 @@
 
 import React, { useState } from "react";
 
+import { useParams, useRouter } from "next/navigation";
+import { overlay } from "overlay-kit";
 import styled from "styled-components";
 
 import Button from "@sparcs-clubs/web/common/components/Button";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
+import Modal from "@sparcs-clubs/web/common/components/Modal";
+import ConfirmModalContent from "@sparcs-clubs/web/common/components/Modal/ConfirmModalContent";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
 import AddEvidenceFrame from "@sparcs-clubs/web/features/manage-club/funding/create/frame/AddEvidenceFrame";
 import BasicEvidenceFrame from "@sparcs-clubs/web/features/manage-club/funding/create/frame/BasicEvidenceFrame";
@@ -28,7 +32,30 @@ const EditFunding = () => {
   });
   const props = { funding, setFunding };
 
+  const router = useRouter();
+  const { id } = useParams<{ id: string }>();
+  const cancelClick = () => {
+    router.push(`/manage-club/funding/${id}`);
+  };
+
+  const openPeriodModal = () => {
+    overlay.open(({ isOpen, close }) => (
+      <Modal isOpen={isOpen}>
+        <ConfirmModalContent
+          onConfirm={() => {
+            // TODO: 저장 로직 넣기
+            close();
+          }}
+        >
+          수정이 완료되었습니다. <br />
+          확인을 누르면 신청 내역 화면으로 이동합니다.
+        </ConfirmModalContent>
+      </Modal>
+    ));
+  };
+
   return (
+    // TODO: RejectReasonToast
     <FlexWrapper direction="column" gap={60}>
       <PageHead
         items={[
@@ -42,8 +69,12 @@ const EditFunding = () => {
       <BasicEvidenceFrame {...props} />
       <AddEvidenceFrame {...props} />
       <ButtonWrapper>
-        <Button type="outlined">취소</Button>
-        <Button type="default">신청</Button>
+        <Button type="outlined" onClick={cancelClick}>
+          취소
+        </Button>
+        <Button type="default" onClick={openPeriodModal}>
+          저장
+        </Button>
       </ButtonWrapper>
     </FlexWrapper>
   );
