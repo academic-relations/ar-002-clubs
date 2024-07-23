a15923679a693d1585eec7ac7a0b8dd018c80b42
Woojeong Choi <gldeel@kaist.ac.kr>
2024-07-23 15:50:04 +0200

    chore: frame to frames

--- packages/web/src/app/manage-club/page.tsx
+++ packages/web/src/app/manage-club/page.tsx
@@ -4,10 +4,10 @@ import React from "react";
 
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
-import ActivityManageFrame from "@sparcs-clubs/web/features/manage-club/frame/ActivityManageFrame";
-import InfoManageFrame from "@sparcs-clubs/web/features/manage-club/frame/InfoManageFrame";
-import MembersManageFrame from "@sparcs-clubs/web/features/manage-club/frame/MembersManageFrame";
-import ServiceManageFrame from "@sparcs-clubs/web/features/manage-club/frame/ServiceManageFrame";
+import ActivityManageFrame from "@sparcs-clubs/web/features/manage-club/frames/ActivityManageFrame";
+import InfoManageFrame from "@sparcs-clubs/web/features/manage-club/frames/InfoManageFrame";
+import MembersManageFrame from "@sparcs-clubs/web/features/manage-club/frames/MembersManageFrame";
+import ServiceManageFrame from "@sparcs-clubs/web/features/manage-club/frames/ServiceManageFrame";
 
 const ManageClub: React.FC = () => (
   <FlexWrapper direction="column" gap={60}>
