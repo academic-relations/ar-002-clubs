9fc34afaa32fb88d8f0bb16ed0d6680d193fe815
andyye <63383967+0ev@users.noreply.github.com>
2024-07-23 13:17:36 +0900

    feat: sticky title

--- packages/web/src/common/components/RejectReasonToast.tsx
+++ packages/web/src/common/components/RejectReasonToast.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 import colors from "@sparcs-clubs/web/styles/themes/colors";
 
+import FlexWrapper from "./FlexWrapper";
 import Icon from "./Icon";
 
 import Typography from "./Typography";
@@ -32,28 +33,39 @@ const ForceBorderRadius = styled.div`
 const RejectReasonToastInner = styled.div`
   color: ${({ theme }) => theme.colors.BLACK};
   display: flex;
-  padding: 12px 16px;
+  flex-direction: column;
   align-items: flex-start;
   gap: 8px;
   width: 100%;
   max-height: 300px;
   overflow-y: auto;
-  .RejectReasonToast-content {
+
+  .RejectReasonToast-title {
+    padding: 12px 16px 0 16px;
+
+    position: sticky;
+    top: 0;
     display: flex;
+    align-items: center;
+    gap: 8px;
+    background: ${({ theme }) => theme.colors.RED[100]};
+    z-index: ${({ theme }) => theme.zIndices.toast + 1};
+  }
+
+  .RejectReasonToast-reasons {
+    display: flex;
+    width: 100%;
+    padding: 0 16px 12px 44px;
     flex-direction: column;
-    align-items: flex-start;
     gap: 8px;
     flex: 1 0 0;
-    align-self: stretch;
-    .RejectReasonToast-reasons {
-      display: flex;
-      width: 540px;
-      flex-direction: column;
-      justify-content: flex-end;
-      align-items: flex-start;
-      gap: 8px;
-      flex: 1 0 0;
-    }
+  }
+
+  .RejectReasonToast-sticky-title {
+    position: sticky;
+    top: 0;
+    background: ${({ theme }) => theme.colors.RED[100]};
+    z-index: ${({ theme }) => theme.zIndices.toast + 1};
   }
 `;
 
@@ -63,23 +75,23 @@ const RejectReasonToast: React.FC<RejectReasonToastProps> = ({
 }) => (
   <ForceBorderRadius>
     <RejectReasonToastInner>
-      <Icon type="error" size={20} color={colors.RED[600]} />
-      <div className="RejectReasonToast-content">
+      <div className="RejectReasonToast-title">
+        <Icon type="error" size={20} color={colors.RED[600]} />
         <Typography fs={16} lh={24} fw="MEDIUM">
           {title}
         </Typography>
-        <div className="RejectReasonToast-reasons">
-          {reasons.map(reason => (
-            <div key={reason.datetime}>
-              <Typography fs={14} lh={16} fw="REGULAR" color="GRAY.600">
-                {reason.datetime}
-              </Typography>
-              <Typography fs={16} lh={24} fw="REGULAR">
-                {reason.reason}
-              </Typography>
-            </div>
-          ))}
-        </div>
+      </div>
+      <div className="RejectReasonToast-reasons">
+        {reasons.map(reason => (
+          <FlexWrapper direction="column" gap={4} key={reason.datetime}>
+            <Typography fs={14} lh={16} fw="REGULAR" color="GRAY.600">
+              {reason.datetime}
+            </Typography>
+            <Typography fs={16} lh={24} fw="REGULAR">
+              {reason.reason}
+            </Typography>
+          </FlexWrapper>
+        ))}
       </div>
     </RejectReasonToastInner>
   </ForceBorderRadius>
