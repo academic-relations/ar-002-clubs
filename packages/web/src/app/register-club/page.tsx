905463044d34ca4eae8773a7ac19deb4c796a71a
chacha <chacha@sparcs.org>
2024-07-23 16:31:44 +0900

    feat: unify selectedType values

--- packages/web/src/app/register-club/page.tsx
+++ packages/web/src/app/register-club/page.tsx
@@ -21,10 +21,6 @@ const RegisterClub = () => {
     RegistrationType.unselected,
   );
 
-  // useEffect(() => {
-  //   console.log(selectedType);
-  // }, [selectedType]);
-
   return (
     <FlexWrapper direction="column" gap={60}>
       <PageHead
@@ -35,14 +31,14 @@ const RegisterClub = () => {
       <ClubButton
         title="재등록"
         buttonText="  •  직전 학기에 정동아리 지위를 유지했던 동아리만 등록 가능"
-        selected={selectedType === "renewalRegistration"}
+        selected={selectedType === RegistrationType.renewalRegistration}
         onClick={() => setSelectedType(RegistrationType.renewalRegistration)}
       />
       <ClubButton
         title="신규 등록"
         buttonText={`  •  2개 정규학기 이상 가등록 지위를 유지한 동아리 등록 가능  
   •  등록 취소 이후 3개 정규학기 이상 지나지 않은 단체 등록 가능`}
-        selected={selectedType === "promotionalRegistration"}
+        selected={selectedType === RegistrationType.promotionalRegistration}
         onClick={() =>
           setSelectedType(RegistrationType.promotionalRegistration)
         }
@@ -51,14 +47,16 @@ const RegisterClub = () => {
         title="가등록"
         buttonText={`  •  새로 동아리를 만들려는 학부 총학생회 정회원 등록 가능  
   •  직전 학기에 가등록 지위를 유지한 동아리 등록 가능`}
-        selected={selectedType === "provisionalRegistration"}
+        selected={selectedType === RegistrationType.provisionalRegistration}
         onClick={() =>
           setSelectedType(RegistrationType.provisionalRegistration)
         }
       />
       <IconButton
         buttonText="등록 신청"
-        type={selectedType === "unselected" ? "disabled" : "default"}
+        type={
+          selectedType === RegistrationType.unselected ? "disabled" : "default"
+        }
       />
     </FlexWrapper>
   );
