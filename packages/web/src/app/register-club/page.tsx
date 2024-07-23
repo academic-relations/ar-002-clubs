066718a7f17b8e0eff46a6b2e97dce929829f602
chacha <chacha@sparcs.org>
2024-07-23 16:19:31 +0900

    feat: add unselected to RegistrationType enum

--- packages/web/src/app/register-club/page.tsx
+++ packages/web/src/app/register-club/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
+import IconButton from "@sparcs-clubs/web/common/components/Forms/IconButton";
+import Info from "@sparcs-clubs/web/common/components/Info";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
 
 import ClubButton from "@sparcs-clubs/web/features/register-club/components/ClubButton";
@@ -12,15 +14,16 @@ const RegisterClub = () => {
     reRegister = "reRegister",
     newRegister = "newRegister",
     preRegister = "preRegister",
+    unselected = "unselected",
   }
 
   const [selectedType, setSelectedType] = useState<RegistrationType>(
-    RegistrationType.reRegister,
+    RegistrationType.unselected,
   );
 
-  useEffect(() => {
-    console.log(selectedType);
-  }, [selectedType]);
+  // useEffect(() => {
+  //   console.log(selectedType);
+  // }, [selectedType]);
 
   return (
     <FlexWrapper direction="column" gap={60}>
@@ -28,6 +31,7 @@ const RegisterClub = () => {
         items={[{ name: "동아리 등록", path: "/register-club" }]}
         title="동아리 등록"
       />
+      <Info text="현재는 2024년 봄학기 동아리 등록 기간입니다 (신청 마감 : 2024년 3월 10일 23:59)" />
       <ClubButton
         title="재등록"
         buttonText="  •  직전 학기에 정동아리 지위를 유지했던 동아리만 등록 가능"
@@ -47,6 +51,10 @@ const RegisterClub = () => {
   •  직전 학기에 가등록 지위를 유지한 동아리 등록 가능`}
         selected={selectedType === "preRegister"}
         onClick={() => setSelectedType(RegistrationType.preRegister)}
+      />
+      <IconButton
+        buttonText="등록 신청"
+        type={selectedType === "unselected" ? "disabled" : "default"}
       />
     </FlexWrapper>
   );
