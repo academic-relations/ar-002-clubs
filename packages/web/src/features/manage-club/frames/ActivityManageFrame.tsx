527cbbe5c695650e6e51b3b68c9c8151ba776239
Woojeong Choi <gldeel@kaist.ac.kr>
2024-07-23 15:58:20 +0200

    chore: component to components

--- packages/web/src/features/manage-club/frames/ActivityManageFrame.tsx
+++ packages/web/src/features/manage-club/frames/ActivityManageFrame.tsx
@@ -3,9 +3,8 @@ import React from "react";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
 import MoreDetailTitle from "@sparcs-clubs/web/common/components/MoreDetailTitle";
-import ActivityReportTable from "@sparcs-clubs/web/features/manage-club/component/ActivityReportTable";
-import FundingTable from "@sparcs-clubs/web/features/manage-club/component/FundingTable";
-import { ManageTablesWrapper } from "@sparcs-clubs/web/features/manage-club/component/ManageFrameWrapper";
+import ActivityReportTable from "@sparcs-clubs/web/features/manage-club/components/ActivityReportTable";
+import FundingTable from "@sparcs-clubs/web/features/manage-club/components/FundingTable";
 import {
   mockupManageFunding,
   mockupManageReport,
@@ -19,27 +18,24 @@ const ActivityManageFrame: React.FC = () => {
         title="동아리 활동"
         toggle={toggle}
         toggleHandler={() => setToggle(!toggle)}
-      />
-      {toggle && (
-        <ManageTablesWrapper>
-          <FlexWrapper direction="column" gap={20}>
-            <MoreDetailTitle
-              title="활동 보고서"
-              moreDetail="내역 더보기"
-              moreDetailPath="/manage-club/activity-report"
-            />
-            <ActivityReportTable activityList={mockupManageReport} />
-          </FlexWrapper>
-          <FlexWrapper direction="column" gap={20}>
-            <MoreDetailTitle
-              title="지원금"
-              moreDetail="내역 더보기"
-              moreDetailPath="/manage-club/funding"
-            />
-            <FundingTable fundingList={mockupManageFunding} />
-          </FlexWrapper>
-        </ManageTablesWrapper>
-      )}
+      >
+        <FlexWrapper direction="column" gap={20}>
+          <MoreDetailTitle
+            title="활동 보고서"
+            moreDetail="내역 더보기"
+            moreDetailPath="/manage-club/activity-report"
+          />
+          <ActivityReportTable activityList={mockupManageReport} />
+        </FlexWrapper>
+        <FlexWrapper direction="column" gap={20}>
+          <MoreDetailTitle
+            title="지원금"
+            moreDetail="내역 더보기"
+            moreDetailPath="/manage-club/funding"
+          />
+          <FundingTable fundingList={mockupManageFunding} />
+        </FlexWrapper>
+      </FoldableSectionTitle>
     </FlexWrapper>
   );
 };
--- packages/web/src/features/manage-club/frames/ServiceManageFrame.tsx
+++ packages/web/src/features/manage-club/frames/ServiceManageFrame.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
 import MoreDetailTitle from "@sparcs-clubs/web/common/components/MoreDetailTitle";
-import ActivityCertificateTable from "@sparcs-clubs/web/features/manage-club/component/ActivityCertificateTable";
-import CommonSpaceTable from "@sparcs-clubs/web/features/manage-club/component/CommonSpaceTable";
-import { ManageTablesWrapper } from "@sparcs-clubs/web/features/manage-club/component/ManageFrameWrapper";
-import PrintingTable from "@sparcs-clubs/web/features/manage-club/component/PrintingTable";
-import RentalTable from "@sparcs-clubs/web/features/manage-club/component/RentalTable";
+import ActivityCertificateTable from "@sparcs-clubs/web/features/manage-club/components/ActivityCertificateTable";
+import CommonSpaceTable from "@sparcs-clubs/web/features/manage-club/components/CommonSpaceTable";
+import PrintingTable from "@sparcs-clubs/web/features/manage-club/components/PrintingTable";
+import RentalTable from "@sparcs-clubs/web/features/manage-club/components/RentalTable";
+
 import {
   mockupManageAcf,
   mockupManageCms,
@@ -23,43 +23,40 @@ const ServiceManageFrame: React.FC = () => {
         title="서비스 신청"
         toggle={toggle}
         toggleHandler={() => setToggle(!toggle)}
-      />
-      {toggle && (
-        <ManageTablesWrapper>
-          <FlexWrapper direction="column" gap={20}>
-            <MoreDetailTitle
-              title="대여 사업"
-              moreDetail="내역 더보기"
-              moreDetailPath="/manage-club/rental-business"
-            />
-            <RentalTable rentalList={mockupManageRental} />
-          </FlexWrapper>
-          <FlexWrapper direction="column" gap={20}>
-            <MoreDetailTitle
-              title="홍보물 인쇄"
-              moreDetail="내역 더보기"
-              moreDetailPath="/manage-club/printing-business"
-            />
-            <PrintingTable printingList={mockupManagePrint} />
-          </FlexWrapper>
-          <FlexWrapper direction="column" gap={20}>
-            <MoreDetailTitle
-              title="활동확인서 발급"
-              moreDetail="내역 더보기"
-              moreDetailPath="/manage-club/activity-certificate"
-            />
-            <ActivityCertificateTable certificateList={mockupManageAcf} />
-          </FlexWrapper>
-          <FlexWrapper direction="column" gap={20}>
-            <MoreDetailTitle
-              title="공용공간 비정기사용"
-              moreDetail="내역 더보기"
-              moreDetailPath="/manage-club/common-space"
-            />
-            <CommonSpaceTable spaceList={mockupManageCms} />
-          </FlexWrapper>
-        </ManageTablesWrapper>
-      )}
+      >
+        <FlexWrapper direction="column" gap={20}>
+          <MoreDetailTitle
+            title="대여 사업"
+            moreDetail="내역 더보기"
+            moreDetailPath="/manage-club/rental-business"
+          />
+          <RentalTable rentalList={mockupManageRental} />
+        </FlexWrapper>
+        <FlexWrapper direction="column" gap={20}>
+          <MoreDetailTitle
+            title="홍보물 인쇄"
+            moreDetail="내역 더보기"
+            moreDetailPath="/manage-club/printing-business"
+          />
+          <PrintingTable printingList={mockupManagePrint} />
+        </FlexWrapper>
+        <FlexWrapper direction="column" gap={20}>
+          <MoreDetailTitle
+            title="활동확인서 발급"
+            moreDetail="내역 더보기"
+            moreDetailPath="/manage-club/activity-certificate"
+          />
+          <ActivityCertificateTable certificateList={mockupManageAcf} />
+        </FlexWrapper>
+        <FlexWrapper direction="column" gap={20}>
+          <MoreDetailTitle
+            title="공용공간 비정기사용"
+            moreDetail="내역 더보기"
+            moreDetailPath="/manage-club/common-space"
+          />
+          <CommonSpaceTable spaceList={mockupManageCms} />
+        </FlexWrapper>
+      </FoldableSectionTitle>
     </FlexWrapper>
   );
 };
