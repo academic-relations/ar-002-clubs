818a96d9721f9cc26ac472e1aec9247674e831e0
chacha <chacha@sparcs.org>
2024-07-23 16:29:06 +0900

    feat: change enum names to official

--- packages/web/src/app/register-club/page.tsx
+++ packages/web/src/app/register-club/page.tsx
@@ -11,9 +11,9 @@ import ClubButton from "@sparcs-clubs/web/features/register-club/components/Club
 
 const RegisterClub = () => {
   enum RegistrationType {
-    reRegister = "reRegister",
-    newRegister = "newRegister",
-    preRegister = "preRegister",
+    renewalRegistration = "renewalRegistration",
+    promotionalRegistration = "promotionalRegistration",
+    provisionalRegistration = "provisionalRegistration",
     unselected = "unselected",
   }
 
@@ -35,22 +35,26 @@ const RegisterClub = () => {
       <ClubButton
         title="재등록"
         buttonText="  •  직전 학기에 정동아리 지위를 유지했던 동아리만 등록 가능"
-        selected={selectedType === "reRegister"}
-        onClick={() => setSelectedType(RegistrationType.reRegister)}
+        selected={selectedType === "renewalRegistration"}
+        onClick={() => setSelectedType(RegistrationType.renewalRegistration)}
       />
       <ClubButton
         title="신규 등록"
         buttonText={`  •  2개 정규학기 이상 가등록 지위를 유지한 동아리 등록 가능  
   •  등록 취소 이후 3개 정규학기 이상 지나지 않은 단체 등록 가능`}
-        selected={selectedType === "newRegister"}
-        onClick={() => setSelectedType(RegistrationType.newRegister)}
+        selected={selectedType === "promotionalRegistration"}
+        onClick={() =>
+          setSelectedType(RegistrationType.promotionalRegistration)
+        }
       />
       <ClubButton
         title="가등록"
         buttonText={`  •  새로 동아리를 만들려는 학부 총학생회 정회원 등록 가능  
   •  직전 학기에 가등록 지위를 유지한 동아리 등록 가능`}
-        selected={selectedType === "preRegister"}
-        onClick={() => setSelectedType(RegistrationType.preRegister)}
+        selected={selectedType === "provisionalRegistration"}
+        onClick={() =>
+          setSelectedType(RegistrationType.provisionalRegistration)
+        }
       />
       <IconButton
         buttonText="등록 신청"
