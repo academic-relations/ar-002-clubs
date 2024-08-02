5270ed5597d26501d401d2f5581e7b1bca160df5
Ryan Lee <ryan@sparcs.org>
2024-08-02 18:41:15 +0900

    feat: implement president card

--- packages/web/src/features/manage-devision/frames/DivisionInfoFrame.tsx
+++ packages/web/src/features/manage-devision/frames/DivisionInfoFrame.tsx
@@ -1,14 +1,14 @@
-import Card from "@sparcs-clubs/web/common/components/Card";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
-import BasicInfoFrame from "@sparcs-clubs/web/features/manage-devision/components/BasicInfoFrame";
+import ChangeDivisionPresidentCard from "@sparcs-clubs/web/features/manage-devision/components/ChangeDivisionPresidentCard";
+import DivisionInformationCard from "@sparcs-clubs/web/features/manage-devision/components/DivisionInformationCard";
 
 const DivisionInfoFrame: React.FC = () => (
   <FlexWrapper gap={40} direction="column">
     <FoldableSectionTitle title="분과 정보">
       <FlexWrapper gap={20} direction="row">
-        <BasicInfoFrame />
-        <Card outline padding="32px" style={{ flex: 1 }} />
+        <DivisionInformationCard />
+        <ChangeDivisionPresidentCard />
       </FlexWrapper>
     </FoldableSectionTitle>
   </FlexWrapper>
