3cac16a308b3fee6e3ceaed0c418b3b27fbecbed
YINGMEI <j.yeongmee@gmail.com>
2024-08-04 04:08:39 +0900

    feat: add delete modal to MeetingDetailFrame

--- packages/web/src/features/meeting/frames/MeetingDetailFrame.tsx
+++ packages/web/src/features/meeting/frames/MeetingDetailFrame.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+import Link from "next/link";
+
+import { useRouter } from "next/navigation";
+import { overlay } from "overlay-kit";
 import styled from "styled-components";
 
 import Button from "@sparcs-clubs/web/common/components/Button";
 import Card from "@sparcs-clubs/web/common/components/Card";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
+import Modal from "@sparcs-clubs/web/common/components/Modal";
+import CancellableModalContent from "@sparcs-clubs/web/common/components/Modal/CancellableModalContent";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
 
 import Typography from "@sparcs-clubs/web/common/components/Typography";
@@ -17,33 +23,57 @@ const RowStretchWrapper = styled.div`
   align-items: center;
   align-self: stretch;
 `;
-// TODO. api 연결
-const MeetingDetailFrame: React.FC = () => (
-  <FlexWrapper direction="column" gap={60}>
-    <PageHead
-      items={[{ name: "의결기구", path: `/meeting` }]}
-      title={mockupData.title}
-      enableLast
-    />
-    <Card outline>
-      <Typography
-        ff="PRETENDARD"
-        fs={16}
-        fw="REGULAR"
-        lh={20}
-        style={{ whiteSpace: "pre-line" }}
-      >
-        {mockupData.content}
-      </Typography>
-    </Card>
-    <RowStretchWrapper>
-      <Button type="default">목록으로 돌아가기</Button>
-      <FlexWrapper direction="row" gap={10}>
-        <Button type="default">삭제</Button>
-        <Button type="default">수정</Button>
-      </FlexWrapper>
-    </RowStretchWrapper>
-  </FlexWrapper>
-);
+
+const MeetingDetailFrame: React.FC = () => {
+  const router = useRouter();
+
+  const deleteHandler = () => {
+    overlay.open(({ isOpen, close }) => (
+      <Modal isOpen={isOpen}>
+        <CancellableModalContent
+          onConfirm={() => {
+            // TODO. 삭제 요청 api 연결
+            close();
+            router.back();
+          }}
+          onClose={close}
+        >
+          공고를 삭제하면 복구할 수 없습니다.
+          <br />
+          ㄱㅊ?
+        </CancellableModalContent>
+      </Modal>
+    ));
+  };
+
+  // TODO. 공고 상세조회 api 연결
+  const data = mockupData;
+
+  return (
+    <FlexWrapper direction="column" gap={60}>
+      <PageHead
+        items={[{ name: "의결기구", path: `/meeting` }]}
+        title={data.title}
+        enableLast
+      />
+      <Card outline>
+        <Typography fs={16} lh={20} style={{ whiteSpace: "pre-line" }}>
+          {data.content}
+        </Typography>
+      </Card>
+      <RowStretchWrapper>
+        <Link href="/meeting">
+          <Button type="default">목록으로 돌아가기</Button>
+        </Link>
+        <FlexWrapper direction="row" gap={10}>
+          <Button type="default" onClick={deleteHandler}>
+            삭제
+          </Button>
+          <Button type="default">수정</Button>
+        </FlexWrapper>
+      </RowStretchWrapper>
+    </FlexWrapper>
+  );
+};
 
 export default MeetingDetailFrame;
