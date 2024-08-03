73d53a6307cf2920bb2b1f95796571c4d038d5c9
Ryan Lee <ryan@sparcs.org>
2024-08-03 15:36:39 +0900

    fix: use common component for toggle list

--- packages/web/src/features/manage-division/components/DivisionInformationCard.tsx
+++ packages/web/src/features/manage-division/components/DivisionInformationCard.tsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
-
 import styled from "styled-components";
 
 import Card from "@sparcs-clubs/web/common/components/Card";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
-import Icon from "@sparcs-clubs/web/common/components/Icon";
 import Tag from "@sparcs-clubs/web/common/components/Tag";
+import Toggle from "@sparcs-clubs/web/common/components/Toggle";
 import Typography from "@sparcs-clubs/web/common/components/Typography";
 
 const ClubListWrapper = styled(FlexWrapper)`
   max-height: 300px;
   overflow-y: scroll;
+  width: 100%;
 `;
 
 const DivisionInformationCard: React.FC = () => {
-  const [showClubList, setShowClubList] = useState<boolean>(false);
   const mockClubList = Array.from({ length: 20 }).map((_, i) => ({
     name: "술박스",
     id: i,
@@ -31,31 +29,15 @@ const DivisionInformationCard: React.FC = () => {
         </Typography>
         <Tag color="PURPLE">생활체육</Tag>
       </FlexWrapper>
-      <FlexWrapper
-        gap={8}
-        direction="row"
-        onClick={() => setShowClubList(!showClubList)}
-      >
-        {showClubList ? (
-          <Icon type="keyboard_arrow_down" size={20} />
-        ) : (
-          <Icon type="keyboard_arrow_right" size={20} />
-        )}
-        <FlexWrapper gap={16} direction="column" style={{ flex: 1 }}>
-          <Typography fw="MEDIUM" fs={16} lh={20}>
-            {`동아리 목록 (${mockClubList.length}개)`}
-          </Typography>
-          {showClubList && (
-            <ClubListWrapper direction="column" gap={12}>
-              {mockClubList.map((club, _) => (
-                <Typography fs={16} lh={20} key={club.id}>
-                  {club.name}
-                </Typography>
-              ))}
-            </ClubListWrapper>
-          )}
-        </FlexWrapper>
-      </FlexWrapper>
+      <Toggle label={`동아리 목록 (${mockClubList.length}개)`}>
+        <ClubListWrapper direction="column" gap={12}>
+          {mockClubList.map((club, _) => (
+            <Typography fs={16} lh={20} key={club.id}>
+              {club.name}
+            </Typography>
+          ))}
+        </ClubListWrapper>
+      </Toggle>
     </Card>
   );
 };
