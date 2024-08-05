f41c8e22f7893319b3f7b60d31a97a04727024a5
Woojeong Choi <gldeel@kaist.ac.kr>
2024-08-05 21:33:37 +0900

    feat: add jsdoc

--- packages/api/src/feature/user/service/user.public.service.ts
+++ packages/api/src/feature/user/service/user.public.service.ts
@@ -23,6 +23,10 @@ export default class UserPublicService {
     return students[0];
   }
 
+  /**
+   * StudentTId를 통해 학생의 Id를 가져옵니다.
+   * 만약 매치되는 id가 존재하지 않을 경우 undefined를 리턴합니다.
+   * */
   async getStudentByTId(studentT: { id: number }) {
     const studentIds = await this.studentRepository.selectStudentIdByStudentTId(
       studentT.id,
