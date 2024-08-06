99b20d3a4e9fbf2858616cdb0280b471b5be8bc9
useruseruse <eliza1102@kaist.ac.kr>
2024-08-06 22:41:25 +0900

    fix: remove socket module

--- packages/api/src/feature/socket/socket.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { MySql2Database } from "drizzle-orm/mysql2";
-
-import { DrizzleAsyncProvider } from "src/drizzle/drizzle.provider";
-
-@Injectable()
-export class SocketRepository {
-  constructor(@Inject(DrizzleAsyncProvider) private db: MySql2Database) {}
-}
