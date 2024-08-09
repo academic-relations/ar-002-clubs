d9ca509c5609588d456d86b90401732ccbf07c69
Ryan Lee <ryan@sparcs.org>
2024-08-09 16:36:10 +0900

    feat: add new components to impl

---
+++ packages/web/src/app/meeting/agenda/add/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
+import PageHead from "@sparcs-clubs/web/common/components/PageHead";
+import NewAgendaFrame from "@sparcs-clubs/web/features/meeting/agenda/add/frames/NewAgendaFrame";
+
+const AgendaAddPage: React.FC = () => (
+  <FlexWrapper gap={60} direction="column">
+    <PageHead
+      items={[
+        { name: "의결기구", path: "/meeting" }, // TODO: figma 나오는대로 맞춰서
+        { name: "안건지", path: "/meeting/agenda" },
+        { name: "작성", path: "/meeting/agenda/add" },
+      ]}
+      title="안건지 작성하기"
+    />
+    <NewAgendaFrame />
+  </FlexWrapper>
+);
+
+export default AgendaAddPage;
---
+++ packages/web/src/features/meeting/agenda/add/components/AgendaBlock.tsx
@@ -0,0 +1,4 @@
+// TODO: 구현하기
+const AgendaBlock: React.FC = () => <>TODO</>;
+
+export default AgendaBlock;
