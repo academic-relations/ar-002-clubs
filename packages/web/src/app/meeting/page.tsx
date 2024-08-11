c2161ad2c901488d928c113bb8e96f3c2a827292
chacha <chacha@sparcs.org>
2024-08-12 00:02:24 +0900

    feat: add more mock data

--- packages/web/src/app/meeting/page.tsx
+++ packages/web/src/app/meeting/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styled from "styled-components";
 
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
-import { MeetingNoticeItem } from "@sparcs-clubs/web/features/meeting/components/MeetingNoticeItem";
+import Pagination from "@sparcs-clubs/web/common/components/Pagination";
+import {
+  MeetingNoticeItem,
+  MeetingNoticeTypeEnum,
+} from "@sparcs-clubs/web/features/meeting/components/MeetingNoticeItem";
 import mockUpMeetingNotice from "@sparcs-clubs/web/features/meeting/services/_mock/mockupMeetingNotice";
 
 const MeetingNoticeListWrapper = styled.div`
@@ -18,23 +22,59 @@ const MeetingNoticeListWrapper = styled.div`
   border-top: 1px solid ${({ theme }) => theme.colors.GRAY[200]};
 `;
 
-const MeetingMainFrame: React.FC = () => (
-  <FlexWrapper gap={60} direction="column">
-    <PageHead
-      items={[{ name: "전체 회의", path: "/meeting" }]}
-      title="전체 회의"
-    />
-    <MeetingNoticeListWrapper>
-      {mockUpMeetingNotice.items.map(e => (
-        <MeetingNoticeItem
-          key={e.id}
-          tag={e.tag}
-          title={e.title}
-          date={e.date}
+const ListWithPaginationWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  flex: 1 0 0;
+  align-self: stretch;
+`;
+
+interface MockUpDataType {
+  id: number;
+  tag: MeetingNoticeTypeEnum;
+  title: string;
+  date: Date;
+}
+
+const MeetingMainFrame: React.FC = () => {
+  const [page, setPage] = useState<number>(1);
+  const [mockUpData, setMockUpData] = useState<MockUpDataType[]>(
+    mockUpMeetingNotice.items.slice(0, 12),
+  );
+
+  useEffect(() => {
+    setMockUpData(mockUpMeetingNotice.items.slice((page - 1) * 12, page * 12));
+  }, [page]);
+
+  return (
+    <FlexWrapper gap={60} direction="column">
+      <PageHead
+        items={[{ name: "전체 회의", path: "/meeting" }]}
+        title="전체 회의"
+      />
+      <ListWithPaginationWrapper>
+        <MeetingNoticeListWrapper>
+          {mockUpData.map(e => (
+            <MeetingNoticeItem
+              key={e.id}
+              tag={e.tag}
+              title={e.title}
+              date={e.date}
+            />
+          ))}
+        </MeetingNoticeListWrapper>
+        <Pagination
+          totalPage={Math.ceil(mockUpMeetingNotice.items.length / 12)}
+          currentPage={page}
+          limit={10}
+          setPage={setPage}
         />
-      ))}
-    </MeetingNoticeListWrapper>
-  </FlexWrapper>
-);
+      </ListWithPaginationWrapper>
+    </FlexWrapper>
+  );
+};
 
 export default MeetingMainFrame;
