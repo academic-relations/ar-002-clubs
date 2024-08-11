786216c169ab22de15e305951b61ca9f1bb76c22
Jiuuung <kjg682534@kaist.ac.kr>
2024-08-11 09:13:09 +0900

    fix: act008 interface fix

--- packages/interface/src/api/activity/endpoint/apiAct008.ts
+++ packages/interface/src/api/activity/endpoint/apiAct008.ts
@@ -1,6 +1,8 @@
 import { HttpStatusCode } from "axios";
 import { z } from "zod";
 
+import { ActivityTypeEnum } from "@sparcs-clubs/interface/common/enum/activity.enum";
+
 /**
  * @version v0.1
  * @description 활동보고서의 활동을 수정합니다.(가동아리 동아리 신규등록 신청을 위한 얘외적 호라동보고서 수정 기능입니다.)
@@ -16,7 +18,30 @@ const requestParam = z.object({
 
 const requestQuery = z.object({});
 
-const resquestBody = z.object({});
+const resquestBody = z.object({
+  name: z.coerce.string().max(255),
+  activityTypeEnumId: z.nativeEnum(ActivityTypeEnum),
+  durations: z.array(
+    z.object({
+      startTerm: z.coerce.date(),
+      endTerm: z.coerce.date(),
+    }),
+  ),
+  location: z.coerce.string().max(255),
+  purpose: z.coerce.string(),
+  detail: z.coerce.string(),
+  evidence: z.coerce.string(),
+  evidenceFiles: z.array(
+    z.object({
+      uid: z.coerce.string().max(255),
+    }),
+  ),
+  participants: z.array(
+    z.object({
+      studentId: z.coerce.number().int().min(1),
+    }),
+  ),
+});
 
 const responseBodyMap = {
   [HttpStatusCode.Ok]: z.object({}),
