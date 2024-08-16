7411c2871609b0417170bc2021407e04d2cfac90
Woojeong Choi <gldeel@kaist.ac.kr>
2024-08-16 17:16:24 +0900

    fix: delete coerce for string

--- packages/interface/src/api/funding/apiFnd003.ts
+++ packages/interface/src/api/funding/apiFnd003.ts
@@ -24,54 +24,54 @@ const requestQuery = z.object({});
 const requestPreBody = z.object({
   clubId: z.coerce.number().int().min(1),
   purposeId: z.coerce.number().int().min(1).optional(),
-  name: z.coerce.string().max(255),
+  name: z.string().max(255).min(1),
   expenditureDate: z.coerce.date(),
   expenditureAmount: z.coerce.number().int().min(1),
 
   tradeEvidenceFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
   tradeDetailFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
-  tradeDetailExplanation: z.coerce.string(),
+  tradeDetailExplanation: z.string().min(1),
 
-  clubSuppliesName: z.coerce.string().max(255).optional(),
+  clubSuppliesName: z.string().max(255).optional(),
   clubSuppliesEvidenceEnumId: z.nativeEnum(FixtureEvidenceEnum).optional(),
   clubSuppliesClassEnumId: z.nativeEnum(FixtureClassEnum).optional(),
-  clubSuppliesPurpose: z.coerce.string().optional(),
+  clubSuppliesPurpose: z.string().optional(),
   clubSuppliesImageFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
-  clubSuppliesSoftwareEvidence: z.coerce.string().optional(),
+  clubSuppliesSoftwareEvidence: z.string().optional(),
   clubSuppliesSoftwareEvidenceFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
   numberOfClubSupplies: z.coerce.number().int().min(1).optional(),
   priceOfClubSupplies: z.coerce.number().int().min(1).optional(),
 
   isFixture: z.coerce.boolean(),
-  fixtureName: z.coerce.string().max(255).optional(),
+  fixtureName: z.string().max(255).optional(),
   fixtureEvidenceEnumId: z.nativeEnum(FixtureEvidenceEnum).optional(),
   fixtureClassEnumId: z.nativeEnum(FixtureClassEnum).optional(),
-  fixturePurpose: z.coerce.string().optional(),
+  fixturePurpose: z.string().optional(),
   fixtureImageFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
-  fixtureSoftwareEvidence: z.coerce.string().optional(),
+  fixtureSoftwareEvidence: z.string().optional(),
   fixtureSoftwareEvidenceFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
   numberOfFixture: z.coerce.number().int().min(1).optional(),
@@ -79,20 +79,20 @@ const requestPreBody = z.object({
 
   isTransportation: z.coerce.boolean(),
   transportationEnumId: z.nativeEnum(TransportationEnum).optional(),
-  origin: z.coerce.string().max(255).optional(),
-  destination: z.coerce.string().max(255).optional(),
-  purposeOfTransportation: z.coerce.string().optional(),
-  placeValidity: z.coerce.string().optional(),
+  origin: z.string().max(255).optional(),
+  destination: z.string().max(255).optional(),
+  purposeOfTransportation: z.string().optional(),
+  placeValidity: z.string().optional(),
   transportationPassengers: z.array(
     z.object({
-      studentNumber: z.coerce.string(),
+      studentNumber: z.string(),
     }),
   ),
 
   isNonCorporateTransaction: z.coerce.boolean(),
-  traderName: z.coerce.string().max(255).optional(),
-  traderAccountNumber: z.coerce.string().max(255).optional(),
-  wasteExplanation: z.coerce.string().optional(),
+  traderName: z.string().max(255).optional(),
+  traderAccountNumber: z.string().max(255).optional(),
+  wasteExplanation: z.string().optional(),
 
   isFoodExpense: z.coerce.boolean(),
   isLaborContract: z.coerce.boolean(),
@@ -102,47 +102,47 @@ const requestPreBody = z.object({
   isJointExpense: z.coerce.boolean(),
   isEtcExpense: z.coerce.boolean(),
 
-  foodExpenseExplanation: z.coerce.string().optional(),
-  laborContractExplanation: z.coerce.string().optional(),
-  externalEventParticipationFeeExplanation: z.coerce.string().optional(),
-  publicationExplanation: z.coerce.string().optional(),
-  profitMakingActivityExplanation: z.coerce.string().optional(),
-  jointExpenseExplanation: z.coerce.string().optional(),
-  etcExpenseExplanation: z.coerce.string().optional(),
+  foodExpenseExplanation: z.string().optional(),
+  laborContractExplanation: z.string().optional(),
+  externalEventParticipationFeeExplanation: z.string().optional(),
+  publicationExplanation: z.string().optional(),
+  profitMakingActivityExplanation: z.string().optional(),
+  jointExpenseExplanation: z.string().optional(),
+  etcExpenseExplanation: z.string().optional(),
 
   foodExpenseFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
   laborContractFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
   externalEventParticipationFeeFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
   publicationFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
   profitMakingActivityFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
   jointExpenseFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
   etcExpenseFiles: z.array(
     z.object({
-      fileId: z.coerce.string().max(128),
+      fileId: z.string().max(128).min(1),
     }),
   ),
 });
