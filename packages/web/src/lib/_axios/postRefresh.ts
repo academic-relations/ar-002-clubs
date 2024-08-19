efeea538dc4047a93dc239913200ce21637c121e
Woojeong Choi <gldeel@kaist.ac.kr>
2024-08-20 01:40:01 +0900

    fix: move error interceptor

--- packages/web/src/lib/_axios/postRefresh.ts
+++ packages/web/src/lib/_axios/postRefresh.ts
@@ -1,11 +1,14 @@
 import apiAut002, {
   ApiAut002ResponseOk,
 } from "@sparcs-clubs/interface/api/auth/endpoint/apiAut002";
-
 import axios from "axios";
 
+import { env } from "@sparcs-clubs/web/env";
+
 const postRefresh = async (): Promise<ApiAut002ResponseOk> => {
-  const { data, status } = await axios.post(apiAut002.url(), {});
+  const instance = axios.create({ baseURL: env.NEXT_PUBLIC_API_URL });
+
+  const { data, status } = await instance.post(apiAut002.url(), {});
 
   switch (status) {
     case 200:
