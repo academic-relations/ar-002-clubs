25daa878e64cf16a497969ecfc2bf1b6b113914d
Woojeong Choi <gldeel@kaist.ac.kr>
2024-08-21 00:03:37 +0900

    fix: fix refresh

--- packages/web/src/lib/_axios/postRefresh.ts
+++ packages/web/src/lib/_axios/postRefresh.ts
@@ -1,18 +1,24 @@
 import apiAut002, {
-  ApiAut002ResponseOk,
+  ApiAut002ResponseCreated,
 } from "@sparcs-clubs/interface/api/auth/endpoint/apiAut002";
 import axios from "axios";
 
 import { env } from "@sparcs-clubs/web/env";
 
-const postRefresh = async (): Promise<ApiAut002ResponseOk> => {
+const postRefresh = async (): Promise<ApiAut002ResponseCreated> => {
   const instance = axios.create({ baseURL: env.NEXT_PUBLIC_API_URL });
 
-  const { data, status } = await instance.post(apiAut002.url(), {});
+  const { data, status } = await instance.post(
+    apiAut002.url(),
+    {},
+    {
+      withCredentials: true,
+    },
+  );
 
   switch (status) {
-    case 200:
-      return apiAut002.responseBodyMap[200].parse(data);
+    case 201:
+      return apiAut002.responseBodyMap[201].parse(data);
     default:
       throw new Error("Unexpected API response");
   }
