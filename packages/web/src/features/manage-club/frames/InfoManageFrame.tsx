9b475afb53bb8c813d94273bbfdde1396d06d8df
chacha <chacha@sparcs.org>
2024-08-20 21:53:14 +0900

    feat: get useGetClubInfo and use in AsyncBoundary

--- packages/web/src/features/manage-club/frames/InfoManageFrame.tsx
+++ packages/web/src/features/manage-club/frames/InfoManageFrame.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import styled from "styled-components";
 
+import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import Button from "@sparcs-clubs/web/common/components/Button";
 import Card from "@sparcs-clubs/web/common/components/Card";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
@@ -10,6 +11,7 @@ import TextInput from "@sparcs-clubs/web/common/components/Forms/TextInput";
 import Typography from "@sparcs-clubs/web/common/components/Typography";
 import ChangeRepresentativeCard from "@sparcs-clubs/web/features/manage-club/components/ChangeRepresentativeCard";
 import { mockClubDescription } from "@sparcs-clubs/web/features/manage-club/services/_mock/mockManageClub";
+import { useGetClubInfo } from "@sparcs-clubs/web/features/manage-club/services/getClubInfo";
 
 const InfoWrapper = styled.div`
   display: flex;
@@ -22,9 +24,10 @@ const InfoWrapper = styled.div`
 `;
 
 const InfoManageFrame: React.FC = () => {
-  const [description, setDescription] = useState<string>(
-    mockClubDescription.description,
-  );
+  const clubId = 1;
+  const { data, isLoading, isError } = useGetClubInfo({ clubId });
+  const [description, setDescription] = useState<string>(data?.description);
+
   const [password, setPassword] = useState<string>(
     mockClubDescription.roomPassword,
   );
@@ -64,14 +67,16 @@ const InfoManageFrame: React.FC = () => {
             <Typography fw="MEDIUM" fs={20} lh={24}>
               기본 정보
             </Typography>
-            <TextInput
-              label="동아리 설명"
-              placeholder="동아리 설명을 입력하세요"
-              area
-              value={description}
-              handleChange={setDescription}
-              errorMessage={errorDescription}
-            />
+            <AsyncBoundary isLoading={isLoading} isError={isError}>
+              <TextInput
+                label="동아리 설명"
+                placeholder="동아리 설명을 입력하세요"
+                area
+                value={description}
+                handleChange={setDescription}
+                errorMessage={errorDescription}
+              />
+            </AsyncBoundary>
             <TextInput
               label="동아리방 비밀번호"
               placeholder="동아리방 비밀번호를 입력하세요"
