cd62a1040cb45791a942ae63b399baea27cc70e5
casio@sparcs.org <casio@sparcs.org>
2024-08-20 22:10:58 +0900

    chore: fix mock logic

--- packages/web/src/features/executive/_mock/mockMemberRegistration.ts
+++ packages/web/src/features/executive/_mock/mockMemberRegistration.ts
@@ -21,22 +21,21 @@ const clubType = [ClubTypeEnum.Provisional, ClubTypeEnum.Regular];
 
 const isClubPermanent = [true, false];
 const items = Array.from({ length: 120 }, (_, index) => ({
-  division: divisionType[Math.floor(Math.random() * divisionType.length)], // 랜덤 상태 선택
+  division: divisionType[index % divisionType.length], // 랜덤 상태 선택
   id: index + 1,
-  type: clubType[Math.floor(Math.random() * clubType.length)],
+  type: clubType[index % clubType.length],
   approvedAll: 120,
   approvedRegular: 90,
   registeredAll: 200,
   registeredRegular: 100,
   clubName: "술박스",
-  isPermanent:
-    isClubPermanent[Math.floor(Math.random() * isClubPermanent.length)],
+  isPermanent: isClubPermanent[index % isClubPermanent.length],
 }));
 
 const mockupRegistrationMember = {
-  total: items.length, // 총 아이템 수
+  total: items.length,
   items,
-  offset: 0, // 데이터 오프셋
+  offset: 0,
 };
 
 export default mockupRegistrationMember;
