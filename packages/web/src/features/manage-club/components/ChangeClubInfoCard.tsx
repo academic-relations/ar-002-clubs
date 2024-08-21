2db57d28b77896c59c48b7a37413e4b386b95ca9
chacha <chacha@sparcs.org>
2024-08-21 20:46:40 +0900

    feat: connect api Clb004 to ChangeClubInfoCard

--- packages/web/src/features/manage-club/components/ChangeClubInfoCard.tsx
+++ packages/web/src/features/manage-club/components/ChangeClubInfoCard.tsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from "react";
 
+import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import Button from "@sparcs-clubs/web/common/components/Button";
 import Card from "@sparcs-clubs/web/common/components/Card";
 import TextInput from "@sparcs-clubs/web/common/components/Forms/TextInput";
 import Typography from "@sparcs-clubs/web/common/components/Typography";
 
 import { mockClubDescription } from "@sparcs-clubs/web/features/manage-club/services/_mock/mockManageClub";
+import { useGetClubInfo } from "@sparcs-clubs/web/features/manage-club/services/getClubInfo";
 
 const ChangeClubInfoCard = () => {
-  const [description, setDescription] = useState<string>(
-    mockClubDescription.description,
-  );
-  const [password, setPassword] = useState<string>(
-    mockClubDescription.roomPassword,
-  );
+  const { data, isLoading, isError } = useGetClubInfo({ clubId: 1 });
+  const [description, setDescription] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [errorDescription, setErrorDescription] = useState<string>("");
   const [errorPassword, setErrorPassword] = useState<string>("");
 
@@ -24,6 +23,13 @@ const ChangeClubInfoCard = () => {
     errorPassword !== ""
       ? "disabled"
       : "default";
+
+  useEffect(() => {
+    if (!isLoading && data?.description && data?.roomPassword) {
+      setDescription(data.description);
+      setPassword(data.roomPassword);
+    }
+  }, [isLoading, data?.description, data?.roomPassword]);
 
   useEffect(() => {
     if (description === "") {
@@ -47,21 +53,23 @@ const ChangeClubInfoCard = () => {
       <Typography fw="MEDIUM" fs={20} lh={24}>
         기본 정보
       </Typography>
-      <TextInput
-        label="동아리 설명"
-        placeholder="동아리 설명을 입력하세요"
-        area
-        value={description}
-        handleChange={setDescription}
-        errorMessage={errorDescription}
-      />
-      <TextInput
-        label="동아리방 비밀번호"
-        placeholder="동아리방 비밀번호를 입력하세요"
-        value={password}
-        handleChange={setPassword}
-        errorMessage={errorPassword}
-      />
+      <AsyncBoundary isLoading={isLoading} isError={isError}>
+        <TextInput
+          label="동아리 설명"
+          placeholder="동아리 설명을 입력하세요"
+          area
+          value={description}
+          handleChange={setDescription}
+          errorMessage={errorDescription}
+        />
+        <TextInput
+          label="동아리방 비밀번호"
+          placeholder="동아리방 비밀번호를 입력하세요"
+          value={password}
+          handleChange={setPassword}
+          errorMessage={errorPassword}
+        />
+      </AsyncBoundary>
       <Button
         type={buttonType}
         style={{ width: "max-content", alignSelf: "flex-end" }}
