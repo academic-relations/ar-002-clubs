ff3e1475dd27f7f6179f1ff2f0595fc670e27ce8
malloc3141 <malloc_3141592@naver.com>
2024-08-21 16:42:58 +0900

    feat: impl /manage-club for delegate

--- packages/web/src/app/manage-club/page.tsx
+++ packages/web/src/app/manage-club/page.tsx
@@ -3,23 +3,37 @@
 import React from "react";
 
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
+import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
+import ChangeClubInfoCard from "@sparcs-clubs/web/features/manage-club/components/ChangeClubInfoCard";
 import ActivityManageFrame from "@sparcs-clubs/web/features/manage-club/frames/ActivityManageFrame";
 import InfoManageFrame from "@sparcs-clubs/web/features/manage-club/frames/InfoManageFrame";
 import MembersManageFrame from "@sparcs-clubs/web/features/manage-club/frames/MembersManageFrame";
 import ServiceManageFrame from "@sparcs-clubs/web/features/manage-club/frames/ServiceManageFrame";
 
-const ManageClub: React.FC = () => (
-  <FlexWrapper direction="column" gap={60}>
-    <PageHead
-      items={[{ name: "대표 동아리 관리", path: "/manage-club" }]}
-      title="대표 동아리 관리"
-    />
-    <InfoManageFrame />
-    <ActivityManageFrame />
-    <MembersManageFrame />
-    <ServiceManageFrame />
-  </FlexWrapper>
-);
+const ManageClub: React.FC = () => {
+  const isRepresentative = false; // 대표자 <--> 대의원
+
+  return (
+    <FlexWrapper direction="column" gap={60}>
+      <PageHead
+        items={[{ name: "대표 동아리 관리", path: "/manage-club" }]}
+        title="대표 동아리 관리"
+      />
+      {isRepresentative ? (
+        <InfoManageFrame />
+      ) : (
+        <FoldableSectionTitle title="동아리 정보">
+          <FlexWrapper direction="column" gap={40}>
+            <ChangeClubInfoCard />
+          </FlexWrapper>
+        </FoldableSectionTitle>
+      )}
+      <ActivityManageFrame />
+      <MembersManageFrame />
+      <ServiceManageFrame />
+    </FlexWrapper>
+  );
+};
 
 export default ManageClub;
