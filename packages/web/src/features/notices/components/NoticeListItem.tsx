10a9bc12853becdf2db192c113893f60f27ffbae
malloc3141 <malloc_3141592@naver.com>
2024-08-22 18:07:27 +0900

    fix: fix notice page mobile design

--- packages/web/src/features/notices/components/NoticeListItem.tsx
+++ packages/web/src/features/notices/components/NoticeListItem.tsx
@@ -14,6 +14,7 @@ interface NoticeListItemProps {
 
 const NoticeListItemInner = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
@@ -22,6 +23,7 @@ const NoticeListItemInner = styled.div`
   @media (max-width: ${({ theme }) => theme.responsive.BREAKPOINT.sm}) {
     flex-direction: column;
     column-gap: 4px;
+    justify-content: flex-start;
   }
 `;
 
@@ -37,6 +39,10 @@ const NoticeIconAndTitle = styled.div`
   flex-wrap: nowrap;
   align-items: center;
   column-gap: 12px;
+  @media (max-width: ${({ theme }) => theme.responsive.BREAKPOINT.sm}) {
+    width: 100%;
+    justify-content: flex-start;
+  }
 `;
 
 // 공지사항별 아이콘이 표기되야 하는 구역입니다.
@@ -49,7 +55,6 @@ const NoticeIconWrapper = styled.div`
 
 const NoticeTitleWrapper = styled.div`
   flex-grow: 1;
-  flex-shrink: 1;
   height: 24px;
   color: ${({ theme }) => theme.colors.BLACK};
   font-weight: ${({ theme }) => theme.fonts.WEIGHT.MEDIUM};
@@ -58,10 +63,18 @@ const NoticeTitleWrapper = styled.div`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+  display: block;
+  @media (max-width: ${({ theme }) => theme.responsive.BREAKPOINT.sm}) {
+    width: 100%;
+    height: 20px;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: left;
+  }
 `;
 
 const NoticeDate = styled.div<{ date: Date }>`
-  flex-basis: 100px;
+  width: 100px;
   flex-grow: 0;
   flex-shrink: 0;
   height: 24px;
@@ -70,6 +83,7 @@ const NoticeDate = styled.div<{ date: Date }>`
   line-height: 24px;
   text-align: center;
   overflow: hidden;
+  display: inline-block;
   &::after {
     content: "${({ date }) =>
       date.toLocaleDateString("ko-KR", {
@@ -79,8 +93,12 @@ const NoticeDate = styled.div<{ date: Date }>`
       })}";
   }
   @media (max-width: ${({ theme }) => theme.responsive.BREAKPOINT.sm}) {
-    font-size: 14px;
+    font-size: 12px;
     line-height: 16px;
+    width: 100%;
+    height: 16px;
+    text-align: left;
+    justify-content: flex-start;
     &::after {
       content: "${({ date }) =>
         date.toLocaleDateString("ko-KR", {
