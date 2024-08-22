ca03c0cdb95c45f72052be4bf1a677fe43d30940
Woojeong Choi <gldeel@kaist.ac.kr>
2024-08-23 02:21:11 +0900

    feat: add onClick for common frames

--- packages/web/src/common/frames/LoginRequired.tsx
+++ packages/web/src/common/frames/LoginRequired.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 
 import ErrorPageTemplate from "@sparcs-clubs/web/common/frames/ErrorPageTemplate";
 
+import { useAuth } from "../providers/AuthContext";
+
 import type { NextPage } from "next";
 
 const ErrorMessage = styled.div`
@@ -25,7 +27,6 @@ const ErrorMessage = styled.div`
 `;
 
 const LoginRequired: NextPage = () => {
-  // ToDo : 버튼 onClick 연결
   const Message = (
     <ErrorMessage>
       현재 접근한 페이지는
@@ -33,10 +34,12 @@ const LoginRequired: NextPage = () => {
       로그인해야 볼 수 있습니다
     </ErrorMessage>
   );
+  const { login } = useAuth();
+
   return (
     <ErrorPageTemplate
       message={Message}
-      buttons={[{ text: "로그인 바로가기", onClick: () => {} }]}
+      buttons={[{ text: "로그인 바로가기", onClick: login }]}
     />
   );
 };
