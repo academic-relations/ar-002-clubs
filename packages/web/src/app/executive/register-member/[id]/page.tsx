bfb14627fbb2692e3e61a77502a1cd39c8f1ae02
casio@sparcs.org <casio@sparcs.org>
2024-08-23 03:14:07 +0900

    feat: impl executive/register-member/id-page

--- packages/web/src/app/executive/register-member/[id]/page.tsx
+++ packages/web/src/app/executive/register-member/[id]/page.tsx
@@ -7,11 +7,11 @@ import { Divider } from "@mui/material";
 import styled from "styled-components";
 
 import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
-import IconButton from "@sparcs-clubs/web/common/components/Buttons/IconButton";
 import Card from "@sparcs-clubs/web/common/components/Card";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
 import Pagination from "@sparcs-clubs/web/common/components/Pagination";
+import Toggle from "@sparcs-clubs/web/common/components/Toggle";
 import Typography from "@sparcs-clubs/web/common/components/Typography";
 import { useGetClubDetail } from "@sparcs-clubs/web/features/clubDetails/services/getClubDetail";
 
@@ -27,20 +27,11 @@ const RegisterMember = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const limit = 10;
 
-  const [isOpened, setIsOpened] = useState(false);
-
   const pathSegments = window.location.pathname.split("/").filter(Boolean);
   const id = pathSegments[pathSegments.length - 1];
 
   const DividerContatiner = styled.div`
     padding-left: 28px;
-  `;
-
-  const HeaderWrapper = styled.div`
-    gap: 8px;
-    align-items: center;
-    flex-direction: row;
-    display: flex;
   `;
 
   /* TODO : API로 데이터 받아오기 */
@@ -65,16 +56,6 @@ const RegisterMember = () => {
 
   const GetPageTitle = () => `회원 등록 신청 내역 (${club.data?.name_kr})`;
 
-  const CustomIconButton = styled(IconButton)`
-    background-color: #ffffff; // 예시: 배경색 변경
-    color: #333333; // 예시: 아이콘 색상 변경
-
-    &:hover {
-      background-color: #ffffff; // 예시: 배경색 변경
-      color: #333333; // 예시: 호버 시 배경색 변경
-    }
-  `;
-
   return (
     <AsyncBoundary isLoading={club.isLoading} isError={club.isError}>
       <FlexWrapper direction="column" gap={20}>
@@ -87,57 +68,35 @@ const RegisterMember = () => {
           title={GetPageTitle()}
         />
         <Card gap={16} padding="16px">
-          <HeaderWrapper>
-            {isOpened ? (
-              <CustomIconButton
-                icon="expand_more"
-                size={16}
-                type="default"
-                onClick={() => setIsOpened(!isOpened)}
-              />
-            ) : (
-              <CustomIconButton
-                icon="chevron_right"
-                size={16}
-                type="default"
-                onClick={() => setIsOpened(!isOpened)}
-              />
-            )}
-            <Typography>회원 등록 신청 통계</Typography>
-          </HeaderWrapper>
-          {isOpened && (
-            <>
+          <Toggle label={<Typography>회원 등록 신청 통계</Typography>}>
+            <StatusInfoFrame
+              statusInfo={
+                calcRegister.get(MemberStatusEnum.Applied) || defaultStatusInfo
+              }
+              status={MemberStatusEnum.Applied}
+            />
+            <StatusInfoFrame
+              statusInfo={
+                calcRegister.get(MemberStatusEnum.Approved) || defaultStatusInfo
+              }
+              status={MemberStatusEnum.Approved}
+            />
+            <FlexWrapper gap={8} direction="column">
               <StatusInfoFrame
                 statusInfo={
-                  calcRegister.get(MemberStatusEnum.Applied) ||
+                  calcRegister.get(MemberStatusEnum.Rejected) ||
                   defaultStatusInfo
                 }
-                status={MemberStatusEnum.Applied}
+                status={MemberStatusEnum.Rejected}
               />
-              <StatusInfoFrame
-                statusInfo={
-                  calcRegister.get(MemberStatusEnum.Approved) ||
-                  defaultStatusInfo
-                }
-                status={MemberStatusEnum.Approved}
+              <DividerContatiner>
+                <Divider />
+              </DividerContatiner>
+              <TotalInfoFrame
+                statusInfo={calcRegister.get("Total") || defaultStatusInfo}
               />
-              <FlexWrapper gap={8} direction="column">
-                <StatusInfoFrame
-                  statusInfo={
-                    calcRegister.get(MemberStatusEnum.Rejected) ||
-                    defaultStatusInfo
-                  }
-                  status={MemberStatusEnum.Rejected}
-                />
-                <DividerContatiner>
-                  <Divider />
-                </DividerContatiner>
-                <TotalInfoFrame
-                  statusInfo={calcRegister.get("Total") || defaultStatusInfo}
-                />
-              </FlexWrapper>
-            </>
-          )}
+            </FlexWrapper>
+          </Toggle>
         </Card>
         <RegisterInfoTable memberRegisterInfoList={paginatedData} />
         <FlexWrapper direction="row" gap={16} justify="center">
