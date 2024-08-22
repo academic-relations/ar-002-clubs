7639cff7f8f0ca80fe1301916e2a38fef8afb3c5
malloc3141 <malloc_3141592@naver.com>
2024-08-23 01:40:19 +0900

    feat: connect link to sparcs logo footer

--- packages/web/src/common/components/Footer/_atomic/SPARCSLogo.tsx
+++ packages/web/src/common/components/Footer/_atomic/SPARCSLogo.tsx
@@ -1,17 +1,43 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Image from "next/image";
 import Link from "next/link";
 
+import styled, { useTheme } from "styled-components";
+
 import sparcsSvg from "@sparcs-clubs/web/assets/sparcs-black.svg";
 import paths from "@sparcs-clubs/web/constants/paths";
 
-const SPARCSLogo = () => (
-  <Link href={paths.HOME.path}>
-    <Image src={sparcsSvg} alt="SPARCS" height={24} />
-  </Link>
-);
+const StyledLink = styled(Link)`
+  @media (max-width: ${({ theme }) => theme.responsive.BREAKPOINT.lg}) {
+    target: "_blank";
+  }
+`;
+
+const SPARCSLogo = () => {
+  const theme = useTheme();
+  const [isSmallView, setIsSmallView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallView(
+        window.innerWidth <= parseInt(theme.responsive.BREAKPOINT.lg),
+      );
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <StyledLink href={isSmallView ? paths.MADE_BY.path : "https://sparcs.org"}>
+      <Image src={sparcsSvg} alt="SPARCS" height={24} />
+    </StyledLink>
+  );
+};
 
 export default SPARCSLogo;
