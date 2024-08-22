dbd16ffb9ff5bda4677436889340423ddc7f817c
malloc3141 <malloc_3141592@naver.com>
2024-08-23 02:25:55 +0900

    feat: connect registration delete api to /clubs/[id]

--- packages/web/src/app/clubs/[id]/page.tsx
+++ packages/web/src/app/clubs/[id]/page.tsx
@@ -1,19 +1,54 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import { RegistrationDeadlineEnum } from "@sparcs-clubs/interface/common/enum/registration.enum";
 import { useParams } from "next/navigation";
 
 import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import ClubDetailMainFrame from "@sparcs-clubs/web/features/clubDetails/frames/ClubDetailMainFrame";
 import { useGetClubDetail } from "@sparcs-clubs/web/features/clubDetails/services/getClubDetail";
-import { useCheckResisteringPeriod } from "@sparcs-clubs/web/hooks/checkResisteringPeriod";
+import { useGetRegisTerm } from "@sparcs-clubs/web/features/clubs/services/useGetRegisTerm";
 
 const ClubDetail = () => {
   const { id } = useParams();
   const { data, isLoading, isError } = useGetClubDetail(id as string);
-  const [isRegistrationPeriod, eventLoading] = useCheckResisteringPeriod();
+  const {
+    data: termData,
+    isLoading: isLoadingTerm,
+    isError: isErrorTerm,
+  } = useGetRegisTerm();
+  const [isRegistrationPeriod, setIsRegistrationPeriod] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    if (termData) {
+      const now = new Date();
+      const currentEvents = termData.events.filter(
+        event => now >= event.startTerm && now <= event.endTerm,
+      );
+      if (currentEvents.length === 0) {
+        setIsRegistrationPeriod(false);
+        return;
+      }
+      const regisEvent = currentEvents.filter(
+        event =>
+          event.registrationEventEnumId ===
+          RegistrationDeadlineEnum.StudentRegistrationApplication,
+      );
+      if (regisEvent.length > 0) {
+        setIsRegistrationPeriod(true);
+      } else {
+        setIsRegistrationPeriod(false);
+      }
+    }
+  }, [termData]);
 
   return (
-    <AsyncBoundary isLoading={isLoading || eventLoading} isError={isError}>
+    <AsyncBoundary
+      isLoading={isLoading || isLoadingTerm}
+      isError={isError || isErrorTerm}
+    >
       {data && (
         <ClubDetailMainFrame
           club={data}
