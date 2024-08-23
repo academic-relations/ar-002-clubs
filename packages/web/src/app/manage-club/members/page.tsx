a30d496677bba4af85ccba758971d4683b8aa834
Woojeong Choi <gldeel@kaist.ac.kr>
2024-08-23 19:35:17 +0900

    feat: not registration period

--- packages/web/src/app/manage-club/members/page.tsx
+++ packages/web/src/app/manage-club/members/page.tsx
@@ -2,10 +2,14 @@
 
 import React, { useEffect, useState } from "react";
 
+import { RegistrationDeadlineEnum } from "@sparcs-clubs/interface/common/enum/registration.enum";
+
 import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import LoginRequired from "@sparcs-clubs/web/common/frames/LoginRequired";
 import NoManageClub from "@sparcs-clubs/web/common/frames/NoManageClub";
+import NotRegistrationPeriod from "@sparcs-clubs/web/common/frames/NotRegistrationPeriod";
 import { useAuth } from "@sparcs-clubs/web/common/providers/AuthContext";
+import { useGetRegistrationTerm } from "@sparcs-clubs/web/features/clubs/services/useGetRegistrationTerm";
 import ManageClubMembers from "@sparcs-clubs/web/features/manage-club/members/frames/ManageClubMembersFrame";
 
 const Members = () => {
@@ -18,8 +22,39 @@ const Members = () => {
     }
   }, [isLoggedIn, profile]);
 
-  if (loading) {
-    return <AsyncBoundary isLoading={loading} isError />;
+  const { data, isLoading, isError } = useGetRegistrationTerm();
+  const [isRegistrationPeriod, setIsRegistrationPeriod] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    if (data) {
+      const now = new Date();
+      const currentEvents = data.events.filter(
+        event => now >= event.startTerm && now <= event.endTerm,
+      );
+      if (currentEvents.length === 0) {
+        setIsRegistrationPeriod(false);
+        return;
+      }
+      const registrationEvent = currentEvents.filter(
+        event =>
+          event.registrationEventEnumId ===
+          RegistrationDeadlineEnum.StudentRegistrationApplication,
+      );
+      if (registrationEvent.length > 0) {
+        setIsRegistrationPeriod(true);
+      } else {
+        setIsRegistrationPeriod(false);
+      }
+    }
+  }, [data]);
+
+  if (loading || isLoading) {
+    return <AsyncBoundary isLoading={loading || isLoading} isError={isError} />;
+  }
+
+  if (!isRegistrationPeriod) {
+    return <NotRegistrationPeriod />;
   }
 
   if (!isLoggedIn) {
