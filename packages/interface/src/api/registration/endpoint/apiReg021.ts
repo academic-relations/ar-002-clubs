65677ab049ef1df5a4279257a6ab5c0e54d8f44c
Woojeong Choi <gldeel@kaist.ac.kr>
2024-08-24 00:42:52 +0900

    feat: impl reg021 interface

---
+++ packages/interface/src/api/registration/endpoint/apiReg021.ts
@@ -0,0 +1,73 @@
+import { HttpStatusCode } from "axios";
+import { z } from "zod";
+
+import {
+  zClubName,
+  zUserName,
+} from "@sparcs-clubs/interface/common/commonString";
+import { zKrPhoneNumber } from "@sparcs-clubs/interface/common/type/phoneNumber.type";
+
+/**
+ * @version v0.1
+ * @description  동아리별 가입 신청의 간략한 상태를 확인합니다.
+ * - 교수로 로그인되어 있어야 합니다.
+ * - 자신을 지도교수로 신청한 동아리 등록 신청만을 조회합니다.
+ * - 분과 > 구분 > 동아리 이름 우선순위로 정렬한 목록을 리턴합니다.
+ */
+
+const url = () => `/professor/registrations/member-registrations/brief`;
+const method = "GET";
+
+const requestParam = z.object({});
+
+const requestQuery = z.object({});
+const requestBody = z.object({});
+
+const responseBodyMap = {
+  [HttpStatusCode.Ok]: z.object({
+    clubs: z.array(
+      z.object({
+        clubId: z.coerce.number().int().min(1),
+        clubTypeEnumId: z.coerce.number().int().min(1),
+        division: z.object({
+          id: z.coerce.number().int().min(1),
+          name: zUserName,
+        }),
+        clubName: zClubName,
+        student: z.object({
+          id: z.coerce.number().int().min(1),
+          studentNumber: z.coerce.number().int().min(1),
+          name: zUserName,
+          phoneNumber: zKrPhoneNumber.optional(),
+          email: z.coerce.string(),
+        }),
+      }),
+    ),
+  }),
+};
+
+const responseErrorMap = {};
+
+const apiReg021 = {
+  url,
+  method,
+  requestParam,
+  requestQuery,
+  requestBody,
+  responseBodyMap,
+  responseErrorMap,
+};
+
+type ApiReg021RequestParam = z.infer<typeof apiReg021.requestParam>;
+type ApiReg021RequestQuery = z.infer<typeof apiReg021.requestQuery>;
+type ApiReg021RequestBody = z.infer<typeof apiReg021.requestBody>;
+type ApiReg021ResponseOk = z.infer<(typeof apiReg021.responseBodyMap)[200]>;
+
+export default apiReg021;
+
+export type {
+  ApiReg021RequestParam,
+  ApiReg021RequestQuery,
+  ApiReg021RequestBody,
+  ApiReg021ResponseOk,
+};
