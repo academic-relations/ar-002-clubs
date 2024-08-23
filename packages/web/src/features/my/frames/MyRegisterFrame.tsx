66d6ed0a8653cce06febdc0bdc411c775b8242ed
Woojeong Choi <gldeel@kaist.ac.kr>
2024-08-23 22:51:03 +0900

    feat: use registration term

--- packages/web/src/features/my/frames/MyRegisterFrame.tsx
+++ packages/web/src/features/my/frames/MyRegisterFrame.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { RegistrationDeadlineEnum } from "@sparcs-clubs/interface/common/enum/registration.enum";
 
@@ -6,6 +6,7 @@ import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
 import MoreDetailTitle from "@sparcs-clubs/web/common/components/MoreDetailTitle";
+import { useGetRegistrationTerm } from "@sparcs-clubs/web/features/clubs/services/useGetRegistrationTerm";
 import MyClubProfTable from "@sparcs-clubs/web/features/my/components/MyClubProfTable";
 import MyClubTable from "@sparcs-clubs/web/features/my/components/MyClubTable";
 import MyMemberTable from "@sparcs-clubs/web/features/my/components/MyMemberTable";
@@ -13,22 +14,57 @@ import {
   mockClubRegister,
   mockMemberRegister,
   mockProfClubRegister,
-  mockRegisterPeriod,
 } from "@sparcs-clubs/web/features/my/services/_mock/mockMyRegister";
 
-const MyRegisterFrame: React.FC<{ profile: string }> = ({ profile }) =>
-  (mockRegisterPeriod.includes(
-    RegistrationDeadlineEnum.ClubRegistrationApplication,
-  ) ||
-    mockRegisterPeriod.includes(
-      RegistrationDeadlineEnum.StudentRegistrationApplication,
-    )) && (
+const MyRegisterFrame: React.FC<{ profile: string }> = ({ profile }) => {
+  const {
+    data: termData,
+    isLoading: isLoadingTerm,
+    isError: isErrorTerm,
+  } = useGetRegistrationTerm();
+
+  const [registrationStatus, setRegistrationStatus] =
+    useState<RegistrationDeadlineEnum>(RegistrationDeadlineEnum.Finish);
+
+  useEffect(() => {
+    if (termData) {
+      const now = new Date();
+      const currentEvents = termData.events.filter(
+        event => now >= event.startTerm && now <= event.endTerm,
+      );
+      if (currentEvents.length === 0) {
+        setRegistrationStatus(RegistrationDeadlineEnum.Finish);
+        return;
+      }
+      const memberRegistrationEvent = currentEvents.filter(
+        event =>
+          event.registrationEventEnumId ===
+          RegistrationDeadlineEnum.StudentRegistrationApplication,
+      );
+      if (memberRegistrationEvent.length > 0) {
+        setRegistrationStatus(
+          RegistrationDeadlineEnum.StudentRegistrationApplication,
+        );
+      }
+      const clubRegistrationEvent = currentEvents.filter(
+        event =>
+          event.registrationEventEnumId ===
+          RegistrationDeadlineEnum.ClubRegistrationApplication,
+      );
+      if (clubRegistrationEvent.length > 0) {
+        setRegistrationStatus(
+          RegistrationDeadlineEnum.ClubRegistrationApplication,
+        );
+      }
+    }
+  }, [termData]);
+
+  return (
     <FoldableSectionTitle title="동아리 신청 내역">
-      <AsyncBoundary isLoading={false} isError={false}>
+      <AsyncBoundary isLoading={isLoadingTerm} isError={isErrorTerm}>
         <FlexWrapper direction="column" gap={40}>
-          {mockRegisterPeriod.includes(
-            RegistrationDeadlineEnum.ClubRegistrationApplication,
-          ) && (
+          {registrationStatus ===
+            RegistrationDeadlineEnum.ClubRegistrationApplication && (
             <FlexWrapper direction="column" gap={20}>
               <MoreDetailTitle
                 title="동아리 등록"
@@ -39,7 +75,11 @@ const MyRegisterFrame: React.FC<{ profile: string }> = ({ profile }) =>
                 {profile === "professor" ? (
                   <MyClubProfTable
                     clubProfRegisterList={
-                      mockProfClubRegister ?? { total: 0, items: [], offset: 0 }
+                      mockProfClubRegister ?? {
+                        total: 0,
+                        items: [],
+                        offset: 0,
+                      }
                     }
                   />
                 ) : (
@@ -52,9 +92,8 @@ const MyRegisterFrame: React.FC<{ profile: string }> = ({ profile }) =>
               </AsyncBoundary>
             </FlexWrapper>
           )}
-          {mockRegisterPeriod.includes(
-            RegistrationDeadlineEnum.StudentRegistrationApplication,
-          ) &&
+          {registrationStatus ===
+            RegistrationDeadlineEnum.StudentRegistrationApplication &&
             profile !== "professor" && (
               <FlexWrapper direction="column" gap={20}>
                 <MoreDetailTitle
@@ -75,5 +114,6 @@ const MyRegisterFrame: React.FC<{ profile: string }> = ({ profile }) =>
       </AsyncBoundary>
     </FoldableSectionTitle>
   );
+};
 
 export default MyRegisterFrame;
