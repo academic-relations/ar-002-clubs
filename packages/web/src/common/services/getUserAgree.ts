b113d27214232b4700ead9bc77aedc82d669b8a1
Woojeong Choi <gldeel@kaist.ac.kr>
2024-08-28 19:56:20 +0900

    feat: use usr 004 005

---
+++ packages/web/src/common/services/getUserAgree.ts
@@ -0,0 +1,34 @@
+import apiUsr005 from "@sparcs-clubs/interface/api/user/endpoint/apiUsr005";
+import { useQuery } from "@tanstack/react-query";
+
+import {
+  axiosClientWithAuth,
+  defineAxiosMock,
+  UnexpectedAPIResponseError,
+} from "@sparcs-clubs/web/lib/axios";
+
+import type { ApiUsr005ResponseOk } from "@sparcs-clubs/interface/api/user/endpoint/apiUsr005";
+
+const useUserAgree = () =>
+  useQuery<ApiUsr005ResponseOk, Error>({
+    queryKey: [apiUsr005.url()],
+    queryFn: async (): Promise<ApiUsr005ResponseOk> => {
+      const { data, status } = await axiosClientWithAuth.get(
+        apiUsr005.url(),
+        {},
+      );
+
+      switch (status) {
+        case 200:
+          return apiUsr005.responseBodyMap[200].parse(data);
+        default:
+          throw new UnexpectedAPIResponseError();
+      }
+    },
+  });
+
+export default useUserAgree;
+
+defineAxiosMock(mock => {
+  mock.onGet(apiUsr005.url()).reply(() => [200, { status: { isAgree: true } }]);
+});
