3967350095d2508143ec1c4bc51e7ade28d93f90
Nicole Lee <nicolelee2001@gmail.com>
2024-08-29 01:26:15 +0900

    fix: year select for register club

--- packages/web/src/features/register-club/components/_atomic/YearSelect.tsx
+++ packages/web/src/features/register-club/components/_atomic/YearSelect.tsx
@@ -1,54 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import { ApiReg001RequestBody } from "@sparcs-clubs/interface/api/registration/endpoint/apiReg001";
 import { useFormContext } from "react-hook-form";
 
 import Select, { SelectItem } from "@sparcs-clubs/web/common/components/Select";
 
-const YearSelect: React.FC<{ isProvisional?: boolean }> = ({
-  isProvisional = false,
-}) => {
+const YearSelect: React.FC = () => {
   const { watch, setValue } = useFormContext<ApiReg001RequestBody>();
+
   const foundedAt = watch("foundedAt");
 
-  const [value, onChange] = useState<number>();
+  const [value, onChangeValue] = useState<string>(
+    foundedAt ? foundedAt.getFullYear().toString() : "",
+  );
 
   const startYear = 1980;
-
   const years = Array.from(
     { length: new Date().getFullYear() - startYear + 1 },
     (_, i) => startYear + i,
   ).reverse();
-
-  useEffect(() => {
-    if (value == null && foundedAt != null) {
-      onChange(foundedAt.getFullYear());
-    }
-  }, [foundedAt, value]);
-
-  useEffect(() => {
-    if (value == null) return;
-
-    const selectedDate = foundedAt == null ? new Date() : foundedAt;
-
-    selectedDate.setFullYear(value);
-    if (foundedAt) {
-      selectedDate.setMonth(foundedAt.getMonth());
-    }
-
-    setValue("foundedAt", selectedDate);
-  }, [value, setValue, isProvisional]);
-
-  const yearSelectItems: SelectItem<number>[] = years.map(data => ({
-    value: data,
+  const items: SelectItem<string>[] = years.map(data => ({
+    value: data.toString(),
     label: data.toString(),
   }));
+
+  const onChange = (_value: string) => {
+    const year = parseInt(_value);
+    const month = foundedAt ? foundedAt.getMonth() : 1;
+    const date = new Date(`${year}-${month.toString().padStart(2, "0")}-01`);
+    setValue("foundedAt", date);
+    onChangeValue(_value);
+  };
 
   return (
     <Select
       label="설립 연도"
       placeholder="설립 연도를 선택해주세요"
-      items={yearSelectItems}
+      items={items}
       value={value}
       onChange={onChange}
     />
