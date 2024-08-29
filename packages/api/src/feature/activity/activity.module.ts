e6f06837468e68c4feba340561b41408d227bb97
perry <perry@channel.io>
2024-08-29 22:08:07 +0900

    fix: module providers

--- packages/api/src/feature/activity/activity.module.ts
+++ packages/api/src/feature/activity/activity.module.ts
@@ -1,8 +1,12 @@
+import { S3Client } from "@aws-sdk/client-s3";
 import { Module } from "@nestjs/common";
 
 import { DrizzleModule } from "src/drizzle/drizzle.module";
 
 import { ClubModule } from "../club/club.module";
+
+import { FileRepository } from "../file/repository/file.repository";
+import FilePublicService from "../file/service/file.public.service";
 
 import ActivityActivityTermController from "./controller/activity.activity-term.controller";
 import ActivityController from "./controller/activity.controller";
@@ -21,6 +25,9 @@ import ActivityService from "./service/activity.service";
     ActivityService,
     ActivityActivityTermService,
     ActivityPublicService,
+    FilePublicService,
+    FileRepository,
+    S3Client,
   ],
   exports: [ActivityPublicService],
 })
