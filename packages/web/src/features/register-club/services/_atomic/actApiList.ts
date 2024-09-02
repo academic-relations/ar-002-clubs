ed3f10c18778e7535a6f9590aac4c72da015545f
Woojeong Choi <gldeel@kaist.ac.kr>
2024-09-02 19:24:53 +0900

    feat: impl getActivityReport

--- packages/web/src/features/register-club/services/_atomic/actApiList.ts
+++ packages/web/src/features/register-club/services/_atomic/actApiList.ts
@@ -1,6 +1,9 @@
+import apiAct002 from "@sparcs-clubs/interface/api/activity/endpoint/apiAct002";
 import apiAct011 from "@sparcs-clubs/interface/api/activity/endpoint/apiAct011";
 import apiAct012 from "@sparcs-clubs/interface/api/activity/endpoint/apiAct012";
 import apiAct013 from "@sparcs-clubs/interface/api/activity/endpoint/apiAct013";
+import apiAct014 from "@sparcs-clubs/interface/api/activity/endpoint/apiAct014";
+import apiAct015 from "@sparcs-clubs/interface/api/activity/endpoint/apiAct015";
 
 const activitiesGet = (profile: string) => {
   if (profile === "professor") {
@@ -12,4 +15,14 @@ const activitiesGet = (profile: string) => {
   return apiAct011.url();
 };
 
-export default activitiesGet;
+const activityDetailGet = (profile: string, id: number) => {
+  if (profile === "professor") {
+    return apiAct015.url(id);
+  }
+  if (profile === "executive") {
+    return apiAct014.url(id);
+  }
+  return apiAct002.url(id);
+};
+
+export { activitiesGet, activityDetailGet };
