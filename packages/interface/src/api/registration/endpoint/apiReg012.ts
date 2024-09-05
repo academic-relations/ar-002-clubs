9bb6d6f3af6736f9bd50fd0753ae9187137feb9f
perry <perry@channel.io>
2024-09-06 04:39:08 +0900

    fix: build error

--- packages/interface/src/api/registration/endpoint/apiReg012.ts
+++ packages/interface/src/api/registration/endpoint/apiReg012.ts
@@ -31,7 +31,7 @@ const responseBodyMap = {
         divisionName: z.string().max(128),
         clubNameKr: zClubName.optional(),
         newClubNameKr: zClubName,
-        clubNameEm: zClubName.optional(),
+        clubNameEn: zClubName.optional(),
         newClubNameEn: zClubName,
         clubId: z.coerce.number().int().min(1),
         activityFieldKr: z.string().max(255),
