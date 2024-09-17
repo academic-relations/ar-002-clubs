95f67234d326882d960c0b9ec2da5281b15838ab
Woojeong Choi <gldeel@kaist.ac.kr>
2024-09-18 01:13:27 +0900

    fix: endTerm date format

--- packages/web/src/constants/manageClubMembers.ts
+++ packages/web/src/constants/manageClubMembers.ts
@@ -1,8 +1,6 @@
-import { getDate, getHours, getMinutes, getMonth, getYear } from "date-fns";
+import { formatDateTime } from "../utils/Date/formatDate";
 
 const newMemberListSectionInfoText = (semester: string, deadline: Date) =>
-  `현재는 ${semester}학기 동아리 신청 기간입니다 
-(신청 마감 : ${getYear(deadline)}년 ${getMonth(deadline)}월 ${getDate(deadline)}일 
-${getHours(deadline)}:${getMinutes(deadline)})`;
+  `현재는 ${semester}학기 동아리 신청 기간입니다 (신청 마감 : ${formatDateTime(deadline)})`;
 
 export { newMemberListSectionInfoText };
