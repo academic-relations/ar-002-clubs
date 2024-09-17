0f0a72098e4ae7313858ff40291e3b354ae550a1
Woojeong Choi <gldeel@kaist.ac.kr>
2024-09-18 01:00:03 +0900

    feat: enable manage club member registration

--- packages/web/src/features/manage-club/frames/ManageClubFrame.tsx
+++ packages/web/src/features/manage-club/frames/ManageClubFrame.tsx
@@ -1,24 +1,54 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { ClubDelegateEnum } from "@sparcs-clubs/interface/common/enum/club.enum";
+
+import { RegistrationDeadlineEnum } from "@sparcs-clubs/interface/common/enum/registration.enum";
 
 import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
 import NoManageClub from "@sparcs-clubs/web/common/frames/NoManageClub";
 // import ActivityManageFrame from "@sparcs-clubs/web/features/manage-club/frames/ActivityManageFrame";
+import { useGetRegistrationTerm } from "@sparcs-clubs/web/features/clubs/services/useGetRegistrationTerm";
 import InfoManageFrame from "@sparcs-clubs/web/features/manage-club/frames/InfoManageFrame";
-// import MembersManageFrame from "@sparcs-clubs/web/features/manage-club/frames/MembersManageFrame";
+import MembersManageFrame from "@sparcs-clubs/web/features/manage-club/frames/MembersManageFrame";
 // import ServiceManageFrame from "@sparcs-clubs/web/features/manage-club/frames/ServiceManageFrame";
 import { useCheckManageClub } from "@sparcs-clubs/web/hooks/checkManageClub";
 
 const ManageClubFrame: React.FC = () => {
   const { delegate, isLoading } = useCheckManageClub();
 
-  if (isLoading) {
-    return <AsyncBoundary isLoading={isLoading} isError />;
+  const { data, isLoading: termIsLoading } = useGetRegistrationTerm();
+  const [isRegistrationPeriod, setIsRegistrationPeriod] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    if (data) {
+      const now = new Date();
+      const currentEvents = data.events.filter(
+        event => now >= event.startTerm && now <= event.endTerm,
+      );
+      if (currentEvents.length === 0) {
+        setIsRegistrationPeriod(false);
+        return;
+      }
+      const registrationEvent = currentEvents.filter(
+        event =>
+          event.registrationEventEnumId ===
+          RegistrationDeadlineEnum.StudentRegistrationApplication,
+      );
+      if (registrationEvent.length > 0) {
+        setIsRegistrationPeriod(true);
+      } else {
+        setIsRegistrationPeriod(false);
+      }
+    }
+  }, [data]);
+
+  if (isLoading || termIsLoading) {
+    return <AsyncBoundary isLoading={isLoading || termIsLoading} isError />;
   }
 
   if (delegate === undefined) {
@@ -34,10 +64,9 @@ const ManageClubFrame: React.FC = () => {
       <InfoManageFrame
         isRepresentative={delegate === ClubDelegateEnum.Representative}
       />
-      {/* TODO: 회원 등록 배포 때 MembersManageFrame 주석 해제 */}
-      {/* <ActivityManageFrame />
-      <MembersManageFrame />
-      <ServiceManageFrame /> */}
+      {/* <ActivityManageFrame /> */}
+      {isRegistrationPeriod && <MembersManageFrame />}
+      {/* <ServiceManageFrame /> */}
     </FlexWrapper>
   );
 };
