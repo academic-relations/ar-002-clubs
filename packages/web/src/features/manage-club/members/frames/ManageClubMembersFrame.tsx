58332ee4c5e1f36e23c725de253bd4a96d35832d
Nicole Lee <nicolelee2001@gmail.com>
2024-09-29 17:11:57 +0900

    feat: get semester data from db

--- packages/web/src/features/manage-club/members/frames/ManageClubMembersFrame.tsx
+++ packages/web/src/features/manage-club/members/frames/ManageClubMembersFrame.tsx
@@ -2,16 +2,22 @@
 
 import React from "react";
 
+import { ApiClb015ResponseOk } from "@sparcs-clubs/interface/api/club/endpoint/apiClb015";
+
 import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
 import NoManageClub from "@sparcs-clubs/web/common/frames/NoManageClub";
-// import AllMemberListFrame from "@sparcs-clubs/web/features/manage-club/members/frames/AllMemberListFrame";
+import AllMemberListFrame from "@sparcs-clubs/web/features/manage-club/members/frames/AllMemberListFrame";
 import RegisterMemberListFrame from "@sparcs-clubs/web/features/manage-club/members/frames/RegisterMemberListFrame";
+import { useGetMyManageClub } from "@sparcs-clubs/web/features/manage-club/services/getMyManageClub";
 import { useCheckManageClub } from "@sparcs-clubs/web/hooks/checkManageClub";
 
 const ManageClubMembers = () => {
   const { delegate, isLoading } = useCheckManageClub();
+  const { data: clubData } = useGetMyManageClub() as {
+    data: ApiClb015ResponseOk;
+  };
 
   if (isLoading) {
     return <AsyncBoundary isLoading={isLoading} isError />;
@@ -31,7 +37,7 @@ const ManageClubMembers = () => {
         title="회원 명단"
       />
       <RegisterMemberListFrame />
-      {/* <AllMemberListFrame /> */}
+      {clubData && <AllMemberListFrame clubId={clubData.clubId} />}
     </FlexWrapper>
   );
 };
