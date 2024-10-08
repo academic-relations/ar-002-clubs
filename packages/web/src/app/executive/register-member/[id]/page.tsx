f1c26a70897bbc3b113e364e715c2fdd2eff67f3
Nicole Lee <nicolelee2001@gmail.com>
2024-10-08 16:50:12 +0900

    fix: show member registration pages anytime for executive

--- packages/web/src/app/executive/register-member/[id]/page.tsx
+++ packages/web/src/app/executive/register-member/[id]/page.tsx
@@ -2,52 +2,18 @@
 
 import React, { useEffect, useState } from "react";
 
-import { RegistrationDeadlineEnum } from "@sparcs-clubs/interface/common/enum/registration.enum";
-
 import Custom404 from "@sparcs-clubs/web/app/not-found";
 import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import LoginRequired from "@sparcs-clubs/web/common/frames/LoginRequired";
 
 import { useAuth } from "@sparcs-clubs/web/common/providers/AuthContext";
 
-import { useGetRegistrationTerm } from "@sparcs-clubs/web/features/clubs/services/useGetRegistrationTerm";
-
 import ExecutiveRegisterMemberDetail from "@sparcs-clubs/web/features/executive/register-member/frames/ExecutiveRegisterMemberDetailFrame";
 
+/** NOTE: (@dora) 등록 기간 무관하게 항상 볼 수 있는 화면 */
 const RegisterMember = () => {
   const { isLoggedIn, login, profile } = useAuth();
   const [loading, setLoading] = useState(true);
-
-  const {
-    data: termData,
-    isLoading: isLoadingTerm,
-    isError: isErrorTerm,
-  } = useGetRegistrationTerm();
-
-  const [isRegistrationPeriod, setIsRegistrationPeriod] = useState<boolean>();
-
-  useEffect(() => {
-    if (termData) {
-      const now = new Date();
-      const currentEvents = termData.events.filter(
-        event => now >= event.startTerm && now <= event.endTerm,
-      );
-      if (currentEvents.length === 0) {
-        setIsRegistrationPeriod(false);
-        return;
-      }
-      const registrationEvent = currentEvents.filter(
-        event =>
-          event.registrationEventEnumId ===
-          RegistrationDeadlineEnum.StudentRegistrationApplication,
-      );
-      if (registrationEvent.length > 0) {
-        setIsRegistrationPeriod(true);
-      } else {
-        setIsRegistrationPeriod(false);
-      }
-    }
-  }, [termData]);
 
   useEffect(() => {
     if (isLoggedIn !== undefined || profile !== undefined) {
@@ -55,13 +21,8 @@ const RegisterMember = () => {
     }
   }, [isLoggedIn, profile]);
 
-  if (loading || isLoadingTerm) {
-    return (
-      <AsyncBoundary
-        isLoading={loading || isLoadingTerm}
-        isError={isErrorTerm}
-      />
-    );
+  if (loading) {
+    return <AsyncBoundary isLoading={loading} isError={false} />;
   }
 
   if (!isLoggedIn) {
@@ -69,10 +30,6 @@ const RegisterMember = () => {
   }
 
   if (profile?.type !== "executive") {
-    return <Custom404 />;
-  }
-
-  if (!isRegistrationPeriod) {
     return <Custom404 />;
   }
 
