fb741ae5a2cb3c1444f8dc267dd262b0f57c2ece
Woojeong Choi <gldeel@kaist.ac.kr>
2024-11-11 23:04:07 +0900

    feat: use react hook form for third frame

--- packages/web/src/features/rental-business/frames/RentalInfoFrame/RentalInfoThirdFrame.tsx
+++ packages/web/src/features/rental-business/frames/RentalInfoFrame/RentalInfoThirdFrame.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 import { FormProvider } from "react-hook-form";
 import styled from "styled-components";
@@ -9,10 +9,7 @@ import Card from "@sparcs-clubs/web/common/components/Card";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FormController from "@sparcs-clubs/web/common/components/FormController";
 import TextInput from "@sparcs-clubs/web/common/components/Forms/TextInput";
-import {
-  ListContainer,
-  ListItem,
-} from "@sparcs-clubs/web/common/components/ListItem";
+import { ListItem } from "@sparcs-clubs/web/common/components/ListItem";
 import Typography from "@sparcs-clubs/web/common/components/Typography";
 import RentalList from "@sparcs-clubs/web/features/rental-business/components/RentalList";
 import mockupAvailableRental from "@sparcs-clubs/web/features/rental-business/service/_mock/mockAvailableRental";
@@ -20,55 +17,49 @@ import { formatDate } from "@sparcs-clubs/web/utils/Date/formatDate";
 
 import { RentalFrameProps } from "../RentalNoticeFrame";
 
-const CardInner = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 16px;
-  align-self: stretch;
-`;
-
 const RentalPeriodInner = styled.div`
   display: flex;
 `;
 
 const RentalInfoThirdFrame: React.FC<
   RentalFrameProps & { setNextEnabled: (enabled: boolean) => void }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
 > = ({ formCtx, setNextEnabled }) => {
-  const { control, getValues } = formCtx;
+  const { control, getValues, watch } = formCtx;
+  const purpose = watch("purpose");
+
+  useEffect(() => {
+    setNextEnabled(!!purpose);
+  }, [purpose, setNextEnabled]);
 
   return (
     <FormProvider {...formCtx}>
       <Card outline gap={20}>
-        <CardInner>
+        <FlexWrapper direction="column" gap={16}>
           <Typography fs={16} lh={20} fw="MEDIUM">
             신청자 정보
           </Typography>
-          <ListContainer>
-            <ListItem>동아리: {getValues("info.clubName")}</ListItem>
-            <ListItem>담당자: {getValues("info.applicant")}</ListItem>
-            <ListItem>연락처: {getValues("info.phoneNumber")}</ListItem>
-          </ListContainer>
-          <FlexWrapper direction="row" gap={16}>
-            <Typography fs={16} lh={20} fw="MEDIUM">
-              대여 기간
-            </Typography>
-            <RentalPeriodInner>
-              {formatDate(getValues("date.start") || new Date())} ~
-              {formatDate(getValues("date.end") || new Date())}
-              {/* new Date() 넣어도 되는 이유: thirdFrame에서는 rental date가 둘 다 not null인 상태로 넘어옴 */}
-            </RentalPeriodInner>
-          </FlexWrapper>
+          <ListItem>동아리: {getValues("info.clubName")}</ListItem>
+          <ListItem>담당자: {getValues("info.applicant")}</ListItem>
+          <ListItem>연락처: {getValues("info.phoneNumber")}</ListItem>
+        </FlexWrapper>
+        <FlexWrapper direction="row" gap={16}>
           <Typography fs={16} lh={20} fw="MEDIUM">
-            대여 물품
+            대여 기간
           </Typography>
-          {/* TODO: RentalList mockupAvailableRental 없애기 */}
-          <RentalList
-            formCtx={formCtx}
-            availableRentals={mockupAvailableRental}
-          />
-        </CardInner>
+          <RentalPeriodInner>
+            {formatDate(getValues("date.start") || new Date())} ~
+            {formatDate(getValues("date.end") || new Date())}
+            {/* new Date() 넣어도 되는 이유: thirdFrame에서는 rental date가 둘 다 not null인 상태로 넘어옴 */}
+          </RentalPeriodInner>
+        </FlexWrapper>
+        <Typography fs={16} lh={20} fw="MEDIUM">
+          대여 물품
+        </Typography>
+        {/* TODO: RentalList mockupAvailableRental 없애기 */}
+        <RentalList
+          formCtx={formCtx}
+          availableRentals={mockupAvailableRental}
+        />
         {/* TODO: 조건 채우기 */}
         <FormController
           name="purpose"
