b9726c47390d97e66301a12ea90cf6ed41c50850
david <dongyun556@gmail.com>
2024-11-26 03:00:54 +0900

    fix: apply react-hook-form to ActivityReportEditFrame using ActivityReportForm

--- packages/web/src/features/manage-club/activity-report/frames/ActivityReportEditFrame.tsx
+++ packages/web/src/features/manage-club/activity-report/frames/ActivityReportEditFrame.tsx
@@ -1,111 +1,89 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect } from "react";
 
-import apiAct003 from "@sparcs-clubs/interface/api/activity/endpoint/apiAct003";
-import { ActivityTypeEnum } from "@sparcs-clubs/interface/common/enum/activity.enum";
-import { isValid, parse } from "date-fns";
+import { ApiAct003RequestBody } from "@sparcs-clubs/interface/api/activity/endpoint/apiAct003";
+
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 
-import { z } from "zod";
-
 import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
-import Button from "@sparcs-clubs/web/common/components/Button";
-import Card from "@sparcs-clubs/web/common/components/Card";
-import FileUpload from "@sparcs-clubs/web/common/components/FileUpload";
-import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
-import DateRangeInput from "@sparcs-clubs/web/common/components/Forms/DateRangeInput";
-import TextInput from "@sparcs-clubs/web/common/components/Forms/TextInput";
 import PageHead from "@sparcs-clubs/web/common/components/PageHead";
-import SectionTitle from "@sparcs-clubs/web/common/components/SectionTitle";
-import Select from "@sparcs-clubs/web/common/components/Select";
 
-import { formatDotDate } from "@sparcs-clubs/web/utils/Date/formatDate";
+import { useGetActivityReport } from "@sparcs-clubs/web/features/manage-club/activity-report/services/useGetActivityReport";
 
-import { mockParticipantData } from "../_mock/mock";
-import SelectParticipant from "../components/SelectParticipant";
-import { useGetActivityReport } from "../services/useGetActivityReport";
-import { usePutActivityReport } from "../services/usePutActivityReport";
+import ActivityReportForm from "@sparcs-clubs/web/features/register-club/components/_atomic/ActivityReportForm";
+import usePutActivityReportForNewClub from "@sparcs-clubs/web/features/register-club/services/usePutActivityReportForNewClub";
 
 const ActivityReportMainFrameInner = styled.div`
-  display: flex;
-  flex-direction: column;
   align-items: flex-start;
   gap: 60px;
+  width: 100%;
 `;
 
-const SectionInner = styled.div`
-  padding-left: 24px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 20px;
-  align-self: stretch;
-`;
-
-const ButtonPlaceRight = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  align-self: stretch;
-`;
-
-type PutActivityReportBody = z.infer<typeof apiAct003.requestBody>;
+// TODO. 활동기간 리스트 추가, 파일업로드 추가
 const ActivityReportEditFrame: React.FC<{ id: string }> = ({ id }) => {
-  // TODO: 실제 프로필 사용
+  const formCtx = useForm<ApiAct003RequestBody>({ mode: "all" });
+
   const { data, isLoading, isError } = useGetActivityReport(
     "undergraduate",
     Number(id),
   );
-  const { mutate } = usePutActivityReport();
-  const [formData, setFormData] = useState<PutActivityReportBody>();
-  const [duration, setDuration] = useState<[string, string]>(["", ""]);
+
   const router = useRouter();
-
-  const handleFormChange = useCallback(
-    <K extends keyof PutActivityReportBody>(
-      key: K,
-      value: PutActivityReportBody[K],
-    ) => {
-      if (!formData) return;
-      setFormData({ ...formData, [key]: value });
-    },
-    [formData],
-  );
-
-  useEffect(() => {
-    const parsedStartDate = parse(duration[0], "yyyy.MM.dd", new Date());
-    const parsedEndDate = parse(duration[1], "yyyy.MM.dd", new Date());
-
-    if (isValid(parsedStartDate) && isValid(parsedEndDate)) {
-      handleFormChange("durations", [
-        { startTerm: parsedStartDate, endTerm: parsedEndDate },
-      ]);
-    }
-  }, [duration]);
 
   useEffect(() => {
     if (data) {
-      setFormData(data);
-      if (data.durations.length) {
-        const { startTerm, endTerm } = data.durations[0];
-        const formattedStart = formatDotDate(startTerm);
-        const formattedEnd = formatDotDate(endTerm);
-        setDuration([formattedStart, formattedEnd]);
-      }
+      /* NOTE: (@dora) request body of put should not include clubId */
+      formCtx.reset({
+        name: data.name,
+        activityTypeEnumId: data.activityTypeEnumId,
+        durations: data.durations,
+        location: data.location,
+        purpose: data.purpose,
+        detail: data.detail,
+        evidence: data.evidence,
+        evidenceFiles: data.evidenceFiles,
+        participants: data.participants,
+      });
+      /* TODO: (@dora) refactor to use lodash */
+      // formCtx.reset(_.omit(activityReportData, "clubId"));
     }
-  }, [data]);
+  }, [data, formCtx]);
 
-  const handleSubmit = useCallback(() => {
-    if (!formData) return;
-    mutate(
-      { activityId: Number(id), body: formData },
-      {
-        onSuccess: () => {
-          router.push("/manage-club/activity-report");
+  const { mutate } = usePutActivityReportForNewClub();
+
+  const submitHandler = useCallback(
+    (_data: ApiAct003RequestBody, e: React.BaseSyntheticEvent) => {
+      e.preventDefault();
+      mutate(
+        {
+          params: { activityId: Number(id) },
+          body: {
+            ..._data,
+            durations: _data.durations.map(({ startTerm, endTerm }) => ({
+              startTerm,
+              endTerm,
+            })),
+            participants: _data.participants.map(({ studentId }) => ({
+              studentId,
+            })),
+          },
         },
-      },
-    );
-  }, [formData, id, mutate, router]);
+        {
+          onSuccess: () => {
+            router.push("/manage-club/activity-report");
+          },
+        },
+      );
+    },
+    [id, router, mutate],
+  );
+
+  const handleSubmit = (e: React.BaseSyntheticEvent) => {
+    formCtx.handleSubmit(_data => submitHandler(_data, e))();
+  };
+
+  if (!data) return null;
 
   return (
     <ActivityReportMainFrameInner>
@@ -118,101 +96,14 @@ const ActivityReportEditFrame: React.FC<{ id: string }> = ({ id }) => {
         enableLast
       />
       <AsyncBoundary isLoading={isLoading} isError={isError}>
-        <SectionTitle>활동 정보</SectionTitle>
-        <SectionInner>
-          <Card outline padding="32px" gap={32}>
-            <TextInput
-              label="활동명"
-              placeholder="활동명을 입력해주세요"
-              name="name"
-              value={formData?.name}
-              handleChange={value => handleFormChange("name", value)}
-            />
-            <FlexWrapper gap={32} direction="row">
-              <Select
-                label="활동 분류"
-                items={[
-                  {
-                    value: ActivityTypeEnum.matchedInternalActivity.toString(),
-                    label: "동아리 성격에 합치하는 내부 활동",
-                    selectable: true,
-                  },
-                  {
-                    value: ActivityTypeEnum.matchedExternalActivity.toString(),
-                    label: "동아리 성격에 합치하는 외부 활동",
-                    selectable: true,
-                  },
-                  {
-                    value: ActivityTypeEnum.notMatchedActivity.toString(),
-                    label: "동아리 성격에 합치하지 않는 활동",
-                    selectable: true,
-                  },
-                ]}
-                value={formData?.activityTypeEnumId.toString()}
-                onChange={value =>
-                  handleFormChange("activityTypeEnumId", Number(value))
-                }
-              />
-              <DateRangeInput
-                label={["활동 기간", "\u200B"]}
-                startValue={duration[0]}
-                endValue={duration[1]}
-                limitStartValue="2000.01.01"
-                limitEndValue="3000.01.01"
-                placeholder="날짜를 입력해주세요"
-                onChange={value => {
-                  const [start, end] = value.split("|");
-                  setDuration([start, end]);
-                }}
-                useDays
-              />
-            </FlexWrapper>
-            <TextInput
-              label="활동 장소"
-              placeholder="활동 장소를 입력해주세요"
-              name="location"
-              value={formData?.location}
-              handleChange={value => handleFormChange("location", value)}
-            />
-            <TextInput
-              label="활동 목적"
-              placeholder="활동 목적을 입력해주세요"
-              name="purpose"
-              value={formData?.purpose}
-              handleChange={value => handleFormChange("purpose", value)}
-            />
-            <TextInput
-              area
-              label="활동 내용"
-              placeholder="활동 내용을 입력해주세요"
-              name="detail"
-              value={formData?.detail}
-              handleChange={value => handleFormChange("detail", value)}
-            />
-          </Card>
-        </SectionInner>
-        <SectionTitle>활동 인원</SectionTitle>
-        <SectionInner>
-          <SelectParticipant data={mockParticipantData} />
-        </SectionInner>
-        <SectionTitle>활동 증빙</SectionTitle>
-        <SectionInner>
-          <Card outline padding="32px" gap={32}>
-            <FileUpload />
-            <TextInput
-              area
-              placeholder="(선택) 활동 증빙에 대해서 작성하고 싶은 것이 있다면 입력해주세요"
-              name="evidence"
-              value={formData?.evidence}
-              handleChange={value => handleFormChange("evidence", value)}
-            />
-          </Card>
-        </SectionInner>
-        <ButtonPlaceRight>
-          <Button type="default" onClick={handleSubmit}>
-            저장
-          </Button>
-        </ButtonPlaceRight>
+        <ActivityReportForm
+          /* eslint-disable  @typescript-eslint/no-explicit-any */
+          clubId={data.clubId}
+          formCtx={formCtx as any}
+          onCancel={() => {}}
+          onSubmit={handleSubmit}
+          asModal={false}
+        />
       </AsyncBoundary>
     </ActivityReportMainFrameInner>
   );
