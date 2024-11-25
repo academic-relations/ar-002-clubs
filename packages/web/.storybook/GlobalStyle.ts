2c906bf49a919980ec3c12b83e5e0d07b1ddda80
YINGMEI <j.yeongmee@gmail.com>
2024-11-25 23:50:07 +0900

    fix: apply fonts to storybook

--- packages/web/.storybook/GlobalStyle.ts
+++ packages/web/.storybook/GlobalStyle.ts
@@ -3,9 +3,28 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+@import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+
 :root {
   background: linear-gradient(180deg, #eafafc 0px, #ffffff 339.67px);
   min-height: 100vh;
+  --next-font-family-pretendard: 'Pretendard', sans-serif;
+  --next-font-family-nanum-square: 'NanumSquare', sans-serif;
+  --next-font-family-raleway: 'Raleway', sans-serif;
+}
+
+
+.nanum-square-font {
+  font-family: var(--next-font-family-nanum-square);
+}
+
+.pretendard-font {
+  font-family: var(--next-font-family-pretendard);
+}
+
+.raleway-font {
+  font-family: var(--next-font-family-raleway);
 }
 
 * {
@@ -17,6 +36,17 @@ const GlobalStyle = createGlobalStyle`
   color: #333333;
   font-weight: 400;
   font-family: var(--next-font-family-pretendard);
+}
+
+p,
+body {
+  margin: 0;
+}
+
+ul,
+li {
+  margin: 0;
+  padding: 0;
 }
 
 button {
@@ -33,6 +63,14 @@ a {
 main {
   overflow-x: hidden;
   min-width: 320px;
+}
+
+.viewer-download {
+  color: #fff;
+  font-family: FontAwesome, serif;
+  font-size: 0.75rem;
+  line-height: 1.5rem;
+  text-align: center;
 }
 `;
 
