19b6fd8c8d02470b59aa4503fa1e0dd3f40f317d
Woojeong Choi <gldeel@kaist.ac.kr>
2024-11-27 13:37:42 +0900

    chore: remove division

--- packages/web/src/features/my/frames/MyChangesFrame.tsx
+++ packages/web/src/features/my/frames/MyChangesFrame.tsx
@@ -6,10 +6,6 @@ import { ClubDelegateChangeRequestStatusEnum } from "@sparcs-clubs/interface/com
 
 import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import useGetUserProfile from "@sparcs-clubs/web/common/services/getUserProfile";
-import { ChangeDivisionPresidentStatusEnum } from "@sparcs-clubs/web/constants/changeDivisionPresident";
-import MyChangeDivisionPresident, {
-  MyChangeDivisionPresidentStatusEnum,
-} from "@sparcs-clubs/web/features/my/components/MyChangeDivisionPresident";
 import MyChangeRepresentative from "@sparcs-clubs/web/features/my/components/MyChangeRepresentative";
 import { useGetMyDelegateRequest } from "@sparcs-clubs/web/features/my/services/getMyDelegateRequest";
 
@@ -17,33 +13,10 @@ export const MyChangesFrame = () => {
   // TODO: clb014 api 구현되면 refetch 테스트
 
   const { data, isLoading, isError, refetch } = useGetMyDelegateRequest();
-  const fetchDivisionPresident = () => {}; // TODO
 
   const { data: myProfile } = useGetUserProfile();
 
   const [type, setType] = useState<"Requested" | "Finished">("Finished");
-  const [
-    mockHasDivisionPresidentChangeNotice,
-    setMockHasDivisionPresidentChangeNotice,
-  ] = useState(false);
-  const mockIsDivisionPresident = true; // TODO: divisionPresident == user
-  const mockChangeDivisionPresident = () => {}; // TODO: change divisionPresident
-  const mockRejectDivisionPresidentChange = () => {}; // TODO: set divisionPresidentChange status to "Rejected"
-
-  const [divisionChangeRequestStatus, setDivisionChangeRequestStatus] =
-    useState<MyChangeDivisionPresidentStatusEnum>(
-      ChangeDivisionPresidentStatusEnum.Requested,
-    );
-
-  const onDivisionPresidentChangeRequestConfirmed = () => {
-    setDivisionChangeRequestStatus(ChangeDivisionPresidentStatusEnum.Confirmed); // TODO: 변경 요청 보내고 다시 받아오는 방식 (api 구현 이후)
-    mockChangeDivisionPresident();
-  };
-
-  const onDivisionPresidentChangeRequestRejected = () => {
-    setMockHasDivisionPresidentChangeNotice(false); // TODO: 변경 요청 보내고 다시 받아오는 방식 (api 구현 이후)
-    mockRejectDivisionPresidentChange();
-  };
 
   useEffect(() => {
     switch (data?.requests[0]?.clubDelegateChangeRequestStatusEnumId) {
@@ -68,16 +41,6 @@ export const MyChangesFrame = () => {
           newRepresentative={`${myProfile?.studentNumber} ${myProfile?.name}`}
           refetch={refetch}
           requestId={data?.requests[0]?.id}
-        />
-      )}
-      {mockHasDivisionPresidentChangeNotice && (
-        <MyChangeDivisionPresident
-          status={divisionChangeRequestStatus}
-          change={["20210227 박병찬", "20200510 이지윤"]}
-          fetch={fetchDivisionPresident}
-          isDivisionPresident={mockIsDivisionPresident}
-          onConfirmed={onDivisionPresidentChangeRequestConfirmed}
-          onRejected={onDivisionPresidentChangeRequestRejected}
         />
       )}
     </AsyncBoundary>
