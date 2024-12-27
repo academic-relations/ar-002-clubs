f13c73b1ce48ff652d8c9d84e8c5c29b0ab76e25
Nicole Lee <nicolelee2001@gmail.com>
2024-12-27 17:43:33 +0900

    feat: table header

--- packages/web/src/features/manage-club/frames/ProfessorManageClubFrame/ClubActivitySection.tsx
+++ packages/web/src/features/manage-club/frames/ProfessorManageClubFrame/ClubActivitySection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
+import MoreDetailTitle from "@sparcs-clubs/web/common/components/MoreDetailTitle";
 
 import ProfessorActivityReportTable from "@sparcs-clubs/web/features/activity-report/components/tables/ProfessorActivityReportTable";
 
@@ -15,7 +16,13 @@ const ClubActivitySection: React.FC<ClubActivitySectionProps> = ({
   <FoldableSectionTitle title="동아리 활동">
     <FlexWrapper direction="column" gap={40}>
       <FlexWrapper direction="column" gap={20}>
+        <MoreDetailTitle
+          title="활동 보고서"
+          moreDetail="내역 더보기"
+          moreDetailPath="/manage-club/activity-report"
+        />
         <ProfessorActivityReportTable clubId={clubId} />
+
         {/* TODO: 지원금 테이블 추가 */}
       </FlexWrapper>
     </FlexWrapper>
