a363da724eebc9b176726f132d23f1594ead6807
Nicole Lee <nicolelee2001@gmail.com>
2024-12-27 17:07:31 +0900

    feat: add table for professor activity report

--- packages/web/src/features/manage-club/frames/ProfessorManageClubFrame/ClubActivitySection.tsx
+++ packages/web/src/features/manage-club/frames/ProfessorManageClubFrame/ClubActivitySection.tsx
@@ -3,24 +3,23 @@ import React from "react";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
 
+import ProfessorActivityReportTable from "@sparcs-clubs/web/features/activity-report/components/tables/ProfessorActivityReportTable";
+
 interface ClubActivitySectionProps {
   clubId: number;
 }
 
 const ClubActivitySection: React.FC<ClubActivitySectionProps> = ({
   clubId,
-}) => {
-  console.log(clubId);
-
-  return (
-    <FoldableSectionTitle title="동아리 활동">
-      <FlexWrapper direction="column" gap={40}>
-        <FlexWrapper direction="column" gap={20}>
-          TBD
-        </FlexWrapper>
+}) => (
+  <FoldableSectionTitle title="동아리 활동">
+    <FlexWrapper direction="column" gap={40}>
+      <FlexWrapper direction="column" gap={20}>
+        <ProfessorActivityReportTable clubId={clubId} />
+        {/* TODO: 지원금 테이블 추가 */}
       </FlexWrapper>
-    </FoldableSectionTitle>
-  );
-};
+    </FlexWrapper>
+  </FoldableSectionTitle>
+);
 
 export default ClubActivitySection;
--- packages/web/src/features/manage-club/frames/ProfessorManageClubFrame/ClubMemberSection.tsx
+++ packages/web/src/features/manage-club/frames/ProfessorManageClubFrame/ClubMemberSection.tsx
@@ -3,22 +3,20 @@ import React from "react";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import FoldableSectionTitle from "@sparcs-clubs/web/common/components/FoldableSectionTitle";
 
+import ProfessorClubMemberTable from "@sparcs-clubs/web/features/activity-report/components/tables/ProfessorClubMemberTable";
+
 interface ClubMemberSectionProps {
   clubId: number;
 }
 
-const ClubMemberSection: React.FC<ClubMemberSectionProps> = ({ clubId }) => {
-  console.log(clubId);
-
-  return (
-    <FoldableSectionTitle title="회원 명단">
-      <FlexWrapper direction="column" gap={40}>
-        <FlexWrapper direction="column" gap={20}>
-          TBD
-        </FlexWrapper>
+const ClubMemberSection: React.FC<ClubMemberSectionProps> = ({ clubId }) => (
+  <FoldableSectionTitle title="회원 명단">
+    <FlexWrapper direction="column" gap={40}>
+      <FlexWrapper direction="column" gap={20}>
+        <ProfessorClubMemberTable clubId={clubId} />
       </FlexWrapper>
-    </FoldableSectionTitle>
-  );
-};
+    </FlexWrapper>
+  </FoldableSectionTitle>
+);
 
 export default ClubMemberSection;
