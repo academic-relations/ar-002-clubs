a4211bea3d6849e89865cc59a245e8811a882d81
Nicole Lee <nicolelee2001@gmail.com>
2024-12-28 20:30:52 +0900

    fix: type

--- packages/api/src/feature/activity/service/activity.service.ts
+++ packages/api/src/feature/activity/service/activity.service.ts
@@ -927,14 +927,10 @@ export default class ActivityService {
           await this.activityRepository.selectDurationByActivityId(row.id);
         return {
           ...row,
-          startTerm: duration.reduce(
-            (prev, curr) => (prev < curr.startTerm ? prev : curr.startTerm),
-            duration[0].startTerm,
-          ),
-          endTerm: duration.reduce(
-            (prev, curr) => (prev > curr.endTerm ? prev : curr.endTerm),
-            duration[0].endTerm,
-          ),
+          durations: duration.map(e => ({
+            startTerm: e.startTerm,
+            endTerm: e.endTerm,
+          })),
         };
       }),
     );
@@ -944,8 +940,7 @@ export default class ActivityService {
       activityStatusEnumId: row.activityStatusEnumId,
       name: row.name,
       activityTypeEnumId: row.activityTypeEnumId,
-      startTerm: row.startTerm,
-      endTerm: row.endTerm,
+      durations: row.durations,
       professorApprovedAt: row.professorApprovedAt,
     }));
   }
