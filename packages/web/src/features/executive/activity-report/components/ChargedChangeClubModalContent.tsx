c136317d9dda3974e2bbdcf19744a6f9fd7b697a
Woojeong Choi <gldeel@kaist.ac.kr>
2025-01-15 00:25:39 +0900

    feat: apply select for ChargedChangeClubModalContent

---
+++ packages/web/src/features/executive/activity-report/components/ChargedChangeClubModalContent.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+
+import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
+import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
+import Select from "@sparcs-clubs/web/common/components/Select";
+import Typography from "@sparcs-clubs/web/common/components/Typography";
+
+import useGetActivityClubChargeAvailableExecutives from "../services/useGetActivityClubChargeAvailableExecutives";
+
+// import ChargedChangeClubModalTable from "./ChargedChangeClubModalTable";
+
+interface ChargedChangeClubModalContentProps {
+  selectedClubIds: number[];
+  selectedExecutiveId: number | null;
+  setSelectedExecutiveId: (value: number | null) => void;
+}
+
+interface ChargeableExecutive {
+  label: string;
+  value: number;
+}
+
+const ChargedChangeClubModalContent: React.FC<
+  ChargedChangeClubModalContentProps
+> = ({ selectedClubIds, selectedExecutiveId, setSelectedExecutiveId }) => {
+  const { data, isLoading, isError } =
+    useGetActivityClubChargeAvailableExecutives({ clubIds: selectedClubIds });
+  const [selectedExecutiveIdModal, setSelectedExecutiveIdModal] = useState<
+    number | null
+  >(selectedExecutiveId);
+
+  const [chargeableExecutives, setChargeableExecutives] = useState<
+    ChargeableExecutive[]
+  >([]);
+
+  useEffect(() => {
+    if (data) {
+      setChargeableExecutives(
+        data.executives.map(executive => ({
+          label: `${executive.name} (${executive.studentNumber})`,
+          value: executive.id,
+        })),
+      );
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (selectedExecutiveIdModal !== null) {
+      setSelectedExecutiveId(selectedExecutiveIdModal);
+    }
+  }, [selectedExecutiveIdModal]);
+
+  return (
+    <AsyncBoundary isLoading={isLoading} isError={isError}>
+      <FlexWrapper
+        direction="column"
+        gap={12}
+        style={{ width: "min(600px, 80vw)" }}
+      >
+        <Select
+          items={chargeableExecutives}
+          value={selectedExecutiveIdModal}
+          onChange={setSelectedExecutiveIdModal}
+          label="동아리별 활동 보고서 담당자"
+        />
+        {/* <ChargedChangeClubModalTable/> */}
+        <Typography fs={14} lh={20} fw="MEDIUM" style={{ textAlign: "left" }}>
+          * 동아리별 활동 보고서 담당자를 변경할 경우, 해당 동아리가 작성한 활동
+          보고서의 담당자가 모두 해당 담당자로 변경됩니다.
+        </Typography>
+      </FlexWrapper>
+    </AsyncBoundary>
+  );
+};
+
+export default ChargedChangeClubModalContent;
--- packages/web/src/features/executive/activity-report/frames/ExecutiveActivityReportFrame.tsx
+++ packages/web/src/features/executive/activity-report/frames/ExecutiveActivityReportFrame.tsx
@@ -8,10 +8,9 @@ import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import Modal from "@sparcs-clubs/web/common/components/Modal";
 import CancellableModalContent from "@sparcs-clubs/web/common/components/Modal/CancellableModalContent";
 import SearchInput from "@sparcs-clubs/web/common/components/SearchInput";
-import Select from "@sparcs-clubs/web/common/components/Select";
-import Typography from "@sparcs-clubs/web/common/components/Typography";
 
 import ActivityReportStatistic from "../components/ActivityReportStatistic";
+import ChargedChangeClubModalContent from "../components/ChargedChangeClubModalContent";
 import ExecutiveActivityChargedTable from "../components/ExecutiveActivityChargedTable";
 import ExecutiveActivityClubTable from "../components/ExecutiveActivityClubTable";
 import useGetExecutiveActivities from "../services/useGetExecutiveActivities";
@@ -22,35 +21,19 @@ const ExecutiveActivityReportFrame = () => {
   const { data, isLoading, isError } = useGetExecutiveActivities();
 
   const [selectedClubIds, setSelectedClubIds] = useState<number[]>([]);
-  const [selectedExecutiveId, setSelectedExecutiveId] = useState<
-    number | null
-  >();
+  const [selectedExecutiveId, setSelectedExecutiveId] = useState<number | null>(
+    null,
+  );
 
   const openChargedChangeModal = () => {
     overlay.open(({ isOpen, close }) => (
       <Modal isOpen={isOpen}>
         <CancellableModalContent onConfirm={close} onClose={close}>
-          <FlexWrapper
-            direction="column"
-            gap={12}
-            style={{ width: "min(600px, 80vw)" }}
-          >
-            <Select
-              items={[]}
-              value={selectedExecutiveId}
-              onChange={setSelectedExecutiveId}
-              label="동아리별 활동 보고서 담당자"
-            />
-            <Typography
-              fs={14}
-              lh={20}
-              fw="MEDIUM"
-              style={{ textAlign: "left" }}
-            >
-              * 동아리별 활동 보고서 담당자를 변경할 경우, 해당 동아리가 작성한
-              활동 보고서의 담당자가 모두 해당 담당자로 변경됩니다.
-            </Typography>
-          </FlexWrapper>
+          <ChargedChangeClubModalContent
+            selectedClubIds={selectedClubIds}
+            selectedExecutiveId={selectedExecutiveId}
+            setSelectedExecutiveId={setSelectedExecutiveId}
+          />
         </CancellableModalContent>
       </Modal>
     ));
