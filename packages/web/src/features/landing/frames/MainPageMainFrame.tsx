4c5528dc11f44bafd20ac39b70e27e8776657329
jinhyeonkwon <athexplorer@kaist.ac.kr>
2025-01-15 22:23:47 +0900

    feat: easteregg

--- packages/web/src/features/landing/frames/MainPageMainFrame.tsx
+++ packages/web/src/features/landing/frames/MainPageMainFrame.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 import styled from "styled-components";
 
@@ -59,33 +59,82 @@ const ResponsiveWrapper = styled(FlexWrapper)`
   }
 `;
 
-const MainPageMainFrame: React.FC = () => (
-  <ResponsiveWrapper direction="column" gap={60}>
-    <Banner icon="star">
-      현재 Clubs는 동아리 등록 신청을 위한 베타 서비스로 운영되고 있습니다.
-      <br />
-      문의나 피드백은 하단 채널톡 버튼을 통해 제보 부탁드립니다!
-    </Banner>
-    <PageTitleWrapper>
-      <ResponsiveSloganTypography
-        fw="SEMIBOLD"
-        style={{ width: "fit-content" }}
-      >
-        <StyledSpan>동아리</StyledSpan>의 모든 것을 한번에
-      </ResponsiveSloganTypography>
-      <ResponsiveSloganTypography
-        fw="SEMIBOLD"
-        style={{ width: "fit-content" }}
-      >
-        동아리연합회 통합 플랫폼, <BreakBeforeStyledSpan />
-        <StyledSpan>Clubs</StyledSpan>입니다
-      </ResponsiveSloganTypography>
-    </PageTitleWrapper>
-    <NoticeAndServiceWrapper>
-      <NoticeSectionFrame />
-      {/* <ServiceSectionFrame /> */}
-    </NoticeAndServiceWrapper>
-  </ResponsiveWrapper>
-);
+const easterEgg = `%c
+                        -     
+                       -=.    
+                      -===    
+                     =====.   
+:                   -=====-   
++-                 -=======   
+++=               =========-  
++++=.            -==========  
++++++:          ============- 
+++++++-        ============== 
++++++++=      ===============:
+++++++++=.  .================:
++++++++++= .===============-  
+++++++++=. ===============.   
++++++++=..==============:     
+++++++=..==============       
++++++= .=============.        
+++++=..============:          
++++=..===========-            
+++= .===========-.            
++=.:==========+++++++===--:::.  
+= :=======+++++++++++++++===: 
+ :====+++++++++++++++=====:   
+.---====++++++++=======-.     
+             :=======-.       
+            :======-.         
+           :=====:            
+          -====:              
+         -===:                
+        -=-.                  
+       =-                     
+     .:.                       
+%c찾으셨군요!
+SPARCS의 여정에 함께하지 않으시겠어요?
+https://sparcs.org/
+https://apply.sparcs.org/ (리크루팅 기간에 접속 가능)
+     `;
+
+const MainPageMainFrame: React.FC = () => {
+  useEffect(() => {
+    console.log(
+      easterEgg,
+      "color:#eba12a; font-weight: bold; margin: auto",
+      "color:#000000;",
+    );
+    console.log();
+  }, []);
+  return (
+    <ResponsiveWrapper direction="column" gap={60}>
+      <Banner icon="star">
+        현재 Clubs는 동아리 등록 신청을 위한 베타 서비스로 운영되고 있습니다.
+        <br />
+        문의나 피드백은 하단 채널톡 버튼을 통해 제보 부탁드립니다!
+      </Banner>
+      <PageTitleWrapper>
+        <ResponsiveSloganTypography
+          fw="SEMIBOLD"
+          style={{ width: "fit-content" }}
+        >
+          <StyledSpan>동아리</StyledSpan>의 모든 것을 한번에
+        </ResponsiveSloganTypography>
+        <ResponsiveSloganTypography
+          fw="SEMIBOLD"
+          style={{ width: "fit-content" }}
+        >
+          동아리연합회 통합 플랫폼, <BreakBeforeStyledSpan />
+          <StyledSpan>Clubs</StyledSpan>입니다
+        </ResponsiveSloganTypography>
+      </PageTitleWrapper>
+      <NoticeAndServiceWrapper>
+        <NoticeSectionFrame />
+        {/* <ServiceSectionFrame /> */}
+      </NoticeAndServiceWrapper>
+    </ResponsiveWrapper>
+  );
+};
 
 export default MainPageMainFrame;
