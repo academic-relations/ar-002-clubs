2a7390e625d9bbb662dbf73a800c6ad86e482b8b
Woojeong Choi <gldeel@kaist.ac.kr>
2025-01-18 04:03:02 +0900

    feat: impl dashboard button

---
+++ packages/web/src/features/executive/components/DashboardButton.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import styled from "styled-components";
+
+import Icon from "@sparcs-clubs/web/common/components/Icon";
+import Typography from "@sparcs-clubs/web/common/components/Typography";
+import colors from "@sparcs-clubs/web/styles/themes/colors";
+
+const DashboardButtonWrapper = styled.div`
+  padding: 12px 20px;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.WHITE};
+  box-shadow: ${({ theme }) => theme.shadow.md};
+  gap: 20px;
+  flex-direction: row;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+`;
+
+const DashboardButton: React.FC<{
+  onClick?: () => void;
+  text: string;
+}> = ({ onClick = () => {}, text }) => (
+  <DashboardButtonWrapper onClick={onClick}>
+    <Typography fs={16} lh={20} fw="MEDIUM">
+      {text}
+    </Typography>
+    <Icon type="chevron_right" size={20} color={colors.BLACK} />
+  </DashboardButtonWrapper>
+);
+
+export default DashboardButton;
--- packages/web/src/features/executive/frames/ExecutiveDashboardFrame.tsx
+++ packages/web/src/features/executive/frames/ExecutiveDashboardFrame.tsx
@@ -4,6 +4,8 @@ import AsyncBoundary from "@sparcs-clubs/web/common/components/AsyncBoundary";
 import FlexWrapper from "@sparcs-clubs/web/common/components/FlexWrapper";
 import SectionTitle from "@sparcs-clubs/web/common/components/SectionTitle";
 
+import DashboardButton from "../components/DashboardButton";
+
 const ExecutiveDashboardFrame = () => {
   const isLoading = false;
   const isError = false;
@@ -17,9 +19,17 @@ const ExecutiveDashboardFrame = () => {
       >
         <FlexWrapper direction="column" gap={20} style={{ flex: 1 }}>
           <SectionTitle>동아리 / 회원 등록</SectionTitle>
+          <FlexWrapper direction="column" gap={12}>
+            <DashboardButton text="동아리 등록 신청 내역" />
+            <DashboardButton text="회원 등록 신청 내역" />
+          </FlexWrapper>
         </FlexWrapper>
         <FlexWrapper direction="column" gap={20} style={{ flex: 1 }}>
           <SectionTitle>활동 보고서 / 지원금</SectionTitle>
+          <FlexWrapper direction="column" gap={12}>
+            <DashboardButton text="활동 보고서 작성 내역" />
+            <DashboardButton text="지원금 신청 내역" />
+          </FlexWrapper>
         </FlexWrapper>
       </FlexWrapper>
     </AsyncBoundary>
