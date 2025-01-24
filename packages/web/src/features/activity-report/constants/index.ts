a8fb28ec541436b5e7f089ae71cc1cc504de060d
YINGMEI <j.yeongmee@gmail.com>
2025-01-25 03:38:25 +0900

    fix: connect api to activity report info text

--- packages/web/src/features/activity-report/constants/index.ts
+++ packages/web/src/features/activity-report/constants/index.ts
@@ -1,1 +1,26 @@
+import { ActivityDeadlineEnum } from "@sparcs-clubs/interface/common/enum/activity.enum";
+import { getDate, getMonth, getYear } from "date-fns";
+
 export const MAX_ACTIVITY_REPORT_COUNT = 20;
+
+export const newActivityReportListSectionInfoText = (
+  semester: string,
+  status: string,
+  deadline?: Date,
+) =>
+  `현재는 ${semester}학기 활동 보고서 ${status} 기간입니다 (${status} 마감 : ${deadline ? `${getYear(deadline)}년 ${getMonth(deadline) + 1}월 ${getDate(deadline)}일 23:59` : "-"})`;
+
+export const activityDeadlineEnumToString = (
+  deadline?: ActivityDeadlineEnum,
+) => {
+  switch (deadline) {
+    case ActivityDeadlineEnum.Activity:
+      return "활동";
+    case ActivityDeadlineEnum.Upload:
+      return "작성";
+    case ActivityDeadlineEnum.Modification:
+      return "수정";
+    default:
+      return "예외";
+  }
+};
