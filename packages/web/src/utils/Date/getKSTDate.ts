787e6013817fbf146f9fb88ef125f3f8f888fcfc
YINGMEI <j.yeongmee@gmail.com>
2025-01-26 19:01:44 +0900

    fix: update frontend getKSTDate logic

--- packages/web/src/utils/Date/getKSTDate.ts
+++ packages/web/src/utils/Date/getKSTDate.ts
@@ -1,7 +1,21 @@
+// 시간을 제회한 날짜만 KST로 변경
+// 예시: 입력 date가 2024-10-18 15:00 일 경우 getKSTDate 씌워서 api 요청 보내면
+// 2024.10.18 00:00 으로 요청 보내짐 (백에서도 그대로 받아서 그대로 프론트에 데이터 보냄)
+// 즉 항상 시간은 00:00 이어야 함
 export function getKSTDate(date: Date): Date {
-  const kstOffset = 9 * 60; // KST: UTC+9
-  const utcDate = new Date(
-    new Date(date).getTime() - new Date(date).getTimezoneOffset() * 60 * 1000,
+  const localDateOnly = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
   );
-  return new Date(utcDate.getTime() + kstOffset * 60 * 1000);
+
+  const kstOffset = 9 * 60; // KST: UTC+9
+  return new Date(localDateOnly.getTime() + kstOffset * 60 * 1000);
+}
+
+// 시간 포함하여 KST 변경
+export function getKSTDateTime(date: Date): Date {
+  const kstOffset = 9 * 60; // KST: UTC+9
+
+  return new Date(new Date(date).getTime() + kstOffset * 60 * 1000);
 }
