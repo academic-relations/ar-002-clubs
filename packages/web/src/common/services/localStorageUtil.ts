71e06b1398178b942b2061257fe8f419ba28f712
YINGMEI <j.yeongmee@gmail.com>
2025-02-03 01:55:18 +0900

    feat: add expireTime to LocalStorageUtil

--- packages/web/src/common/services/localStorageUtil.ts
+++ packages/web/src/common/services/localStorageUtil.ts
@@ -1,13 +1,28 @@
+import logger from "@sparcs-clubs/web/utils/logger";
+
+type StorageValue<T> = {
+  value: T;
+  expireTime: number | undefined;
+};
 class LocalStorageUtil {
   /**
    * 임시저장을 할 때 호출해야 하는 함수. 로컬 스토리지에 임시 데이터를 저장함.
    *
    * name: 로컬 스토리지에 이 이름대로 저장됨
    * tempData: 저장할 데이터 (추후 getLocalStorage를 통해 불러올 최종 데이터 구조와 똑같거나 typecasting 가능할 정도로 구조가 유사해야 함)
+   * ttl: 만료 시간(ms). 디폴트로 24시간 뒤에 만료됨
    *
    */
-  public static save = <T>(name: string, tempData: T) => {
-    localStorage.setItem(name, JSON.stringify(tempData));
+  public static save = <T>(
+    name: string,
+    tempData: T,
+    ttl: number = 60 * 1000,
+  ): void => {
+    const data: StorageValue<T> = {
+      value: tempData,
+      expireTime: Date.now() + ttl,
+    };
+    localStorage.setItem(name, JSON.stringify(data));
   };
 
   /**
@@ -18,14 +33,20 @@ class LocalStorageUtil {
    *
    */
   public static get = <T>(name: string): T | undefined => {
+    const rawData = localStorage.getItem(name);
+
+    if (!rawData) return undefined;
+
     try {
-      const rawData = localStorage.getItem(name);
-      if (rawData != null) {
-        const storedData = JSON.parse(rawData) as T;
-        return storedData;
+      const storedData: StorageValue<T> = JSON.parse(rawData);
+
+      if (storedData.expireTime && Date.now() > storedData.expireTime) {
+        this.remove(name);
+        return undefined;
       }
-      return undefined;
-    } catch {
+      return storedData.value;
+    } catch (error) {
+      logger.log("Failed to parse localStorage Item: ", error);
       return undefined;
     }
   };
@@ -40,6 +61,14 @@ class LocalStorageUtil {
   public static remove = (name: string) => {
     localStorage.removeItem(name);
   };
+
+  /**
+   * 로컬 스토리지에 해당 키에 대한 값이 존재하는지 확인함
+   *
+   * name: 확인할 데이터의 키
+   *
+   */
+  public static hasValue = (name: string) => this.get(name) != null;
 }
 
 export default LocalStorageUtil;
