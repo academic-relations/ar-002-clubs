10988054eeca04dfebd3465295914feb244680a7
YINGMEI <j.yeongmee@gmail.com>
2025-02-22 04:36:41 +0900

    chore: update restore draft modal default sub text

--- packages/web/src/common/components/Modal/RestoreDraftModal.tsx
+++ packages/web/src/common/components/Modal/RestoreDraftModal.tsx
@@ -26,7 +26,8 @@ const RestoreDraftModal: React.FC<RestoreDraftModalProps> = ({
     >
       {mainText ?? `임시 저장된 데이터가 있습니다. 불러오시겠습니까?`}
       <Typography color="GRAY.600" fs={12} lh={16} fw="REGULAR">
-        ‘새로 작성하기’를 선택하면 기존 임시 저장된 데이터가 삭제됩니다.
+        ‘새로 작성하기’를 선택하면 기존 임시 저장된 데이터가 삭제됩니다. 임시
+        저장 된 데이터는 24시간 후 자동 삭제됩니다.
       </Typography>
     </CancellableModalContent>
   </Modal>
