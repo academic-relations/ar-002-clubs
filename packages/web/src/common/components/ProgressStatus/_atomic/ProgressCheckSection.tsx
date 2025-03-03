89782e5383b3a440ddc4d741eeeadae5b5dd8f35
edge <edge@sparcs.org>
2025-03-03 21:48:31 +0900

    feat: add progress check section to progress status

---
+++ packages/web/src/common/components/ProgressStatus/_atomic/ProgressCheckSection.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+
+import { ProgressCheckSectionStatusEnum } from "../progressCheckStationStatus";
+import Progress from "./Progress";
+import ProgressLine from "./ProgressLine";
+
+export interface StatusAndDate {
+  status: ProgressCheckSectionStatusEnum;
+  date: Date | undefined;
+}
+interface ProgressCheckSectionProps {
+  labels: string[];
+  progress: StatusAndDate[];
+}
+
+const ProgressCheckSectionWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+  align-items: flex-start;
+  height: 76px;
+  width: 100%;
+  padding: 0 48px;
+`;
+
+const ProgressLineWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  margin-top: 12px;
+`;
+
+const ProgressCheckSection: React.FC<ProgressCheckSectionProps> = ({
+  labels,
+  progress,
+}) => (
+  <ProgressCheckSectionWrapper>
+    {labels.map((label, index) => (
+      <React.Fragment key={String(index) + label}>
+        <Progress
+          status={
+            progress[index]
+              ? progress[index].status
+              : ProgressCheckSectionStatusEnum.Pending
+          }
+          label={label}
+          date={progress[index] ? progress[index].date : undefined}
+        />
+        {index < labels.length - 1 && (
+          <ProgressLineWrapper>
+            <ProgressLine
+              status={
+                progress[index + 1]
+                  ? progress[index].status
+                  : ProgressCheckSectionStatusEnum.Pending
+              }
+            />
+          </ProgressLineWrapper>
+        )}
+      </React.Fragment>
+    ))}
+  </ProgressCheckSectionWrapper>
+);
+export default ProgressCheckSection;
---
+++ packages/web/src/common/components/ProgressStatus/progressCheckStationStatus.ts
@@ -0,0 +1,5 @@
+export enum ProgressCheckSectionStatusEnum {
+  Approved, // 체크
+  Canceled, // X
+  Pending, // 빈 원
+}
