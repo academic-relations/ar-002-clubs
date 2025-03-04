11b19425affc4eea3b21c151e81e6b235e08186e
Gerbera3090 <hw3090@gmail.com>
2025-03-04 22:04:31 +0900

    feat: take functions

--- packages/api/src/common/util/util.ts
+++ packages/api/src/common/util/util.ts
@@ -1,3 +1,5 @@
+import { BadRequestException, NotFoundException } from "@nestjs/common";
+
 export const takeUnique = <T>(values: T[]): T => values[0];
 export const isEmptyObject = obj =>
   obj && Object.keys(obj).length === 0 && obj.constructor === Object;
@@ -27,4 +29,58 @@ export function getArrayDiff<T extends string | number>(
 
   // 합집합에서 교집합 요소를 제거
   return Array.from(union).filter(x => !intersection.has(x));
+}
+
+type IdType = number | string;
+export function takeOnlyOne<T>(name?: string): (array: T[]) => T {
+  return (array: T[]) => {
+    // 배열의 요소가 하나만 나왔는 지를 검증하는 함수
+    if (array.length === 0)
+      throw new NotFoundException(`${name ?? "array"} is empty`);
+    if (array.length > 1)
+      throw new BadRequestException(`${name ?? "array"} is not only one`);
+    return array[0];
+  };
+}
+
+export function getUniqueArray<
+  T extends string | number | boolean | symbol | null | undefined | bigint,
+>(array: T[]): T[] {
+  // 중복을 제외하고 배열을 반환하는 함수
+  // JS 기본 자료형에 대해 잘 작동할듯??
+  return [...new Set(array)];
+}
+
+export function checkContainAllId<T, K extends IdType>(
+  ids: K[],
+  array: T[],
+  name?: string,
+): asserts array is T[] & { [key in K]: T } {
+  // 중복을 제외하고, 넣은 id가 모두 값이 잘 나왔는지를 체크해서 값을 얻는 함수
+  const uniqueIds = getUniqueArray(ids);
+  if (ids.some(id => !uniqueIds.includes(id))) {
+    throw new NotFoundException(
+      `The length of ${name ?? "array"} does not match | id length: ${uniqueIds.length} || array length: ${array.length}`,
+    );
+  }
+}
+
+export function takeAll<T extends { id: K }, K extends IdType>(
+  ids: K[],
+  name?: string,
+): (array: T[]) => T[] {
+  // 중복을 제외하고, 넣은 id가 모두 값이 잘 나왔는지를 체크해서 값을 얻는 함수
+  // ex) 다른 모듈에서 club ids 로 find해서 모든 값이 있는 지 확인
+  return (array: T[]) => {
+    checkContainAllId(ids, array, name);
+    return array;
+  };
+}
+
+export function takeIfExist<T>(name?: string): (array: T[]) => T[] {
+  return (array: T[]) => {
+    if (array.length === 0)
+      throw new NotFoundException(`${name ?? "array"} is empty`);
+    return array;
+  };
 }
